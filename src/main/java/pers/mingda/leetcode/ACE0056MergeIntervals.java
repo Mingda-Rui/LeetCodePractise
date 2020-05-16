575023fa6eb3de0866d875dcb3431559da895586
Mingda Rui <mingdarui@gmail.com>
2020-05-16 15:53:00 +0100

    Leetcode problem 56 Merge Intervals V2

--- src/main/java/pers/mingda/leetcode/ACE0056MergeIntervals.java
+++ src/main/java/pers/mingda/leetcode/ACE0056MergeIntervals.java
@@ -1,9 +1,16 @@
 package pers.mingda.leetcode;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class ACE0056MergeIntervals {
-    public int[][] merge(int[][] intervals) {
+
+    /**
+     * O(n), iterate three times.
+     * @param intervals
+     * @return
+     */
+    public int[][] merge_v1(int[][] intervals) {
         if (intervals.length == 0) return intervals;
         int length = 3000;
         int[] lefts = new int[length];
@@ -20,8 +27,7 @@ public class ACE0056MergeIntervals {
             if (lefts[i] != 0) {
                 int originalOffset = offset;
                 offset += lefts[i];
-                if (originalOffset != 0)
-                    lefts[i] = 0;
+                if (originalOffset != 0) lefts[i] = 0;
             }
             if (rights[i] != 0) {
                 offset -= rights[i];
@@ -39,8 +45,43 @@ public class ACE0056MergeIntervals {
                 index++;
             }
         }
-
         return result;
+    }
+
+    /**
+     * Compared to the first version, use a LinkedList, iterate twice,
+     * but slower and use more memory
+     * @param intervals
+     * @return
+     */
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) return intervals;
+        int length = 3000;
+        int[] lefts = new int[length];
+        int[] rights = new int[length];
+
+        for (int[] interval : intervals) {
+            lefts[interval[0]]++;
+            rights[interval[1]]++;
+        }
+
+        int offset = 0;
+        int previousLegitStart = 0;
+        LinkedList<int[]> merged = new LinkedList<>();
+        for (int i = 0; i < length; i++) {
+            if (lefts[i] != 0) {
+                int originalOffset = offset;
+                offset += lefts[i];
+                if (originalOffset != 0) lefts[i] = 0;
+                else previousLegitStart = i;
+            }
+            if (rights[i] != 0) {
+                offset -= rights[i];
+                if (offset != 0) rights[i] = 0;
+                else merged.add(new int[]{previousLegitStart, i});
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
     }
 
     public static void main(String ... args) {
