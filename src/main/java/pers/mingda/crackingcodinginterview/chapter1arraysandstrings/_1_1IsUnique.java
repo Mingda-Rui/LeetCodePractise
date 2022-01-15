ccb93ec8fdf5feb75a11f1451a1da9b0e3638acf
Mingda Rui <mingdarui@gmail.com>
2022-01-15 10:34:06 +0000

    1.1 Is Unique: Improvements with arrays solutions

--- src/main/java/pers/mingda/crackingcodinginterview/chapter1arraysandstrings/_1_1IsUnique.java
+++ src/main/java/pers/mingda/crackingcodinginterview/chapter1arraysandstrings/_1_1IsUnique.java
@@ -1,16 +1,23 @@
 package pers.mingda.crackingcodinginterview.chapter1arraysandstrings;
 
-/* Implement an algorithm to determine if a string has all unique characters. 
+/* Is Unique: Implement an algorithm to determine if a string has all unique characters. 
  * What if you cannot use additional data structures? 
  * */
 
 public class _1_1IsUnique {
     
     public boolean isUnique(String str) {
+        
         return false;
     }
 
     public boolean isUniqueWithArrys(String str) {
+        // approvement: if the string is longer than 128/256 chars
+        // it has to have duplications.
+        if (str.length() > 256)
+            return false;
+        
+        // approvement: boolean array could be better
         int[] charRecorder = new int[255];
         for (char c: str.toCharArray()) {
             if (charRecorder[c] != 0) return false;
