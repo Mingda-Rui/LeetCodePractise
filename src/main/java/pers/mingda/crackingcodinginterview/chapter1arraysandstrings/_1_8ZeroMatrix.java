ded49b189ce2550755cb9606d37e98706fd179b4
Mingda Rui <mingdarui@gmail.com>
2022-01-19 09:13:25 +0000

    1.8 Zero Matrix: no extra space

--- src/main/java/pers/mingda/crackingcodinginterview/chapter1arraysandstrings/_1_8ZeroMatrix.java
+++ src/main/java/pers/mingda/crackingcodinginterview/chapter1arraysandstrings/_1_8ZeroMatrix.java
@@ -26,4 +26,43 @@ public class _1_8ZeroMatrix {
             }
         }
     }
+
+    public static void setZerosNoExtraSpace(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                setZerosRowAndColumn(matrix, i, -1);
+            }
+        }
+
+        for (int j = 1; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                setZerosRowAndColumn(matrix, -1, j);
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            setZerosRowAndColumn(matrix, 0, 0);
+        }
+    }
+
+    private static void setZerosRowAndColumn(int[][] matrix, int row, int column) {
+        if (row >= 0) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[row][j] = 0;
+            }
+        }
+        if (column >= 0) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][column] = 0;
+            }
+        }
+    }
 }
--- src/test/java/pers/mingda/crackingcodinginterview/chapter1arraysandstrings/_1_8ZeroMatrixTest.java
+++ src/test/java/pers/mingda/crackingcodinginterview/chapter1arraysandstrings/_1_8ZeroMatrixTest.java
@@ -8,10 +8,16 @@ public class _1_8ZeroMatrixTest {
 
     @Test
     public void testSetZeros() {
-        int[][] testMatrix = new int[][] {{1,1,1,1}, {1,1,1,1}, {1,1,1,1}, {1,1,1,0}};
-        int[][] expectedMatrix = new int[][] {{1,1,1,0}, {1,1,1,0}, {1,1,1,0},{0,0,0,0}};
+        int[][] testMatrix = new int[][] {{0,1,1,1}, {1,1,1,1}, {1,1,1,1}, {1,1,1,1}};
+        int[][] expectedMatrix = new int[][] {{0,0,0,0}, {0,1,1,1}, {0,1,1,1}, {0,1,1,1}};
         _1_8ZeroMatrix.setZeros(testMatrix);
         assertArrayEquals(expectedMatrix, testMatrix);
+
+        testMatrix = new int[][] {{1,1,1,1}, {1,1,1,1}, {1,1,1,1}, {1,1,1,0}};
+        expectedMatrix = new int[][] {{1,1,1,0}, {1,1,1,0}, {1,1,1,0},{0,0,0,0}};
+        _1_8ZeroMatrix.setZeros(testMatrix);
+        assertArrayEquals(expectedMatrix, testMatrix);
+
 
         testMatrix = new int[][] {{1,1,1,1}, {1,0,1,1}, {1,1,0,1}, {1,1,1,1}};
         expectedMatrix = new int[][] {{1,0,0,1}, {0,0,0,0}, {0,0,0,0},{1,0,0,1}};
@@ -29,4 +35,32 @@ public class _1_8ZeroMatrixTest {
         assertArrayEquals(expectedMatrix, testMatrix);
     }
     
+    @Test
+    public void testSetZerosNoExtraSpace() {
+        int[][] testMatrix = new int[][] {{0,1,1,1}, {1,1,1,1}, {1,1,1,1}, {1,1,1,1}};
+        int[][] expectedMatrix = new int[][] {{0,0,0,0}, {0,1,1,1}, {0,1,1,1}, {0,1,1,1}};
+        _1_8ZeroMatrix.setZerosNoExtraSpace(testMatrix);
+        assertArrayEquals(expectedMatrix, testMatrix);
+
+        testMatrix = new int[][] {{1,1,1,1}, {1,1,1,1}, {1,1,1,1}, {1,1,1,0}};
+        expectedMatrix = new int[][] {{1,1,1,0}, {1,1,1,0}, {1,1,1,0},{0,0,0,0}};
+        _1_8ZeroMatrix.setZerosNoExtraSpace(testMatrix);
+        assertArrayEquals(expectedMatrix, testMatrix);
+
+
+        testMatrix = new int[][] {{1,1,1,1}, {1,0,1,1}, {1,1,0,1}, {1,1,1,1}};
+        expectedMatrix = new int[][] {{1,0,0,1}, {0,0,0,0}, {0,0,0,0},{1,0,0,1}};
+        _1_8ZeroMatrix.setZerosNoExtraSpace(testMatrix);
+        assertArrayEquals(expectedMatrix, testMatrix);
+
+        testMatrix = new int[][] {{1,1,1,0}, {1,1,0,1}, {1,0,1,1}, {0,1,1,1}};
+        expectedMatrix = new int[][] {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+        _1_8ZeroMatrix.setZerosNoExtraSpace(testMatrix);
+        assertArrayEquals(expectedMatrix, testMatrix);
+
+        testMatrix = new int[][] {{1,2,3,4}, {4,3,2,1}, {1,1,1,1}, {1,1,1,1}};
+        expectedMatrix = new int[][] {{1,2,3,4}, {4,3,2,1}, {1,1,1,1}, {1,1,1,1}};
+        _1_8ZeroMatrix.setZerosNoExtraSpace(testMatrix);
+        assertArrayEquals(expectedMatrix, testMatrix);
+    }
 }
