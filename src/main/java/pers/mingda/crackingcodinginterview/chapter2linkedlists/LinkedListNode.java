854f1da37ffced6ec12e48d82262836debcc34bf
Mingda Rui <mingdarui@gmail.com>
2022-01-22 19:21:50 +0000

    LinkedListNode and builder

---
+++ src/main/java/pers/mingda/crackingcodinginterview/chapter2linkedlists/LinkedListNode.java
@@ -0,0 +1,12 @@
+package pers.mingda.crackingcodinginterview.chapter2linkedlists;
+
+public class LinkedListNode {
+    
+    protected LinkedListNode next;
+    protected int data;
+
+    public LinkedListNode(LinkedListNode next, int data) {
+        this.next = next;
+        this.data = data;
+    }
+}
