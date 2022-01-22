005e8086bd8fa547fac5e42aeaf19462d1eab9de
Mingda Rui <mingdarui@gmail.com>
2022-01-22 21:41:44 +0000

    LinkedListTestHelper

---
+++ src/test/java/pers/mingda/crackingcodinginterview/chapter2linkedlists/LinkedListTestHelper.java
@@ -0,0 +1,30 @@
+package pers.mingda.crackingcodinginterview.chapter2linkedlists;
+
+import java.util.List;
+
+public class LinkedListTestHelper {
+    public LinkedListNode buildNodeList(List<Integer> nodeValues) {
+        if (nodeValues.isEmpty()) {
+            throw new IllegalArgumentException("Failed to crate linked list, no valid data provided");
+        }
+        LinkedListNode node = new LinkedListNode(null, nodeValues.get(0));
+        LinkedListNode headNode = node;
+        for (int i = 1; i < nodeValues.size(); i++) {
+            node.next = new LinkedListNode(null, nodeValues.get(i));
+            node = node.next;
+        }
+        return headNode;
+    }
+
+    public boolean equals(LinkedListNode node1, LinkedListNode node2) {
+        while (node1 != null && node2 != null) {
+            if (node1.data != node2.data) {
+                return false;
+            }
+            node1 = node1.next;
+            node2 = node2.next;
+        }
+
+        return node1 == null && node2 == null;
+    }
+}
---
+++ src/test/java/pers/mingda/crackingcodinginterview/chapter2linkedlists/LinkedListTestHelperTest.java
@@ -0,0 +1,46 @@
+package pers.mingda.crackingcodinginterview.chapter2linkedlists;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LinkedListTestHelperTest {
+
+    private LinkedListTestHelper testHelper;
+
+    @BeforeEach
+    public void setup() {
+        testHelper = new LinkedListTestHelper();
+    }
+
+    @Test
+    public void testBuildNodeList() {
+        List<Integer> nodeValues = List.of(0, 1, 2, 3, 4);
+        LinkedListNode node = testHelper.buildNodeList(nodeValues);
+
+        for (int nodeValue: nodeValues) {
+            assertEquals(nodeValue, node.data);
+            node = node.next;
+        }
+        assertNull(node);
+    }
+
+    @Test
+    public void testEquals() {
+        List<Integer> nodeValues = List.of(0, 1, 2, 3, 4);
+        
+        LinkedListNode node = testHelper.buildNodeList(nodeValues);
+        LinkedListNode equalNode = testHelper.buildNodeList(nodeValues);
+        assertTrue(testHelper.equals(node, equalNode));
+
+        List<Integer> unequalNodeValues = List.of(0, 1, 2, 3, 5);
+        LinkedListNode unequalNode = testHelper.buildNodeList(unequalNodeValues);
+        assertFalse(testHelper.equals(node, unequalNode));
+    }
+}
