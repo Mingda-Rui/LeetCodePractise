f0ff9c834026485b7a86027cfb7b02a78fa8bc0b
Mingda Rui <mingdarui@gmail.com>
2022-01-25 11:44:44 +0000

    2.4 Partition: add to head and tail

--- src/main/java/pers/mingda/crackingcodinginterview/chapter2linkedlists/_2_4Partition.java
+++ src/main/java/pers/mingda/crackingcodinginterview/chapter2linkedlists/_2_4Partition.java
@@ -47,6 +47,25 @@ public class _2_4Partition {
         return head;
     }
 
+    public static LinkedListNode partitionAddToHeadAndTail(LinkedListNode node, int x) {
+        LinkedListNode head = node;
+        LinkedListNode tail = node;
+
+        while (node != null) {
+            LinkedListNode next = node.next;
+            if (node.data < x) {
+                node.next = head;
+                head = node;
+            } else {
+                tail.next = node;
+                tail = tail.next;
+            }
+            node = next;
+        }
+        tail.next = null;
+        return head;
+    }
+
     private static LinkedListNode removePseudoHead(LinkedListNode pseudoHead) {
         LinkedListNode realHead = pseudoHead.next;
         pseudoHead.next = null;
--- src/test/java/pers/mingda/crackingcodinginterview/chapter2linkedlists/_2_4PartitionTest.java
+++ src/test/java/pers/mingda/crackingcodinginterview/chapter2linkedlists/_2_4PartitionTest.java
@@ -71,4 +71,31 @@ public class _2_4PartitionTest {
             result = result.next;
         }
     }
+
+    @Test
+    public void testPartitionAddToHeadAndTail() {
+        // Input:       3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition = 5]
+        // Output:      3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
+
+        List<Integer> testValues = List.of(3, 5, 8, 5, 10, 2, 1);
+        LinkedListNode testNode = testHelper.buildNodeList(testValues);
+
+        int partitionVal  = 2;
+        LinkedListNode result = _2_4Partition.partitionAddToHeadAndTail(testNode, partitionVal);
+        int resultSize = testHelper.getSize(result);
+        assertEquals(testValues.size(), resultSize);
+        
+        boolean shouldSmall = true;
+        while (result != null) {
+            if (result.data >= partitionVal && shouldSmall) {
+                shouldSmall = false;
+            }
+            if (shouldSmall) {
+                assertTrue(result.data < partitionVal);
+            } else {
+                assertFalse(result.data < partitionVal);
+            }
+            result = result.next;
+        }
+    }
 }
