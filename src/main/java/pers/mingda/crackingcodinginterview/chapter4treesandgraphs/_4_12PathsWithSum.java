2f9b501179e54060bccf629cd56e981a227a43c0
Mingda Rui <mingdarui@gmail.com>
2022-02-28 22:20:17 +0000

    4.12 Paths with Sum: brute force

---
+++ src/main/java/pers/mingda/crackingcodinginterview/chapter4treesandgraphs/_4_12PathsWithSum.java
@@ -0,0 +1,34 @@
+package pers.mingda.crackingcodinginterview.chapter4treesandgraphs;
+
+/**
+ *  4.12 Paths with Sum: You are given a binary tree in which each node contains an integer value (which
+ *  might be positive or negative). Design an algorithm to count the number of paths that sum to a
+ *  given value. The path does not need to start or end at the root or a leaf, but it must go downwards
+ *  (traveling only from parent nodes to child nodes).
+ */
+
+public class _4_12PathsWithSum {
+    public static int countPathsWithSum(TreeNode node, int targetSum) {
+        if (node == null)
+            return 0;
+
+        // int targetRemain = targetSum - node.data;
+        // counter += countPathsWithSum(node.left, targetRemain);
+        // counter += countPathsWithSum(node.right, targetRemain);
+        int counter = countPathsWithRemainSum(node, targetSum);
+        counter += countPathsWithSum(node.left, targetSum);
+        counter += countPathsWithSum(node.right, targetSum);
+        return counter;
+    }
+
+    private static int countPathsWithRemainSum(TreeNode node, int remainSum) {
+        if (node == null)
+            return 0;
+        int counter = 0;
+        if (node.data == remainSum)
+            counter++;
+        counter += countPathsWithRemainSum(node.left, remainSum - node.data);
+        counter += countPathsWithRemainSum(node.right, remainSum - node.data);
+        return counter;
+    }
+}
---
+++ src/test/java/pers/mingda/crackingcodinginterview/chapter4treesandgraphs/_4_12PathsWIthSumTest.java
@@ -0,0 +1,27 @@
+package pers.mingda.crackingcodinginterview.chapter4treesandgraphs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class _4_12PathsWIthSumTest {
+
+    private TreeNodeTestHelper testHelper;
+
+    @BeforeEach
+    public void setup() {
+        testHelper = new TreeNodeTestHelper();
+    }
+
+    @Test
+    public void testCountPathsWithSum() {
+        TreeNode three = testHelper.createTreeNode(3, 3, -2);
+        TreeNode two = testHelper.createTreeNode(2, null, 1);
+        TreeNode five = testHelper.createTreeNode(5, three, two);
+        TreeNode minusThree = testHelper.createTreeNode(-3, null, 11);
+        TreeNode root = testHelper.createTreeNode(10, five, minusThree);
+        int pathsWithSum = _4_12PathsWithSum.countPathsWithSum(root, 8);
+        assertEquals(3, pathsWithSum);
+    }
+}
