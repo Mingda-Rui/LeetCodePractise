3979e6f0c989ac582b9b8146759a1df82d5dd02c
Mingda Rui <mingdarui@gmail.com>
2022-03-04 21:57:22 +0000

    4.1 Route Between Nodes

---
+++ src/main/java/pers/mingda/crackingcodinginterview/chapter4treesandgraphs/Graph.java
@@ -0,0 +1,16 @@
+package pers.mingda.crackingcodinginterview.chapter4treesandgraphs;
+
+import java.util.List;
+
+public class Graph {
+
+    private List<Node> nodes;
+
+    public Graph(List<Node> nodes) {
+        this.nodes = nodes;
+    }
+
+    public List<Node> getNodes() {
+        return nodes;
+    }
+}
