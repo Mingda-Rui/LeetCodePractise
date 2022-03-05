9016dd7b51344a5114601e0ebb559d3011100bdf
Mingda Rui <mingdarui@gmail.com>
2022-03-05 17:12:28 +0000

    1971 Find if Path Exists in Graph: add comments

--- src/main/java/pers/mingda/leetcode/CXV1971FindIfPathExistsInGraph.java
+++ src/main/java/pers/mingda/leetcode/CXV1971FindIfPathExistsInGraph.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class CXV1971FindIfPathExistsInGraph {
+
+    // BFS
     public boolean validPathIteration(int n, int[][] edges, int source, int destination) {
         boolean[] visited = new boolean[n];
         visited[source] = true;
@@ -29,7 +31,7 @@ public class CXV1971FindIfPathExistsInGraph {
         return visited[destination];
     }
 
-
+    // DFS
     public boolean validPathMapSolution(int n, int[][] edges, int source, int destination) {
         // Given a vertex, get all its neighbors.
         // a table/array to record whether certain vertex is traversed.
