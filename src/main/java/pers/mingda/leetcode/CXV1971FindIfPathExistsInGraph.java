2d43c8718df507f49e69792902fadcc9bf0781c3
Mingda Rui <mingdarui@gmail.com>
2022-03-05 16:47:58 +0000

    1971 Find if Path Existst in Graph: Iteration solution (fastest solution)

--- src/main/java/pers/mingda/leetcode/CXV1971FindIfPathExistsInGraph.java
+++ src/main/java/pers/mingda/leetcode/CXV1971FindIfPathExistsInGraph.java
@@ -4,7 +4,33 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class CXV1971FindIfPathExistsInGraph {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
+    public boolean validPathIteration(int n, int[][] edges, int source, int destination) {
+        boolean[] visited = new boolean[n];
+        visited[source] = true;
+        boolean newVisit = true;
+
+        while (newVisit) {
+            newVisit = false;
+            for (int[] edge: edges) {
+                if (visited[destination])
+                    return true;
+                int vertex1 = edge[0];
+                int vertex2 = edge[1];
+                if (visited[vertex1] && !visited[vertex2]) {
+                    visited[vertex2] = true;
+                    newVisit = true;
+                } else if (!visited[vertex1] && visited[vertex2]) {
+                    visited[vertex1] = true;
+                    newVisit = true;
+                }
+            }
+        }
+
+        return visited[destination];
+    }
+
+
+    public boolean validPathMapSolution(int n, int[][] edges, int source, int destination) {
         // Given a vertex, get all its neighbors.
         // a table/array to record whether certain vertex is traversed.
 
