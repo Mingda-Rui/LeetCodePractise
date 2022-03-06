2cfc237b849f5bc7f9c52267a705adc423505642
Mingda Rui <mingdarui@gmail.com>
2022-03-06 19:22:57 +0000

    238 Product of Array Except Self: optimize to two for loop (but seems no help in total run time)

--- src/main/java/pers/mingda/leetcode/AJE0238ProductOfArrayExceptSelf.java
+++ src/main/java/pers/mingda/leetcode/AJE0238ProductOfArrayExceptSelf.java
@@ -2,20 +2,22 @@ package pers.mingda.leetcode;
 
 public class AJE0238ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
+        return productExceptSelfTraverse(nums);
+    }
+
+    private int[] productExceptSelfTraverse(int[] nums) {
         int[] result = new int[nums.length];
 
         for (int i = result.length - 1; i >= 0; i--) {
             int previous = i == result.length - 1 ? 1: result[i + 1];
             result[i] = previous * nums[i];
         }
+
+        int product = 1;
         for (int i = 0; i < nums.length; i++) {
-            int previous = i == 0 ? 1 : nums[i - 1];
-            nums[i] = previous * nums[i];
-        }
-        for (int i = 0; i < nums.length; i++) {
-            int previous = i == 0 ? 1 : nums[i - 1];
             int after = i == nums.length - 1 ? 1 : result[i + 1];
-            result[i] = previous * after;
+            result[i] = after * product;
+            product = nums[i] * product;
         }
 
         return result;
