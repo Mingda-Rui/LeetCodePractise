c54becea1656f91462f87865da469bdde73eb7aa
Mingda Rui <mingdarui@gmail.com>
2022-03-06 19:38:05 +0000

    238 Product of Array Except Self: 2 step less product calculation

--- src/main/java/pers/mingda/leetcode/AJE0238ProductOfArrayExceptSelf.java
+++ src/main/java/pers/mingda/leetcode/AJE0238ProductOfArrayExceptSelf.java
@@ -8,16 +8,16 @@ public class AJE0238ProductOfArrayExceptSelf {
     private int[] productExceptSelfTraverse(int[] nums) {
         int[] result = new int[nums.length];
 
-        for (int i = result.length - 1; i >= 0; i--) {
-            int previous = i == result.length - 1 ? 1: result[i + 1];
-            result[i] = previous * nums[i];
+        int productOfPrevious = 1;
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = productOfPrevious;
+            productOfPrevious *= nums[i];
         }
 
-        int product = 1;
-        for (int i = 0; i < nums.length; i++) {
-            int after = i == nums.length - 1 ? 1 : result[i + 1];
-            result[i] = after * product;
-            product = nums[i] * product;
+        int productOfAfter = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= productOfAfter;
+            productOfAfter *= nums[i];
         }
 
         return result;
