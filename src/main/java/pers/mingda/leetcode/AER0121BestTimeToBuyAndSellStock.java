fdad3d4f7c8ad4dad219f7427b5dab57dc6cbac0
Mingda Rui <mingdarui@gmail.com>
2022-03-06 16:52:29 +0000

    0121 Best Time to Buy and Sell Stock

---
+++ src/main/java/pers/mingda/leetcode/AER0121BestTimeToBuyAndSellStock.java
@@ -0,0 +1,14 @@
+package pers.mingda.leetcode;
+
+public class AER0121BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+        int maxProfit = Integer.MIN_VALUE;
+        int minPrice = Integer.MAX_VALUE;
+        for (int price: prices) {
+            maxProfit = Math.max(maxProfit, price - minPrice);
+            minPrice = Math.min(minPrice, price);
+        }
+        return Math.max(0, maxProfit);
+    }
+}
