0d45abe5b85b25e114b1f3f278a7c35f61abf7c7
Mingda Rui <mingdarui@gmail.com>
2022-03-06 16:19:03 +0000

    0001 Two Sum: sorting solution nlogn

--- src/main/java/pers/mingda/leetcode/AAB0001TwoSum.java
+++ src/main/java/pers/mingda/leetcode/AAB0001TwoSum.java
@@ -1,5 +1,7 @@
 package pers.mingda.leetcode;
 
+import java.util.Arrays;
+
 public class AAB0001TwoSum {
 
     public int[] twoSumBruteForce(int[] nums, int target) {
@@ -9,6 +11,34 @@ public class AAB0001TwoSum {
                     return new int[]{i, j};
                 }
             }
+        }
+        throw new RuntimeException("No solution found!");
+    }
+
+    public int[] twoSumSorting(int[] nums, int target) {
+        int[] coyied = Arrays.copyOf(nums, nums.length);
+        int[] twoNums = getNums(coyied, target);
+        int first = -1;
+        int second = -1;
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] == twoNums[0] || nums[i] == twoNums[1]) {
+                first = first == -1 ? i : first;
+                second = first == -1 ? second : i;
+            }
+
+        return new int[]{first, second};
+    }
+
+    private int[] getNums(int[] nums, int target) {
+        Arrays.sort(nums);
+        int first = 0;
+        int second = nums.length - 1;
+        while (first < second) {
+            int sum = nums[first] + nums[second];
+            if (sum == target)
+                return new int[]{nums[first], nums[second]};
+            first = sum < target ? first + 1 : first;
+            second = sum > target ? second - 1 : second;
         }
         throw new RuntimeException("No solution found!");
     }
