9c1dacec3747315a049e948af746baaf186ce5d0
Mingda Rui <mingdarui@gmail.com>
2022-03-06 21:40:06 +0000

    152 Maximum Product Subarray: fix typo

--- src/main/java/pers/mingda/leetcode/AFW0152MaximumProductSubarray.java
+++ src/main/java/pers/mingda/leetcode/AFW0152MaximumProductSubarray.java
@@ -5,18 +5,18 @@ public class AFW0152MaximumProductSubarray {
     public int maxProduct(int[] nums) {
         int max = Integer.MIN_VALUE;
         int product = 1;
-        int maxNegitive = 1;
+        int maxNegative = 1;
         for (int num: nums) {
             if (num == 0) {
                 product = 1;
-                maxNegitive = 1;
+                maxNegative = 1;
                 max = Math.max(max, 0);
             } else {
                 product *= num;
-                int currentMax = product < 0 ? product / maxNegitive : product;
+                int currentMax = product < 0 ? product / maxNegative : product;
                 max = Math.max(max, currentMax);
                 if (product < 0) {
-                    maxNegitive = maxNegitive > 0 ? product : Math.max(maxNegitive, product);
+                    maxNegative = maxNegative > 0 ? product : Math.max(maxNegative, product);
                 }
             }
         }
