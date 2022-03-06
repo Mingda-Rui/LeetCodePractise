438764f30ba61485f96ba0efcfdc2e10c20a7f57
Mingda Rui <mingdarui@gmail.com>
2022-03-06 13:11:45 +0000

    0001 Two Sums: brute force solution

---
+++ src/main/java/pers/mingda/leetcode/AAB0001TwoSum.java
@@ -0,0 +1,15 @@
+package pers.mingda.leetcode;
+
+public class AAB0001TwoSum {
+
+    public int[] twoSumBruteForce(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new RuntimeException("No solution found!");
+    }
+}
