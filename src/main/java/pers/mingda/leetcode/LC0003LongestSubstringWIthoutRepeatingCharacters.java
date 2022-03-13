aa5d7eaaeaa575a9ca71d685d5914175f4fb6e05
Mingda Rui <mingdarui@gmail.com>
2022-03-13 17:08:34 +0000

    LC0003 Longest Substring without Repeating Characters: record previous index for each character solution

--- src/main/java/pers/mingda/leetcode/LC0003LongestSubstringWIthoutRepeatingCharacters.java
+++ src/main/java/pers/mingda/leetcode/LC0003LongestSubstringWIthoutRepeatingCharacters.java
@@ -22,4 +22,27 @@ public class LC0003LongestSubstringWithoutRepeatingCharacters {
         }
         return Math.max(currentLength, maxLength);
     }
+
+    public int lengthOfLongestSubstringPreviousIndex(String s) {
+        int[] record = new int[256];
+        for (int i = 0; i < record.length; i++)
+            record[i] = -1;
+        int startIndex = 0;
+        int currentLength = 0;
+        int maxLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (record[c] < startIndex) {
+                record[c] = i;
+                currentLength++;
+                maxLength = Math.max(currentLength, maxLength);
+            } else {
+                int previousIndex = record[c];
+                currentLength -= previousIndex - startIndex;
+                startIndex = previousIndex + 1;
+                record[c] = i;
+            }
+        }
+        return maxLength;
+    }
 }
