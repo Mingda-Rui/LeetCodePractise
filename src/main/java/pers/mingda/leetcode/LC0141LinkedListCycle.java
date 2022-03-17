1561f3bc5a46f114746560430b16a4b3cc7b1234
Mingda Rui <mingdarui@gmail.com>
2022-03-17 23:00:34 +0000

    LC0141 Linked List Cycle: refactor

--- src/main/java/pers/mingda/leetcode/LC0141LinkedListCycle.java
+++ src/main/java/pers/mingda/leetcode/LC0141LinkedListCycle.java
@@ -2,14 +2,14 @@ package pers.mingda.leetcode;
 
 public class LC0141LinkedListCycle {
     public boolean hasCycle(ListNode head) {
-        if (head == null)
-            return false;
-        ListNode fast = head.next;
+        ListNode fast = head;
         ListNode slow = head;
-        while (fast != null && fast.next != null && fast != slow) {
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
+            if (fast == slow)
+                return true;
         }
-        return fast == slow;
+        return false;
     }
 }
