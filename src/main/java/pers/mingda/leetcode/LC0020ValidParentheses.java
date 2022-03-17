b55e459aaabbd3f2de5368ecf1d9b62bb5c2bd8c
Mingda Rui <mingdarui@gmail.com>
2022-03-17 13:38:28 +0000

    LC0020 Valid Parentheses: refactor

--- src/main/java/pers/mingda/leetcode/LC0020ValidParentheses.java
+++ src/main/java/pers/mingda/leetcode/LC0020ValidParentheses.java
@@ -4,6 +4,9 @@ import java.util.Stack;
 
 public class LC0020ValidParentheses {
     public boolean isValid(String s) {
+        if (s.length() % 2 != 0)
+            return false;
+
         Stack<Character> stack = new Stack<>();
         for (char p: s.toCharArray()) {
             if (p == '(' || p == '{' || p == '[') {
@@ -12,19 +15,20 @@ public class LC0020ValidParentheses {
                 return false;
             } else {
                 char open = stack.pop();
-                if (p != getClose(open))
+                if (p != getClosure(open))
                     return false;
             }
         }
         return stack.isEmpty();
     }
 
-    private char getClose(char open) {
-        if (open == '(')
-            return ')';
-        else if (open == '{')
-            return '}';
-        else
-            return ']';
+    private char getClosure(char open) {
+        char closure;
+        switch (open) {
+            case '(': closure = ')'; break;
+            case '{': closure = '}'; break;
+            default: closure = ']';
+        }
+        return closure;
     }
 }
