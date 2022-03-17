fbcf747014ef871df7e4be855422da6d9db957be
Mingda Rui <mingdarui@gmail.com>
2022-03-17 23:03:11 +0000

    move ListNode to a standalone class

--- src/main/java/pers/mingda/leetcode/LC0206ReverseLinkedList.java
+++ src/main/java/pers/mingda/leetcode/LC0206ReverseLinkedList.java
@@ -124,10 +124,3 @@ public class LC0206ReverseLinkedList {
     }
 }
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
