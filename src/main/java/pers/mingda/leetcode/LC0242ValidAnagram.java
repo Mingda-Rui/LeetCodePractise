4f9e20aaf25c34ab8501e8d08b1d688a577a0870
Mingda Rui <mingdarui@gmail.com>
2022-03-17 11:35:33 +0000

    LC0242 Valid Anagram: array solution

---
+++ src/main/java/pers/mingda/leetcode/LC0242ValidAnagram.java
@@ -0,0 +1,21 @@
+package pers.mingda.leetcode;
+
+public class LC0242ValidAnagram {
+    public boolean isAnagramArraySolution(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        int[] record = new int[128];
+        for (char cInS: s.toCharArray())
+            record[cInS]++;
+        for (char cInT: t.toCharArray()) {
+            record[cInT]--;
+            if (record[cInT] < 0)
+                return false;
+        }
+        for (int count: record) {
+            if (count < 0)
+                return false;
+        }
+        return true;
+    }
+}
