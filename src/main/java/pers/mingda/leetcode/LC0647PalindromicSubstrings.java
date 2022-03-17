41fab34e0a4d7954491da3d906151166c3ea0e31
Mingda Rui <mingdarui@gmail.com>
2022-03-17 21:35:52 +0000

    LC0647 Palindromic Substrings

---
+++ src/main/java/pers/mingda/leetcode/LC0647PalindromicSubstrings.java
@@ -0,0 +1,22 @@
+package pers.mingda.leetcode;
+
+public class LC0647PalindromicSubstrings {
+    public int countSubstrings(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            result += countPalindromicSubstrings(s, i, i);
+            result += countPalindromicSubstrings(s, i, i + 1);
+        }
+        return result;
+    }
+
+    private int countPalindromicSubstrings(String s, int left, int right) {
+        int result = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            result++;
+            left--;
+            right++;
+        }
+        return result;
+    }
+}
