ae2dde5e830f48c99e3d95a6cd8a6523a698551e
Mingda Rui <mingdarui@gmail.com>
2022-03-17 11:10:50 +0000

    LC0076 Minimum Window Substring: better variable naming

--- src/main/java/pers/mingda/leetcode/LC0076MinimumWindowSubstring.java
+++ src/main/java/pers/mingda/leetcode/LC0076MinimumWindowSubstring.java
@@ -9,21 +9,21 @@ public class LC0076MinimumWindowSubstring {
         int resultEnd = s.length();
         int start = 0;
         int[] record = new int[128];
-        Map<Character, Integer> cur = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         for (char c: t.toCharArray()) {
             record[c]++;
-            cur.put(c, record[c]);
+            map.put(c, record[c]);
         }
 
         int[] current = new int[128];
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             current[c]++;
-            Object val = cur.computeIfPresent(c, (k, v) -> v - 1);
+            Object val = map.computeIfPresent(c, (k, v) -> v - 1);
 
             if (val != null && ((int) val) == 0)
-                cur.remove(c);
-            while(cur.isEmpty()) {
+                map.remove(c);
+            while(map.isEmpty()) {
                 if (i - start < resultEnd - resultStart) {
                     resultStart = start;
                     resultEnd = i;
@@ -34,7 +34,7 @@ public class LC0076MinimumWindowSubstring {
                 start++;
                 int offset = record[charAtStart] - current[charAtStart];
                 if (offset > 0)
-                    cur.put(charAtStart, offset);
+                    map.put(charAtStart, offset);
             }
         }
         return resultEnd == s.length() ? "" : s.substring(resultStart, resultEnd + 1) ;
