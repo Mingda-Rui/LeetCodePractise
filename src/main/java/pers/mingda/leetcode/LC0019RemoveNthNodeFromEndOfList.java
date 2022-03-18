138f4f0da08c74d22836b78fedcdc19adee6b322
Mingda Rui <mingdarui@gmail.com>
2022-03-18 20:53:55 +0000

    LC0019 Remove Nth Node From End of List: recursive solution

--- src/main/java/pers/mingda/leetcode/LC0019RemoveNthNodeFromEndOfList.java
+++ src/main/java/pers/mingda/leetcode/LC0019RemoveNthNodeFromEndOfList.java
@@ -16,4 +16,19 @@ public class LC0019RemoveNthNodeFromEndOfList {
         slow.next = slow.next.next;
         return OneBeforeHead.next;
     }
+
+    public ListNode removeNthFromEndRecursive(ListNode head, int n) {
+        ListNode pseudoHead = new ListNode(-1, head);
+        removeNthFromEndHelper(pseudoHead, n);
+        return pseudoHead.next;
+    }
+
+    private int removeNthFromEndHelper(ListNode head, int n) {
+        if (head == null)
+            return 0;
+        int count = removeNthFromEndHelper(head.next, n);
+        if (count == n)
+            head.next = head.next.next;
+        return count + 1;
+    }
 }
