9e49932d93ac2e5a08c08ea7f713b679d5a9dc63
Mingda Rui <mingdarui@gmail.com>
2022-03-18 00:44:21 +0000

    LC0023 Merge k Sorted Lists: refactor

--- src/main/java/pers/mingda/leetcode/LC0023MergeKSortedLists.java
+++ src/main/java/pers/mingda/leetcode/LC0023MergeKSortedLists.java
@@ -2,16 +2,16 @@ package pers.mingda.leetcode;
 
 public class LC0023MergeKSortedLists {
     public ListNode mergeKListsBruteForce(ListNode[] lists) {
-        ListNode smallest = new ListNode(Integer.MAX_VALUE);
+        ListNode smallest = null;
         int index = -1;
         for (int i = 0; i < lists.length; i++) {
             ListNode list = lists[i];
-            if (list != null && smallest.val > list.val) {
+            if (list != null && (smallest == null || smallest.val > list.val)) {
                 index = i;
                 smallest = list;
             }
         }
-        if (index == -1)
+        if (smallest == null)
             return null;
         lists[index] = lists[index].next;
         smallest.next = mergeKListsBruteForce(lists);
