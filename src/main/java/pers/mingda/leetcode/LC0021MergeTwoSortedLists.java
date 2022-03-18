2523d1400544cc4a936fe45c176bb7f6383b15a5
Mingda Rui <mingdarui@gmail.com>
2022-03-18 00:01:19 +0000

    LC0021 Merge Two Sorted Lists: recursive solution

--- src/main/java/pers/mingda/leetcode/LC0021MergeTwoSortedLists.java
+++ src/main/java/pers/mingda/leetcode/LC0021MergeTwoSortedLists.java
@@ -17,4 +17,18 @@ public class LC0021MergeTwoSortedLists {
 
         return head.next;
     }
+
+    public ListNode mergeTwoListsRecursive(ListNode list1, ListNode list2) {
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+
+        boolean list1Smaller = list1.val < list2.val;
+        ListNode small = list1Smaller ? list1 : list2;
+        ListNode large = list1Smaller ? list2 : list1;
+        small.next = mergeTwoListsRecursive(small.next, large);
+
+        return small;
+    }
 }
