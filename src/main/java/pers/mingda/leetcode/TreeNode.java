b8cf7044beacc0c3e7061fe45302423c21783866
Mingda Rui <mingdarui@gmail.com>
2022-03-27 20:28:32 +0100

    TreeNode

---
+++ src/main/java/pers/mingda/leetcode/TreeNode.java
@@ -0,0 +1,14 @@
+package pers.mingda.leetcode;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
