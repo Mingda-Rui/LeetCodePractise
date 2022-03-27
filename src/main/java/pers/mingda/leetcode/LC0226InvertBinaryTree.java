d9659577449c0e66688d4b38d0b5e3cfcb9d1a44
Mingda Rui <mingdarui@gmail.com>
2022-03-27 21:12:58 +0100

    LC0226 invert Binary Tree: iterative solution

--- src/main/java/pers/mingda/leetcode/LC0226InvertBinaryTree.java
+++ src/main/java/pers/mingda/leetcode/LC0226InvertBinaryTree.java
@@ -1,5 +1,8 @@
 package pers.mingda.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LC0226InvertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
         if (root == null)
@@ -8,6 +11,22 @@ public class LC0226InvertBinaryTree {
         TreeNode right = invertTree(root.right);
         root.left = right;
         root.right = left;
+        return root;
+    }
+
+    public TreeNode invertTreeIterative(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                TreeNode tmp = node.left;
+                node.left = node.right;
+                node.right = tmp;
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }
+        }
         return root;
     }
 }
