ab8c4aac6de0de13bf33b73c9e4a753b39483313
Mingda Rui <mingdarui@gmail.com>
2022-03-27 23:12:48 +0100

    LC0142 Binary Tree Maximum Path Sum: Iterative sulotion, very slow

--- src/main/java/pers/mingda/leetcode/LC0124BinaryTreeMaximumPathSum.java
+++ src/main/java/pers/mingda/leetcode/LC0124BinaryTreeMaximumPathSum.java
@@ -1,5 +1,11 @@
 package pers.mingda.leetcode;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+
 public class LC0124BinaryTreeMaximumPathSum {
     public int maxPathSum(TreeNode root) {
         int[] maxHolder = new int[]{Integer.MIN_VALUE};
@@ -17,5 +23,32 @@ public class LC0124BinaryTreeMaximumPathSum {
         int sum = left + root.val + right;
         maxHolder[0] = Math.max(maxHolder[0], sum);
         return Math.max(left, right) + root.val;
+    }
+
+    public int maxPathSumIterative(TreeNode root) {
+        int max = Integer.MIN_VALUE;
+        Map<TreeNode, Integer> map = new HashMap<>();
+        Stack<TreeNode> stack = new Stack<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                stack.push(node);
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            int left = node.left == null ? 0 : map.get(node.left);
+            int right = node.right == null ? 0 : map.get(node.right);
+            int currentSum = Math.max(left, right) + node.val;
+            map.put(node, Math.max(currentSum, 0));
+            max = Math.max(max, left + right + node.val);
+        }
+
+        return max;
     }
 }
