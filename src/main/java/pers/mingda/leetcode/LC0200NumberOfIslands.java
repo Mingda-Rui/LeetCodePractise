86601a07f5d8fc72c9c6fdbeb7ced02f3acdd26b
Mingda Rui <mingdarui@gmail.com>
2022-03-27 16:08:03 +0100

    LC0200 Number of Islands: BFS solution

--- src/main/java/pers/mingda/leetcode/LC0200NumberOfIslands.java
+++ src/main/java/pers/mingda/leetcode/LC0200NumberOfIslands.java
@@ -1,5 +1,8 @@
 package pers.mingda.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LC0200NumberOfIslands {
     public int numIslandsDfs(char[][] grid) {
         int counter = 0;
@@ -29,5 +32,34 @@ public class LC0200NumberOfIslands {
         exploreIsland(grid, x, y - 1, visited);
         exploreIsland(grid, x, y + 1, visited);
         return true;
+    }
+
+    public int numIslandsBfs(char[][] grid) {
+        int counter = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    queue.add(new int[]{i, j});
+                    while (!queue.isEmpty()) {
+                        int[] coord = queue.remove();
+                        int x = coord[0];
+                        int y = coord[1];
+                        if (!visited[x][y] && grid[x][y] == '1') {
+                            visited[x][y] = true;
+                            queue.add(new int[]{x + 1, y});
+                            queue.add(new int[]{x - 1, y});
+                            queue.add(new int[]{x, y + 1});
+                            queue.add(new int[]{x, y - 1});
+                        }
+                    }
+                    counter++;
+                }
+            }
+        }
+
+        return counter;
     }
 }
