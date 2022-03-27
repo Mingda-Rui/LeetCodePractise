b886490b7997d4d16c39a513c3e3be981ff420fd
Mingda Rui <mingdarui@gmail.com>
2022-03-27 21:02:36 +0100

    LC0100 Same Tree: iterative solution

--- src/main/java/pers/mingda/leetcode/LC0100SameTree.java
+++ src/main/java/pers/mingda/leetcode/LC0100SameTree.java
@@ -1,5 +1,8 @@
 package pers.mingda.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LC0100SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null)
@@ -11,5 +14,28 @@ public class LC0100SameTree {
         isSame = isSame && isSameTree(p.left, q.left);
         isSame = isSame && isSameTree(p.right, q.right);
         return isSame;
+    }
+
+    public boolean isSameTreeIterative(TreeNode p, TreeNode q) {
+        Queue<TreeNode> pQueue = new LinkedList<>();
+        Queue<TreeNode> qQueue = new LinkedList<>();
+        pQueue.offer(p);
+        qQueue.offer(q);
+        while (!pQueue.isEmpty() && !qQueue.isEmpty()) {
+            p = pQueue.poll();
+            q = qQueue.poll();
+            if (p != null && q != null) {
+                if (p.val != q.val)
+                    return false;
+                pQueue.add(p.left);
+                qQueue.add(q.left);
+                pQueue.add(p.right);
+                qQueue.add(q.right);
+            } else if (p != q) {
+                return false;
+            }
+        }
+
+        return pQueue.isEmpty() && qQueue.isEmpty();
     }
 }
