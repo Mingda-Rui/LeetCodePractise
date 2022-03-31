327ac2e509bc076b77408f8e70ff4c52a27e89d5
Mingda Rui <mingdarui@gmail.com>
2022-03-31 20:57:19 +0100

    LC0572 Subtree of Another Tree: preorder and string.contains() solution

--- src/main/java/pers/mingda/leetcode/LC0572SubtreeOfAnotherTree.java
+++ src/main/java/pers/mingda/leetcode/LC0572SubtreeOfAnotherTree.java
@@ -17,4 +17,22 @@ public class LC0572SubtreeOfAnotherTree {
                 && isSameTree(root.left, subRoot.left)
                 && isSameTree(root.right, subRoot.right);
     }
+
+    public boolean isSubtreePreorder(TreeNode root, TreeNode subRoot) {
+        StringBuilder sbRoot = new StringBuilder(",");
+        getStringPreOrder(root, sbRoot);
+        StringBuilder sbSubRoot = new StringBuilder(",");
+        getStringPreOrder(subRoot, sbSubRoot);
+        return sbRoot.toString().contains(sbSubRoot.toString());
+    }
+
+    private void getStringPreOrder(TreeNode root, StringBuilder sb) {
+        if (root == null)
+            sb.append("#,");
+        else {
+            sb.append(root.val).append(",");
+            getStringPreOrder(root.left, sb);
+            getStringPreOrder(root.right, sb);
+        }
+    }
 }
