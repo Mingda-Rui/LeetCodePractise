6d8483538d25a4fb6b06bb0d39098159f7626c51
Mingda Rui <mingdarui@gmail.com>
2022-04-05 21:30:30 +0100

    LC0230 Kth Smallest Element in A BST: Iterative solution

--- src/main/java/pers/mingda/leetcode/LC0230KthSmallestElementInABst.java
+++ src/main/java/pers/mingda/leetcode/LC0230KthSmallestElementInABst.java
@@ -1,5 +1,7 @@
 package pers.mingda.leetcode;
 
+import java.util.Stack;
+
 public class LC0230KthSmallestElementInABst {
     public int kthSmallest(TreeNode root, int k) {
         int[] resultHolder = new int[]{-1};
@@ -14,5 +16,22 @@ public class LC0230KthSmallestElementInABst {
         if (k == 1)
             resultHolder[0] = root.val;
         return kthSmallestRecursive(root.right, k - 1, resultHolder);
+    }
+
+    public int KthSmallestIterative(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                k--;
+                if (k == 0)
+                    return root.val;
+                root = root.right;
+            }
+        }
+        return -1;
     }
 }
