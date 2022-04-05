c6f137dc33a14f387ba00ea6c9107437b9e20964
Mingda Rui <mingdarui@gmail.com>
2022-04-05 21:53:31 +0100

    LC0235 Lowest Common Ancestor of a Binary Search Tree: one method solution, also works on a non-BST

---
+++ src/main/java/pers/mingda/leetcode/LC0235LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,31 @@
+package pers.mingda.leetcode;
+
+public class LC0235LowestCommonAncestorOfABinarySearchTree {
+    public TreeNode lowestCommonAncestorNonBst(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)
+            return null;
+
+        boolean rootEquals = root == p || root == q;
+        TreeNode left = lowestCommonAncestorNonBst(root.left, p, q);
+        if (left != null && left != p && left != q)
+            return left;
+        if (rootEquals && left != null) {
+            return root;
+        }
+        TreeNode right = lowestCommonAncestorNonBst(root.right, p, q);
+        if (right != null && right != p && right != q)
+            return right;
+        if (rootEquals && right != null) {
+            return root;
+        }
+        if (left != null && right != null)
+            return root;
+
+        if (p == root || p == left || p == right)
+            return p;
+        if (q == root || q == left || q == right)
+            return q;
+
+        return null;
+    }
+}
