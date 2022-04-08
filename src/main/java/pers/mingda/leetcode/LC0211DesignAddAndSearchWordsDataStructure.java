6246332f0544c0207dabe1b9bfe2b24286277ab7
Mingda Rui <mingdarui@gmail.com>
2022-04-08 22:02:02 +0100

    LC0211 Design Add and Search Words Data Structure

---
+++ src/main/java/pers/mingda/leetcode/LC0211DesignAddAndSearchWordsDataStructure.java
@@ -0,0 +1,67 @@
+package pers.mingda.leetcode;
+
+public class LC0211DesignAddAndSearchWordsDataStructure {
+
+}
+
+class WordDictionary {
+
+    WordDictionary[] record;
+    boolean isEnd;
+
+    public WordDictionary() {
+        this(false);
+    }
+
+    private WordDictionary(boolean isEnd) {
+        this.record = new WordDictionary[128];
+        this.isEnd = isEnd;
+    }
+
+    public void addWord(String word) {
+        addWord(word, 0);
+    }
+
+    private void addWord(String word, int index) {
+        if (index < word.length()) {
+            char c = word.charAt(index);
+            if (record[c] == null)
+                record[c] = new WordDictionary();
+            if (index == word.length() - 1)
+                record[c].isEnd = true;
+            record[c].addWord(word, index + 1);
+        }
+    }
+
+    public boolean search(String word) {
+        return search(word, 0);
+    }
+
+    private boolean search(String word, int index) {
+        if (index == word.length())
+            return false;
+        char c = word.charAt(index);
+        if (c == '.') {
+            for (WordDictionary wd: record) {
+                if (wd != null) {
+                    if (index == word.length() - 1 && wd.isEnd)
+                        return true;
+                    if (wd.search(word, index + 1))
+                        return true;
+                }
+            }
+        }
+        if (record[c] == null)
+            return false;
+        if (index == word.length() - 1)
+            return record[c].isEnd;
+        return record[c].search(word, index + 1);
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
