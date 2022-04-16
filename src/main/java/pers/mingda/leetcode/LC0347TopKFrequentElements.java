6fffd9eae90f8257b3198d81ae44b5f90aba31ca
Mingda Rui <mingdarui@gmail.com>
2022-04-17 00:19:28 +0100

    LC0347 Top K Frequent Elements: Pojo with bucket sort

--- src/main/java/pers/mingda/leetcode/LC0347TopKFrequentElements.java
+++ src/main/java/pers/mingda/leetcode/LC0347TopKFrequentElements.java
@@ -1,7 +1,10 @@
 package pers.mingda.leetcode;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -26,5 +29,45 @@ public class LC0347TopKFrequentElements {
             result[i] = queue.poll().getKey();
 
         return result;
+    }
+
+    public int[] topKFrequentBucketSortWithPojo(int[] nums, int k) {
+        List<Integer> result = new LinkedList<>();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int num: nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        int[] offsetNumCounts = new int[max - min + 1];
+        int mostCounts = 0;
+        for (int num: nums) {
+            int offsetNum = num - min;
+            offsetNumCounts[offsetNum]++;
+            mostCounts = Math.max(mostCounts, offsetNumCounts[offsetNum]);
+        }
+
+        List<List<Integer>> bucket = new ArrayList<>();
+        for (int i = 0;  i < mostCounts + 1; i++)
+            bucket.add(null);
+
+        for (int offsetNum = 0; offsetNum < offsetNumCounts.length; offsetNum++) {
+            int count = offsetNumCounts[offsetNum];
+            if (count != 0) {
+                if (bucket.get(count) == null)
+                    bucket.set(count, new LinkedList<>());
+                int num = offsetNum + min;
+                bucket.get(count).add(num);
+            }
+        }
+
+        for (int count = bucket.size() - 1; count >= 0 && result.size() < k; count--) {
+            List<Integer> bigNums = bucket.get(count);
+            if (bigNums != null)
+                result.addAll(bigNums);
+        }
+
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
