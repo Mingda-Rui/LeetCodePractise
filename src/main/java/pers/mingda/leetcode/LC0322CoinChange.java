69fa00c2c86749efd7de330ff4036d31564efd67
Mingda Rui <mingdarui@gmail.com>
2022-05-13 23:59:22 +0100

    LC0322 Coin Change: refactor

--- src/main/java/pers/mingda/leetcode/LC0322CoinChange.java
+++ src/main/java/pers/mingda/leetcode/LC0322CoinChange.java
@@ -2,22 +2,16 @@ package pers.mingda.leetcode;
 
 public class LC0322CoinChange {
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0)
-            return amount;
         int[] record = new int[amount + 1];
-        for (int i = 0; i < amount + 1; i++) {
-            int fewest = Integer.MAX_VALUE;
-            boolean found = false;
+        record[0] = 0;
+        for (int i = 1; i < amount + 1; i++) {
+            record[i] = -1;
             for (int coin: coins) {
-                if (coin == i) {
-                    fewest = 1;
-                    found = true;
-                } if (i - coin > 0 && record[i - coin] > 0) {
-                    fewest = Math.min(fewest, record[i - coin] + 1);
-                    found = true;
+                if (i - coin >= 0 && record[i - coin] >= 0) {
+                    int fewest = record[i] > 0 ? record[i] : Integer.MAX_VALUE;
+                    record[i] = Math.min(fewest, record[i - coin] + 1);
                 }
             }
-            record[i] = found ? fewest : -1;
         }
         return record[amount];
     }
