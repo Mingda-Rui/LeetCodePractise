7fa45e81b880c4740d2f0b231465ed29513f5215
Mingda Rui <mingdarui@gmail.com>
2022-05-14 18:39:47 +0100

    LC0198 House Robber: no array solution

--- src/main/java/pers/mingda/leetcode/LC0198HouseRobber.java
+++ src/main/java/pers/mingda/leetcode/LC0198HouseRobber.java
@@ -13,4 +13,18 @@ public class LC0198HouseRobber {
         }
         return Math.max(sum[nums.length - 1], sum[nums.length - 2]);
     }
+
+    public int robNoArray(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+        int prev1 = nums[0];
+        int prev2 = nums[1];
+        for (int i = 2; i < nums.length; i++) {
+            int current = nums[i];
+            int max = Math.max(prev2, prev1 + current);
+            prev1 = prev2;
+            prev2 = max;
+        }
+        return prev2;
+    }
 }
