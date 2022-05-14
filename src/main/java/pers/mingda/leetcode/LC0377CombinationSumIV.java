b63e5e6951c259312b575e8862a4e5bba1938ca1
Mingda Rui <mingdarui@gmail.com>
2022-05-14 20:31:34 +0100

    LC0377 Comnication Sum IV: refactor

--- src/main/java/pers/mingda/leetcode/LC0377CombinationSumIV.java
+++ src/main/java/pers/mingda/leetcode/LC0377CombinationSumIV.java
@@ -3,14 +3,10 @@ package pers.mingda.leetcode;
 public class LC0377CombinationSumIV {
     public int combinationSum4(int[] nums, int target) {
         int[] record = new int[target + 1];
-        for (int i = 1; i < target + 1; i++) {
-            for (int num: nums) {
-                if (i - num == 0)
-                    record[i] += 1;
-                else if (i - num > 0)
-                    record[i] += record[i - num];
-            }
-        }
+        for (int i = 1; i < target + 1; i++)
+            for (int num: nums)
+                if (i - num >= 0)
+                    record[i] += (i - num == 0) ? 1 : record[i - num];
         return record[target];
     }
 }
