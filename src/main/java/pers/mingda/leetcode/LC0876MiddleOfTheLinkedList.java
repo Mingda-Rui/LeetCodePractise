a11e1121cdaf86b027f39887c7f6c966e8c5620d
Mingda Rui <mingdarui@gmail.com>
2022-05-14 22:59:47 +0100

    LC0876 Middle of the Linked List

---
+++ src/main/java/pers/mingda/leetcode/LC0876MiddleOfTheLinkedList.java
@@ -0,0 +1,13 @@
+package pers.mingda.leetcode;
+
+public class LC0876MiddleOfTheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
