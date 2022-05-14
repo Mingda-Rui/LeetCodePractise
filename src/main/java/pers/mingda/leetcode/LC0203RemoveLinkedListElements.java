17b363d85187af026fef9c0c262e0aa519ee0140
Mingda Rui <mingdarui@gmail.com>
2022-05-14 21:59:15 +0100

    LC0203 Remove Linked List Elements

---
+++ src/main/java/pers/mingda/leetcode/LC0203RemoveLinkedListElements.java
@@ -0,0 +1,24 @@
+package pers.mingda.leetcode;
+
+public class LC0203RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null)
+            return head;
+        ListNode newHead = head;
+        while (head != null && head.next != null) {
+            if (head.val == val) {
+                ListNode tmp = head;
+                head = head.next;
+                tmp.next = null;
+                newHead = head;
+            } else if (head.next.val == val) {
+                ListNode tmp = head.next;
+                head.next = tmp.next;
+                tmp.next = null;
+            } else {
+                head = head.next;
+            }
+        }
+        return newHead.val == val ? null : newHead;
+    }
+}
