5cc924eb7b89f02ae2fb07d13a610caf513cc724
Mingda Rui <mingdarui@gmail.com>
2022-05-14 11:00:47 +0100

    LC0062 Unique Paths

---
+++ src/main/java/pers/mingda/leetcode/LC0062UniquePaths.java
@@ -0,0 +1,16 @@
+package pers.mingda.leetcode;
+
+public class LC0062UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] record = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int top = i - 1 < 0 ? 0 : record[i - 1][j];
+                int left = j - 1 < 0 ? 0 : record[i][j - 1];
+                int total = Math.max(top + left, 1);
+                record[i][j] = total;
+            }
+        }
+        return record[m - 1][n - 1];
+    }
+}
