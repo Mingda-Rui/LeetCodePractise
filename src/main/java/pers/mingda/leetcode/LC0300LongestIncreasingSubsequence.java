2a057caa7f97ef7a63dbb0402a42fd1123ef0939
Mingda Rui <mingdarui@gmail.com>
2022-05-15 18:51:22 +0100

    LC0300 Longest Increasing Subsequence: binary search

--- src/main/java/pers/mingda/leetcode/LC0300LongestIncreasingSubsequence.java
+++ src/main/java/pers/mingda/leetcode/LC0300LongestIncreasingSubsequence.java
@@ -1,5 +1,7 @@
 package pers.mingda.leetcode;
 
+import java.util.Arrays;
+
 public class LC0300LongestIncreasingSubsequence {
     public int lengthOfLIS(int[] nums) {
         int[] record = new int[nums.length];
@@ -13,5 +15,24 @@ public class LC0300LongestIncreasingSubsequence {
             globalMax = Math.max(globalMax, record[i]);
         }
         return globalMax;
+    }
+
+    public int lengthOfLISBinarySearch(int[] nums) {
+        int[] record = new int[nums.length];
+        int counter = 0;
+        record[counter] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int val = nums[i];
+            int index = findReplaceIndex(record, 0, counter + 1, val);
+            counter = Math.max(counter, index);
+            record[index] = val;
+        }
+
+        return counter + 1;
+    }
+
+    private int findReplaceIndex(int[] record, int start, int finish, int val) {
+        int index = Arrays.binarySearch(record, start, finish, val);
+        return index >= 0 ? index : - index - 1;
     }
 }
