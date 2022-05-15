c4ee010968d5e00042a43bf80031120e7267965d
Mingda Rui <mingdarui@gmail.com>
2022-05-15 22:09:39 +0100

    LC0082 Remove Duplicates from Sorted List II: recursive solution, refactored

--- src/main/java/pers/mingda/leetcode/LC0082RemoveDuplicatesFromSortedListII.java
+++ src/main/java/pers/mingda/leetcode/LC0082RemoveDuplicatesFromSortedListII.java
@@ -18,26 +18,16 @@ public class LC0082RemoveDuplicatesFromSortedListII {
     }
 
     public ListNode deleteDuplicatesRecursive(ListNode head) {
-        if (head == null)
+        if (head == null || head.next == null)
             return head;
-        return deleteDuplicates(head, head.next);
-
-    }
-
-    private ListNode deleteDuplicates(ListNode parent, ListNode head) {
-        if (head == null)
-            return parent;
-        if (parent.val == head.val) {
-            if (head.next == null)
-                return deleteDuplicates(head.next, null);
-            else if (head.next.val == head.val) {
-                return deleteDuplicates(parent, head.next);
-            } else {
-                return deleteDuplicates(head.next, head.next.next);
-            }
+        if (head.val == head.next.val) {
+            if (head.next.next != null && head.next.next.val == head.val)
+                head = deleteDuplicates(head.next);
+            else
+                head = deleteDuplicates(head.next.next);
         } else {
-            parent.next = deleteDuplicates(head, head.next);
-            return parent;
+            head.next = deleteDuplicates(head.next);
         }
+        return head;
     }
 }
