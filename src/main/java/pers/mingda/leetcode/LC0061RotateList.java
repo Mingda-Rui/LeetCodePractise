bf80b7b6735529700c053f0d003b36e912da317f
Mingda Rui <mingdarui@gmail.com>
2022-05-16 21:49:15 +0100

    LC0061 Rotate List: refactor

--- src/main/java/pers/mingda/leetcode/LC0061RotateList.java
+++ src/main/java/pers/mingda/leetcode/LC0061RotateList.java
@@ -4,32 +4,22 @@ public class LC0061RotateList {
     public ListNode rotateRight(ListNode head, int k) {
         if (k == 0 || head == null)
             return head;
-        int length = getLength(head);
+        int length = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            length++;
+            tail = tail.next;
+        }
+        tail.next = head;
         int rotate = k % length;
-        if (rotate == 0)
-            return head;
         int splitIndex = length - rotate - 1;
         ListNode newTail = head;
 
         for (int i = 0; i < splitIndex; i++)
             newTail = newTail.next;
-        ListNode newHead = newTail.next;
+        head = newTail.next;
         newTail.next = null;
 
-        ListNode tail = newHead;
-        while (tail.next != null)
-            tail = tail.next;
-
-        tail.next = head;
-        return newHead;
-    }
-
-    private int getLength(ListNode head) {
-        int counter = 0;
-        while (head != null) {
-            counter++;
-            head = head.next;
-        }
-        return counter;
+        return head;
     }
 }
