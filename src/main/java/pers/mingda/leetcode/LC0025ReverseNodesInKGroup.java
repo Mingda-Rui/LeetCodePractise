4b61e830be6cfb3df5d511f1a128faf833c265ca
Mingda Rui <mingdarui@gmail.com>
2022-05-17 23:13:52 +0100

    LC0025 Reverse Nodes in k-Group: recursive solution

---
+++ src/main/java/pers/mingda/leetcode/LC0025ReverseNodesInKGroup.java
@@ -0,0 +1,34 @@
+package pers.mingda.leetcode;
+
+public class LC0025ReverseNodesInKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        int counter = 1;
+        ListNode h1 = head;
+        ListNode t1 = head;
+
+        while (counter < k && t1 != null) {
+            t1 = t1.next;
+            counter++;
+        }
+
+        if (counter < k || t1 == null)
+            return h1;
+
+        ListNode h2 = t1.next;
+        t1.next = null;
+
+        reverse(h1);
+        h1.next = reverseKGroup(h2, k);
+        return t1;
+    }
+
+    private void reverse(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode oldHead = head;
+            head = head.next;
+            oldHead.next = newHead;
+            newHead = oldHead;
+        }
+    }
+}
