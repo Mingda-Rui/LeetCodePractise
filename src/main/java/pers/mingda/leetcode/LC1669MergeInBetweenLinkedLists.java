fbd5998589da39bde1ccc6d568304dea57944470
Mingda Rui <mingdarui@gmail.com>
2022-05-20 19:47:06 +0100

    LC1669 Merge In Between Linked Lists: refactored

--- src/main/java/pers/mingda/leetcode/LC1669MergeInBetweenLinkedLists.java
+++ src/main/java/pers/mingda/leetcode/LC1669MergeInBetweenLinkedLists.java
@@ -2,23 +2,19 @@ package pers.mingda.leetcode;
 
 public class LC1669MergeInBetweenLinkedLists {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-        int counter = 0;
-        ListNode ite = list1;
-        while (ite != null) {
-            if (counter + 1 == a) {
-                ListNode aNode = ite.next;
-                ite.next = list2;
-                ite = aNode;
-            } else if (counter == b) {
-                while (list2 != null && list2.next != null)
-                    list2 = list2.next;
-                list2.next = ite.next;
-                ite.next = null;
-                return list1;
-            } else
-                ite = ite.next;
-            counter++;
-        }
+        ListNode beforeANode = list1;
+        for (int i = 0; i < a - 1; i++)
+            beforeANode = beforeANode.next;
+
+        ListNode bNode = beforeANode;
+        for (int i = a - 1; i < b; i++)
+            bNode = bNode.next;
+
+        beforeANode.next = list2;
+        while (list2.next != null)
+            list2 = list2.next;
+        list2.next = bNode.next;
+        bNode.next = null;
         return list1;
     }
 }
