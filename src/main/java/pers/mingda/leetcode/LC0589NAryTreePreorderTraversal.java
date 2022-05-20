baeb13b286f0c5fac630b1b115e378c69d1c7128
Mingda Rui <mingdarui@gmail.com>
2022-05-20 20:00:42 +0100

    LC0589 N-ary Tree Preorder Traversal: stack solution

---
+++ src/main/java/pers/mingda/leetcode/LC0589NAryTreePreorderTraversal.java
@@ -0,0 +1,42 @@
+package pers.mingda.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class LC0589NAryTreePreorderTraversal {
+    public List<Integer> preorder(Node root) {
+        List<Integer> result = new LinkedList<>();
+        if (root == null)
+            return result;
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node current = stack.pop();
+            result.add(current.val);
+
+            List<Node> children = current.children;
+            for (int i = children.size() - 1; i >= 0; i--)
+                stack.push(children.get(i));
+
+        }
+        return result;
+    }
+}
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
