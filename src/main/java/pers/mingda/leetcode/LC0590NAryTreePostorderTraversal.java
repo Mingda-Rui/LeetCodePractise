f52b2ae367811820e4467ecd54b8e1e0adc335f0
Mingda Rui <mingdarui@gmail.com>
2022-05-20 20:49:06 +0100

    LC0590 N-ary Tree Postorder Traversal: add Node class

--- src/main/java/pers/mingda/leetcode/LC0590NAryTreePostorderTraversal.java
+++ src/main/java/pers/mingda/leetcode/LC0590NAryTreePostorderTraversal.java
@@ -41,3 +41,19 @@ public class LC0590NAryTreePostorderTraversal {
         return result;
     }
 }
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
