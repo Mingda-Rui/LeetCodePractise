db29e1cad0c4ad39e772437192af278a450853fe
Mingda Rui <mingdarui@gmail.com>
2022-05-21 17:17:27 +0100

    LC0114 Flatten Binary Tree to Linked List: iterative solution

--- src/main/java/pers/mingda/leetcode/LC0114FlattenBinaryTreeToLinkedList.java
+++ src/main/java/pers/mingda/leetcode/LC0114FlattenBinaryTreeToLinkedList.java
@@ -1,5 +1,7 @@
 package pers.mingda.leetcode;
 
+import java.util.Stack;
+
 public class LC0114FlattenBinaryTreeToLinkedList {
     public TreeNode flattenRecursive(TreeNode root) {
         if (root == null || root.left == null && root.right == null)
@@ -15,5 +17,26 @@ public class LC0114FlattenBinaryTreeToLinkedList {
                 return leftTail;
         }
         return flattenRecursive(rootRight);
+    }
+
+    public void flattenIterative(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null)
+            stack.push(root);
+        TreeNode dummyHead = new TreeNode(-1, null, root);
+        root = dummyHead;
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+            root.right = current;
+            if (current.right != null) {
+                stack.push(current.right);
+                current.right = null;
+            }
+            if (current.left != null) {
+                stack.push(current.left);
+                current.left = null;
+            }
+            root = current;
+        }
     }
 }
