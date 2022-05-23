a2a517a41d9c24bd0eb72e9c88cd5ee4b072ad31
Mingda Rui <mingdarui@gmail.com>
2022-05-23 21:44:33 +0100

    LC0222 Count Complete Tree Nodes

---
+++ src/main/java/pers/mingda/leetcode/LC0222CountCompleteTreeNodes.java
@@ -0,0 +1,41 @@
+package pers.mingda.leetcode;
+
+public class LC0222CountCompleteTreeNodes {
+    public int countNodes(TreeNode root) {
+        if (root == null)
+            return 0;
+        int depth = 0;
+        TreeNode pointer = root;
+        while (pointer != null) {
+            pointer = pointer.left;
+            depth++;
+        }
+
+        int[] pow = new int[depth];
+        pow[0] = 1;
+
+        for (int i = 1; i < pow.length; i++)
+            pow[i] = pow[i - 1] * 2;
+        int currentCount = 1;
+        for (int i = 0; i < depth - 1; i++)
+            currentCount += pow[i];
+
+        return countNodes(root, depth, 0, currentCount, pow);
+    }
+
+    private int countNodes(TreeNode root, int depth, int currentD, int currentCount, int[] pow) {
+        if (root == null)
+            return 0;
+        if (depth == currentD + 1)
+            return currentCount;
+
+        int offset = pow[depth - currentD - 2];
+        int rightCount = countNodes(root.right, depth, currentD + 1, currentCount + offset, pow);
+        if (rightCount != 0)
+            return rightCount;
+        int leftCount = countNodes(root.left, depth, currentD + 1, currentCount, pow);
+        if (leftCount != 0)
+            return leftCount;
+        return 0;
+    }
+}
