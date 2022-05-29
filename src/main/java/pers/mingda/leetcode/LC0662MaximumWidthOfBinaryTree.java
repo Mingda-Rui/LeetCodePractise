00784602bc7a0f1b6a32ccf5856dfc1489df4980
Mingda Rui <mingdarui@gmail.com>
2022-05-29 18:12:19 +0100

    LC0662 Maximum Width of Binary Tree: recursive solutin refactored

--- src/main/java/pers/mingda/leetcode/LC0662MaximumWidthOfBinaryTree.java
+++ src/main/java/pers/mingda/leetcode/LC0662MaximumWidthOfBinaryTree.java
@@ -5,32 +5,29 @@ import java.util.List;
 
 public class LC0662MaximumWidthOfBinaryTree {
     public int widthOfBinaryTree(TreeNode root) {
-        List<Long> min = new ArrayList<>();
-        List<Long> max = new ArrayList<>();
-        widthOfBinaryTree(root, 1, 0, min, max);
-        long maxWid = Long.MIN_VALUE;
+        List<Integer> min = new ArrayList<>();
+        List<Integer> width = new ArrayList<>();
+        widthOfBinaryTree(root, 1, 0, min, width);
+        int maxWid = Integer.MIN_VALUE;
         for (int i = 0; i < min.size(); i++) {
-            long currentWid = max.get(i) - min.get(i) + 1;
+            int currentWid = width.get(i);
             maxWid = Math.max(maxWid, currentWid);
         }
-        return (int) maxWid;
+        return maxWid;
     }
 
-    private void widthOfBinaryTree(TreeNode root, long num, int level, List<Long> min, List<Long> max) {
+    private void widthOfBinaryTree(TreeNode root, int num, int level, List<Integer> min, List<Integer> width) {
         if (root == null)
             return;
         if (level == min.size()) {
             min.add(num);
-            max.add(num);
+            width.add(1);
         } else {
-            long currentMin = min.get(level);
-            long newMin = Math.min(currentMin, num);
-            min.set(level, newMin);
-            long currentMax = max.get(level);
-            long newMax = Math.max(currentMax, num);
-            max.set(level, newMax);
+            int currentWidth = width.get(level);
+            int maxWidth = Math.max(currentWidth, num - min.get(level) + 1);
+            width.set(level, maxWidth);
         }
-        widthOfBinaryTree(root.left, num * 2, level + 1, min, max);
-        widthOfBinaryTree(root.right, num * 2 + 1, level + 1, min, max);
+        widthOfBinaryTree(root.left, num * 2, level + 1, min, width);
+        widthOfBinaryTree(root.right, num * 2 + 1, level + 1, min, width);
     }
 }
