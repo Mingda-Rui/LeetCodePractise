537f274801da0db153f5b133b462167e7a589f44
Mingda Rui <mingdarui@gmail.com>
2022-05-29 20:47:05 +0100

    LC0662 Maximum Width of Binary Tree: iterative solution

--- src/main/java/pers/mingda/leetcode/LC0662MaximumWidthOfBinaryTree.java
+++ src/main/java/pers/mingda/leetcode/LC0662MaximumWidthOfBinaryTree.java
@@ -1,7 +1,9 @@
 package pers.mingda.leetcode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class LC0662MaximumWidthOfBinaryTree {
     public int widthOfBinaryTree(TreeNode root) {
@@ -20,5 +22,33 @@ public class LC0662MaximumWidthOfBinaryTree {
         int childWidth = Math.max(leftWidth, rightWidth);
         int currentWidth = num - min.get(level) + 1;
         return Math.max(currentWidth, childWidth);
+    }
+
+    public int widthOfBinaryTreeIterative(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<Integer> num = new LinkedList<>();
+        queue.add(root);
+        num.add(1);
+        int maxWidth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int start = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.remove();
+                int n = num.remove();
+                if (i == 0)
+                    start = n;
+                maxWidth = Math.max(maxWidth, node.val - start + 1);
+                if (node.left != null) {
+                    queue.add(node.left);
+                    num.add(n * 2);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                    num.add(n * 2 + 1);
+                }
+            }
+        }
+        return maxWidth;
     }
 }
