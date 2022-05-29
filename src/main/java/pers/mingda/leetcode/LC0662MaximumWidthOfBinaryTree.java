ba3797a3289808d9970b0159c1a397910f6bbae0
Mingda Rui <mingdarui@gmail.com>
2022-05-29 20:31:32 +0100

    LC0662 Maximum Width of Binary Tree: recursive solution, one list

--- src/main/java/pers/mingda/leetcode/LC0662MaximumWidthOfBinaryTree.java
+++ src/main/java/pers/mingda/leetcode/LC0662MaximumWidthOfBinaryTree.java
@@ -6,23 +6,19 @@ import java.util.List;
 public class LC0662MaximumWidthOfBinaryTree {
     public int widthOfBinaryTree(TreeNode root) {
         List<Integer> min = new ArrayList<>();
-        List<Integer> width = new ArrayList<>();
-        return widthOfBinaryTree(root, 1, 0, min, width);
+        return widthOfBinaryTree(root, 1, 0, min);
     }
 
-    private int widthOfBinaryTree(TreeNode root, int num, int level, List<Integer> min, List<Integer> width) {
+    private int widthOfBinaryTree(TreeNode root, int num, int level, List<Integer> min) {
         if (root == null)
             return 0;
-        if (level == min.size()) {
+        if (level == min.size())
             min.add(num);
-            width.add(1);
-        } else {
-            int maxWidth = num - min.get(level) + 1;
-            width.set(level, maxWidth);
-        }
-        int leftWidth = widthOfBinaryTree(root.left, num * 2, level + 1, min, width);
-        int rightWidth = widthOfBinaryTree(root.right, num * 2 + 1, level + 1, min, width);
+
+        int leftWidth = widthOfBinaryTree(root.left, num * 2, level + 1, min);
+        int rightWidth = widthOfBinaryTree(root.right, num * 2 + 1, level + 1, min);
         int childWidth = Math.max(leftWidth, rightWidth);
-        return Math.max(width.get(level), childWidth);
+        int currentWidth = num - min.get(level) + 1;
+        return Math.max(currentWidth, childWidth);
     }
 }
