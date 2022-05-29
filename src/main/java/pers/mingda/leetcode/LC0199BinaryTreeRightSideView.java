3965b94a3e37e7b8ba9a8a55bc4ffe338fe20f5a
Mingda Rui <mingdarui@gmail.com>
2022-05-29 15:38:50 +0100

    LC0199 Bianry Tree Right Side View: iterative solution

--- src/main/java/pers/mingda/leetcode/LC0199BinaryTreeRightSideView.java
+++ src/main/java/pers/mingda/leetcode/LC0199BinaryTreeRightSideView.java
@@ -2,6 +2,7 @@ package pers.mingda.leetcode;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class LC0199BinaryTreeRightSideView {
     public List<Integer> rightSideView(TreeNode root) {
@@ -15,6 +16,29 @@ public class LC0199BinaryTreeRightSideView {
             result.add(root.val);
         rightSideView(root.right, level + 1, result);
         rightSideView(root.left, level + 1, result);
+        return result;
+    }
+
+    public List<Integer> rightSideViewIterative(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if (root == null)
+            return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.remove();
+                if (level == result.size())
+                    result.add(node.val);
+                if (node.right != null)
+                    queue.add(node.right);
+                if (node.left != null)
+                    queue.add(node.left);
+            }
+            level++;
+        }
         return result;
     }
 }
