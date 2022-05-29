475df56847659c64f9576b6088ba581c27e4da32
Mingda Rui <mingdarui@gmail.com>
2022-05-29 22:44:39 +0100

    LC0129 Sum Root to Leaf N Numbers: iterative solution

--- src/main/java/pers/mingda/leetcode/LC0129SumRootToLeafNumbers.java
+++ src/main/java/pers/mingda/leetcode/LC0129SumRootToLeafNumbers.java
@@ -1,5 +1,7 @@
 package pers.mingda.leetcode;
 
+import java.util.Stack;
+
 public class LC0129SumRootToLeafNumbers {
     public int sumNumbers(TreeNode root) {
         return sumNumbers(root, 0);
@@ -14,5 +16,32 @@ public class LC0129SumRootToLeafNumbers {
         int leftSum = sumNumbers(root.left, sum);
         int rightSum = sumNumbers(root.right, sum);
         return leftSum + rightSum;
+    }
+
+    public int sumNumbersIterative(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        int sum = 0;
+        int current = 0;
+        TreeNode prev = null;
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                current = current * 10 + root.val;
+                root = root.left;
+            }
+            root = stack.peek();
+
+            if (root.right == null || root.right == prev) {
+                if (root.left == null && root.right == null)
+                    sum += current;
+                current /= 10;
+                prev = stack.pop();
+                root = null;
+            } else {
+                root = root.right;
+            }
+
+        }
+        return sum;
     }
 }
