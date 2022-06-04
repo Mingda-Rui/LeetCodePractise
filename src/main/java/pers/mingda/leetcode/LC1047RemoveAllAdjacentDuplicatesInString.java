57d0aaef8446fb05affaee1dd708f874e49aee1d
Mingda Rui <mingdarui@gmail.com>
2022-06-04 22:03:17 +0100

    LC1047 Remove All Adjacent Duplicates in String: Two pointers solution

--- src/main/java/pers/mingda/leetcode/LC1047RemoveAllAdjacentDuplicatesInString.java
+++ src/main/java/pers/mingda/leetcode/LC1047RemoveAllAdjacentDuplicatesInString.java
@@ -13,4 +13,28 @@ public class LC1047RemoveAllAdjacentDuplicatesInString {
         }
         return sb.toString();
     }
+
+    public String removeDuplicatesTwoPointers(String s) {
+        char[] arr = s.toCharArray();
+        int current = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[current] == arr[i]) {
+                if (current == 0) {
+                    if (i == arr.length - 1) {
+                        return "";
+                    } else {
+                        i++;
+                        arr[current] = arr[i];
+                    }
+                } else {
+                    current--;
+                }
+            } else {
+                current++;
+                arr[current] = arr[i];
+            }
+        }
+
+        return String.valueOf(arr, 0, current + 1);
+    }
 }
