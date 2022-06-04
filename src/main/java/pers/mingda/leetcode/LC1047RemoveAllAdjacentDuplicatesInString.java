08555abbe0c49a6eae10c8c0d91a371853c05890
Mingda Rui <mingdarui@gmail.com>
2022-06-04 18:47:16 +0100

    LC1047 Remove All Adjacent Duplicates in String: StringBuilder solution

--- src/main/java/pers/mingda/leetcode/LC1047RemoveAllAdjacentDuplicatesInString.java
+++ src/main/java/pers/mingda/leetcode/LC1047RemoveAllAdjacentDuplicatesInString.java
@@ -1,17 +1,16 @@
 package pers.mingda.leetcode;
 
 public class LC1047RemoveAllAdjacentDuplicatesInString {
-    public String removeDuplicateIterative(String s) {
-        for (int i = 0; i < s.length() - 1; i++) {
-            char c = s.charAt(i);
-            char next = s.charAt(i + 1);
-            if (c == next) {
-                String head = s.substring(0, i);
-                String tail = i + 2 < s.length() ? s.substring(i + 2, s.length()) : "";
-                s = head + tail;
-                i = Math.max(-1, i - 2);
+    public String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c: s.toCharArray()) {
+            int tailIndex = sb.length() - 1;
+            if (tailIndex < 0 || sb.charAt(tailIndex) != c) {
+                sb.append(c);
+            } else {
+                sb.deleteCharAt(tailIndex);
             }
         }
-        return s;
+        return sb.toString();
     }
 }
