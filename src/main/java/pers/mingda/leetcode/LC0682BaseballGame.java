2f6c104ea08d56c4abb40f5076b4fd45a54643bc
Mingda Rui <mingdarui@gmail.com>
2022-06-10 23:12:08 +0100

    LC0682 Baseball Game: no stack

--- src/main/java/pers/mingda/leetcode/LC0682BaseballGame.java
+++ src/main/java/pers/mingda/leetcode/LC0682BaseballGame.java
@@ -30,4 +30,37 @@ public class LC0682BaseballGame {
             result += stack.pop();
         return result;
     }
+
+    public int calPointsNoStack(String[] ops) {
+        int[] record = new int[ops.length];
+        int pointer = -1;
+        int sum = 0;
+        for (String op: ops) {
+            switch (op) {
+                case "C" -> {
+                    sum -= record[pointer];
+                    pointer--;
+                }
+                case "D" -> {
+                    int val = record[pointer] * 2;
+                    pointer++;
+                    record[pointer] = val;
+                    sum += val;
+                }
+                case "+" -> {
+                    int first = record[pointer - 1];
+                    int val = first + record[pointer];
+                    pointer++;
+                    record[pointer] = val;
+                    sum += val;
+                }
+                default -> {
+                    pointer++;
+                    record[pointer] = Integer.parseInt(op);
+                    sum += record[pointer];
+                }
+            }
+        }
+        return sum;
+    }
 }
