3e687f288bc7324042e60365eec36972a188cc4a
Mingda Rui <mingdarui@gmail.com>
2022-06-15 19:48:38 +0100

    LC1971 Find If Path Exists in Graph: remove DFS solution as it uses untyped generic classes

--- src/main/java/pers/mingda/leetcode/LC1971FindIfPathExistsInGraph.java
+++ src/main/java/pers/mingda/leetcode/LC1971FindIfPathExistsInGraph.java
@@ -1,8 +1,5 @@
 package pers.mingda.leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class LC1971FindIfPathExistsInGraph {
 
     public boolean validPathUnionFind(int n, int[][] edges, int source, int destination) {
@@ -60,44 +57,5 @@ public class LC1971FindIfPathExistsInGraph {
         }
 
         return visited[destination];
-    }
-
-    // DFS
-    public boolean validPathMapSolution(int n, int[][] edges, int source, int destination) {
-        // Given a vertex, get all its neighbors.
-        // a table/array to record whether certain vertex is traversed.
-
-        Set<Integer>[] map = new Set[n];
-        for (int[] edge: edges) {
-            recordEdge(map, edge);
-        }
-        boolean[] traverseTable = new boolean[n];
-        return validPath(map, traverseTable, source, destination);
-    }
-
-    private void recordEdge(Set<Integer>[] map, int[] edge) {
-        int vertex1 = edge[0];
-        int vertex2 = edge[1];
-        if (map[vertex1] == null)
-            map[vertex1] = new HashSet<>();
-        map[vertex1].add(vertex2);
-
-        if (map[vertex2] == null)
-            map[vertex2] = new HashSet<>();
-        map[vertex2].add(vertex1);
-    }
-
-    private boolean validPath(Set<Integer>[] map, boolean[] traverseTable, int source, int destination) {
-        if (traverseTable[source])
-            return false;
-        if (source == destination)
-            return true;
-        traverseTable[source] = true;
-        for (int value: map[source]) {
-            if (validPath(map, traverseTable, value, destination)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
