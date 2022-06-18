8aec21b7fac7bf61a9fa0d6642b480cd4b069d57
Mingda Rui <mingdarui@gmail.com>
2022-06-18 17:39:44 +0100

    LC0907 Sum of Subarray Minimumsb: stack solution

---
+++ src/main/java/pers/mingda/leetcode/LC0907SumOfSubarrayMinimums.java
@@ -0,0 +1,27 @@
+package pers.mingda.leetcode;
+
+import java.util.Stack;
+
+public class LC0907SumOfSubarrayMinimums {
+    public int sumSubarrayMins(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        long result = 0;
+        int currentTotal = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int current = arr[i];
+            while (!stack.isEmpty() && arr[stack.peek()] > current) {
+                int prevIndex = stack.pop();
+                int prevPrevIndex = stack.isEmpty() ? -1 : stack.peek();
+                int count = prevIndex - prevPrevIndex;
+                currentTotal -= arr[prevIndex] * count;
+                currentTotal += current * count;
+            }
+            stack.push(i);
+            currentTotal += current;
+            result += currentTotal;
+        }
+        if (result < Integer.MAX_VALUE)
+            return (int) result;
+        return (int) (result % 1000000007);
+    }
+}
