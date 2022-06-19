6ab7b527c6d1ffbeee6ac75198b0ba163b695fd1
Mingda Rui <mingdarui@gmail.com>
2022-06-19 21:18:04 +0100

    LC0402 Remove K Digits: replace stack with array, faster solution

--- src/main/java/pers/mingda/leetcode/LC0402RemoveKDigits.java
+++ src/main/java/pers/mingda/leetcode/LC0402RemoveKDigits.java
@@ -1,37 +1,31 @@
 package pers.mingda.leetcode;
 
-import java.util.Stack;
-
 public class LC0402RemoveKDigits {
     public String removeKdigits(String num, int k) {
         if (k >= num.length())
             return "0";
 
-        Stack<Character> stack = new Stack<>();
+        char[] stack = new char[num.length()];
+        int stackIndex = -1;
         int index = 0;
         while (index < num.length()) {
             char c = num.charAt(index);
-            if (k > 0 && !stack.isEmpty() && stack.peek() > c) {
-                stack.pop();
+            if (k > 0 && stackIndex >= 0 && stack[stackIndex] > c) {
+                stackIndex--;
                 k--;
             } else {
-                if (stack.size() == 1 && stack.peek() == '0')
-                    stack.pop();
-                stack.push(c);
+                if (stackIndex == 0 && stack[0] == '0')
+                    stackIndex--;
+                stackIndex++;
+                stack[stackIndex] = c;
                 index++;
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            char c = stack.pop();
-            if (k == 0)
-                sb.append(c);
-            else
-                k--;
-        }
+        if (stackIndex + 1 <= k)
+            return "0";
 
-        return sb.isEmpty() ? "0" : sb.reverse().toString();
+        return String.valueOf(stack, 0, stackIndex - k + 1);
     }
 }
 
