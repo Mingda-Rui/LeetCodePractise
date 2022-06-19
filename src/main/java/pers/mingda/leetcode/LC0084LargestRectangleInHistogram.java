a11e767d34b259e620e7cb20e73482f82ad37dea
Mingda Rui <mingdarui@gmail.com>
2022-06-19 17:46:17 +0100

    LC0084 Largest Rectangle in Histogram: DP to calculate boundraies solution

--- src/main/java/pers/mingda/leetcode/LC0084LargestRectangleInHistogram.java
+++ src/main/java/pers/mingda/leetcode/LC0084LargestRectangleInHistogram.java
@@ -39,4 +39,32 @@ public class LC0084LargestRectangleInHistogram {
         }
         return result;
     }
+
+    public int largestRectangleAreaDp(int[] heights) {
+        int[] leftBoundrayRecord = new int[heights.length];
+        for (int i = 0; i < heights.length; i++) {
+            int currentH = heights[i];
+            int leftBoundray = i - 1;
+            while (leftBoundray >= 0 && heights[leftBoundray] >= currentH)
+                leftBoundray = leftBoundrayRecord[leftBoundray];
+            leftBoundrayRecord[i] = leftBoundray;
+        }
+
+        int[] rightBoundrayRecord = new int[heights.length];
+        for (int i = heights.length - 1; i >= 0; i--) {
+            int currentH = heights[i];
+            int rightBoundray = i + 1;
+            while (rightBoundray < heights.length && heights[rightBoundray] >= currentH)
+                rightBoundray = rightBoundrayRecord[rightBoundray];
+            rightBoundrayRecord[i] = rightBoundray;
+        }
+
+        int result = 0;
+        for (int i = 0; i < heights.length; i++) {
+            int currentH = heights[i];
+            int distance = rightBoundrayRecord[i] - leftBoundrayRecord[i] - 1;
+            result = Math.max(result, distance * currentH);
+        }
+        return result;
+    }
 }
