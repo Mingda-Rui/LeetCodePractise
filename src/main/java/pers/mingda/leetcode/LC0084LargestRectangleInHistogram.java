17c3f906dac8d3dac2ec560b3dc394768ff90aaa
Mingda Rui <mingdarui@gmail.com>
2022-06-19 18:24:04 +0100

    LC0084 Largest Rectangle in Histogram: stack solution, refactored

--- src/main/java/pers/mingda/leetcode/LC0084LargestRectangleInHistogram.java
+++ src/main/java/pers/mingda/leetcode/LC0084LargestRectangleInHistogram.java
@@ -4,39 +4,22 @@ import java.util.Stack;
 
 public class LC0084LargestRectangleInHistogram {
     public int largestRectangleArea(int[] heights) {
-        int[] leftHighests = new int[heights.length];
-        Stack<Integer> leftIndexStack = new Stack<>();
-        for (int i = 0; i < heights.length; i++) {
-            int currentH = heights[i];
-            int currentBoundray = i;
-            while (!leftIndexStack.isEmpty() && heights[leftIndexStack.peek()] >= currentH)
-                currentBoundray = leftIndexStack.pop();
-
-            leftIndexStack.push(i);
-            int distance = i - currentBoundray + 1;
-            int prevDistance = Math.max(0, leftHighests[currentBoundray] - 1);
-            leftHighests[i] = distance + prevDistance;
-        }
-
-        int[] rightHighests = new int[heights.length];
-        Stack<Integer> rightIndexStack = new Stack<>();
-        for (int i = heights.length - 1; i >= 0; i--) {
-            int currentH = heights[i];
-            int currentBoundray = i;
-            while (!rightIndexStack.isEmpty() && heights[rightIndexStack.peek()] >= currentH)
-                currentBoundray = rightIndexStack.pop();
-
-            rightIndexStack.push(i);
-            int distance = currentBoundray - i + 1;
-            int prevDistance = Math.max(0, rightHighests[currentBoundray] - 1);
-            rightHighests[i] = distance + prevDistance;
-        }
-
+        Stack<Integer> stack = new Stack<>();
         int result = 0;
-        for (int i = 0; i < heights.length; i++) {
-            int area = (leftHighests[i] + rightHighests[i] - 1) * heights[i];
-            result = Math.max(result, area);
+        int index = 0;
+        while (index <= heights.length) {
+            int currentH = index < heights.length ? heights[index] : 0;
+            if (stack.isEmpty() || heights[stack.peek()] < currentH) {
+                stack.push(index);
+                index++;
+            } else {
+                int targetIndex = stack.pop();
+                int leftIndex = stack.isEmpty() ? -1 : stack.peek();
+                int area = (index - leftIndex - 1) * heights[targetIndex];
+                result = Math.max(result, area);
+            }
         }
+
         return result;
     }
 
