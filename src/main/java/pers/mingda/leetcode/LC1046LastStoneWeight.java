fb56d626c49ebcf8f21d083540d4dae1e27728ce
Mingda Rui <mingdarui@gmail.com>
2022-06-20 20:05:07 +0100

    LC1046 Last Stone Weight

---
+++ src/main/java/pers/mingda/leetcode/LC1046LastStoneWeight.java
@@ -0,0 +1,20 @@
+package pers.mingda.leetcode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class LC1046LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
+        Queue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int stone: stones)
+            queue.add(stone);
+
+        while (queue.size() > 1) {
+            int stone1 = queue.remove();
+            int stone2 = queue.remove();
+            queue.add(Math.abs(stone1 - stone2));
+        }
+        return queue.remove();
+    }
+}
