9c45367bc60016010c036813d5b4f4819c43d3bf
Mingda Rui <mingdarui@gmail.com>
2022-06-29 20:16:10 +0100

    LC0349 Intersection of Two Arrays: binary search solution O(nlog(n))

--- src/main/java/pers/mingda/leetcode/LC0349IntersectionOfTwoArrays.java
+++ src/main/java/pers/mingda/leetcode/LC0349IntersectionOfTwoArrays.java
@@ -1,5 +1,6 @@
 package pers.mingda.leetcode;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,5 +16,37 @@ public class LC0349IntersectionOfTwoArrays {
             if (!set2.contains(num))
                 set1.remove(num);
         return set1.stream().mapToInt(n -> n).toArray();
+    }
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> bin = new HashSet<>();
+        for (int num: nums2) {
+            if (set.contains(num) || bin.contains(num))
+                continue;
+            if (binarySearch(nums1, num))
+                set.add(num);
+            else
+                bin.add(num);
+        }
+        return set.stream().mapToInt(n -> n).toArray();
+    }
+
+    private boolean binarySearch(int[] nums1, int target) {
+        int start = 0;
+        int end = nums1.length;
+        int mid = 0;
+        while (start < end) {
+            mid = start + (end - start) / 2;
+            int current = nums1[mid];
+            if (current == target)
+                return true;
+            else if (current < target)
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        return false;
     }
 }
