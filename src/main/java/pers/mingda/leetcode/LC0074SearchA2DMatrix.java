7de3be391c766593e6b7dbe1e5b2babd857835d1
Mingda Rui <mingdarui@gmail.com>
2022-07-01 23:25:40 +0100

    LC0074 Search a 2D Matrix: search 2 axis

--- src/main/java/pers/mingda/leetcode/LC0074SearchA2DMatrix.java
+++ src/main/java/pers/mingda/leetcode/LC0074SearchA2DMatrix.java
@@ -21,4 +21,38 @@ public class LC0074SearchA2DMatrix {
         }
         return false;
     }
+
+    public boolean searchMatrixSearch2Axis(int[][] matrix, int target) {
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int start = 0;
+        int end = row;
+        int rowNum = 0;
+        while (start < end) {
+            rowNum = start + (end - start) / 2;
+            int val = matrix[rowNum][0];
+            if (val == target)
+                return true;
+            else if (start + 1 == end)
+                break;
+            else if (val < target)
+                start = rowNum;
+            else
+                end = rowNum;
+        }
+
+        start = 0;
+        end = column;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            int val = matrix[rowNum][mid];
+            if (val == target)
+                return true;
+            else if (val < target)
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        return false;
+    }
 }
