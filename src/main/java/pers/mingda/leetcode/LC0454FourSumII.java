2dbdd15ce81fbde0d48c3ec2493c1c5ea5e60a98
Mingda Rui <mingdarui@gmail.com>
2022-07-02 21:36:58 +0100

    LC0454 Four Sum II: n^2 hashmap solution, refactored

--- src/main/java/pers/mingda/leetcode/LC0454FourSumII.java
+++ src/main/java/pers/mingda/leetcode/LC0454FourSumII.java
@@ -18,8 +18,7 @@ public class LC0454FourSumII {
         for (int i = 0; i < nums1.length; i++) {
             for (int j = 0; j < nums2.length; j++) {
                 int sum = nums1[i] + nums2[j];
-                if (map.containsKey(-sum))
-                    total += map.get(-sum);
+                total += map.getOrDefault(-sum, 0);
             }
         }
 
