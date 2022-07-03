b0f5c85c4d4b33c91d3130fba433bd1fdd52208b
Mingda Rui <mingdarui@gmail.com>
2022-07-03 22:56:02 +0100

    LC0387 First Unique Character in a String: one pass

--- src/main/java/pers/mingda/leetcode/LC0387FirstUniqueCharacterInAString.java
+++ src/main/java/pers/mingda/leetcode/LC0387FirstUniqueCharacterInAString.java
@@ -1,5 +1,8 @@
 package pers.mingda.leetcode;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class LC0387FirstUniqueCharacterInAString {
     public int firstUniqChar(String s) {
         int[] record = new int[128];
@@ -11,5 +14,20 @@ public class LC0387FirstUniqueCharacterInAString {
                 return i;
         }
         return -1;
+    }
+
+    public int firstUniqCharOnePass(String s) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        boolean[] seen = new boolean[128];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!seen[c]) {
+                map.put(c, i);
+                seen[c] = true;
+            } else {
+                map.remove(c);
+            }
+        }
+        return map.isEmpty() ? -1 : map.entrySet().iterator().next().getValue();
     }
 }
