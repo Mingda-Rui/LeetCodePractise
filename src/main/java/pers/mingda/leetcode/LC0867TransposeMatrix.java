5ec00fac2448d058b2e082ae8038276d91aea1a7
Mingda Rui <mingdarui@gmail.com>
2022-07-04 21:15:23 +0100

    LC0867 Transpose Matrix: in place transpose

--- src/main/java/pers/mingda/leetcode/LC0867TransposeMatrix.java
+++ src/main/java/pers/mingda/leetcode/LC0867TransposeMatrix.java
@@ -2,6 +2,8 @@ package pers.mingda.leetcode;
 
 public class LC0867TransposeMatrix {
     public int[][] transpose(int[][] matrix) {
+        if (matrix.length == matrix[0].length)
+            return inPlaceTranspose(matrix);
         int[][] result = new int[matrix[0].length][matrix.length];
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[0].length; j++) {
@@ -9,5 +11,16 @@ public class LC0867TransposeMatrix {
             }
         }
         return result;
+    }
+
+    private int[][] inPlaceTranspose(int[][] matrix) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        return matrix;
     }
 }
