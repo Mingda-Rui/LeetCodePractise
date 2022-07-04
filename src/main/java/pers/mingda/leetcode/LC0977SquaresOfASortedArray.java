a76846460d434c7c69d55c52f178f63997ce7578
Mingda Rui <mingdarui@gmail.com>
2022-07-04 20:53:53 +0100

    LC0977 Squares of a Sorted Array: refactored

--- src/main/java/pers/mingda/leetcode/LC0977SquaresOfASortedArray.java
+++ src/main/java/pers/mingda/leetcode/LC0977SquaresOfASortedArray.java
@@ -2,24 +2,20 @@ package pers.mingda.leetcode;
 
 public class LC0977SquaresOfASortedArray {
     public int[] sortedSquares(int[] nums) {
-        int minIndex = 0;
-        while (minIndex < nums.length && nums[minIndex] < 0)
-            minIndex++;
-        int negative = minIndex - 1;
-        int positive = minIndex;
         int[] result = new int[nums.length];
-        for (int i = 0; i < result.length; i++) {
-            int negVal = negative >= 0 ? nums[negative] : Integer.MAX_VALUE;
-            int posVal = positive < nums.length ? nums[positive] : Integer.MAX_VALUE;
-            if (Math.abs(negVal) < posVal) {
-                result[i] = negVal * negVal;
-                negative--;
+        int head = 0;
+        int tail = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int headVal = nums[head];
+            int tailVal = nums[tail];
+            if (Math.abs(headVal) > Math.abs(tailVal)) {
+                result[i] = headVal * headVal;
+                head++;
             } else {
-                result[i] = posVal * posVal;
-                positive++;
+                result[i] = tailVal * tailVal;
+                tail--;
             }
         }
-
         return result;
     }
 }
