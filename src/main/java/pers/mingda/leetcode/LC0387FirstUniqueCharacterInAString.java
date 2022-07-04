65c71061e2d0c1a4ec6450622a2f9b782badbde3
Mingda Rui <mingdarui@gmail.com>
2022-07-04 20:36:28 +0100

    LC0387 First Unique Character in a String: string indexOf solution

--- src/main/java/pers/mingda/leetcode/LC0387FirstUniqueCharacterInAString.java
+++ src/main/java/pers/mingda/leetcode/LC0387FirstUniqueCharacterInAString.java
@@ -50,4 +50,14 @@ public class LC0387FirstUniqueCharacterInAString {
         }
         return -1;
     }
+
+    public int firstUniqCharStringIndexOf(String s) {
+        int result = Integer.MAX_VALUE;
+        for (char c = 'a'; c <= 'z'; c++) {
+            int index = s.indexOf(c);
+            if (index != -1 && index == s.lastIndexOf(c))
+                result = Math.min(result, index);
+        }
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
 }
