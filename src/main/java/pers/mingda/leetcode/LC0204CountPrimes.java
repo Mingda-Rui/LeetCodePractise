bf79146a120fcf98cddb0f5c5563b8efa429f822
Mingda Rui <mingdarui@gmail.com>
2022-07-07 23:21:06 +0100

    LC0204 Count Primes: optimized

--- src/main/java/pers/mingda/leetcode/LC0204CountPrimes.java
+++ src/main/java/pers/mingda/leetcode/LC0204CountPrimes.java
@@ -2,13 +2,18 @@ package pers.mingda.leetcode;
 
 public class LC0204CountPrimes {
     public int countPrimes(int n) {
+        if (n < 3)
+            return 0;
+
         boolean[] record = new boolean[n];
-        int counter = 0;
-        for (int i = 2; i < n; i++) {
+        int counter = 1;
+        for (int i = 3; i < n; i += 2) {
             if (!record[i]) {
                 counter++;
-                for (int j = i + i; j < n; j += i)
+                boolean boundary = i <= Math.sqrt(n);
+                for (int j = i * i; boundary && j < n; j += i * 2)
                     record[j] = true;
+
             }
         }
         return counter;
