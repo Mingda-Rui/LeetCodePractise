ae2a9450589b1eb8a483cb0e8154ca05369247bb
Mingda Rui <mingdarui@gmail.com>
2022-07-10 20:37:39 +0100

    LC0713 Subarray Product Less Than K: refactor, get rid of size

--- src/main/java/pers/mingda/leetcode/LC0713SubarrayProductLessThanK.java
+++ src/main/java/pers/mingda/leetcode/LC0713SubarrayProductLessThanK.java
@@ -6,20 +6,17 @@ public class LC0713SubarrayProductLessThanK {
         int slow = 0;
         int fast = 0;
         int product = 1;
-        int size = 0;
 
         while (fast < nums.length) {
             int nextProduct = product * nums[fast];
             if (nextProduct < k || slow == fast) {
                 product = nextProduct;
                 fast++;
-                size++;
                 if (product < k)
-                    counter += size;
+                    counter += fast - slow;
             } else {
                 product /= nums[slow];
                 slow++;
-                size--;
             }
         }
         return counter;
