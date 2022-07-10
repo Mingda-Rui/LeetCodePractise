20d03c4400f8bd014e99c9472d2282e6d04cc156
Mingda Rui <mingdarui@gmail.com>
2022-07-10 11:23:00 +0100

    LC0059 Spiral Matrix II: refactored

--- src/main/java/pers/mingda/leetcode/LC0059SpiralMatrixII.java
+++ src/main/java/pers/mingda/leetcode/LC0059SpiralMatrixII.java
@@ -2,64 +2,24 @@ package pers.mingda.leetcode;
 
 public class LC0059SpiralMatrixII {
     public int[][] generateMatrix(int n) {
-        int[][] matrix = new int[n][n];
+        int[][] result = new int[n][n];
         int numOfLayers = (n + 1) / 2;
-        for (int i = 0; i < numOfLayers; i++)
-            generateMatrixByLayer(matrix, i);
-        return matrix;
-    }
-
-    private int[][] generateMatrixByLayer(int[][] matrix, int layer) {
-        int x = layer;
-        int y = layer;
-        int num = calcStartNum(matrix, layer);
-        int[][] iteArr = generateIteArr(matrix, layer);
-        for (int[] offset: iteArr) {
-            matrix[x][y] = num;
-            x += offset[0];
-            y += offset[1];
-            num++;
-        }
-        return matrix;
-    }
-
-    private int calcStartNum(int[][] matrix, int layer) {
-        int num = 0;
-        for (int i = 0; i < layer; i++) {
-            int length = matrix.length - 1 - i * 2;
-            num += length * 4;
-        }
-        num++;
-        return num;
-    }
-
-    private int[][] generateIteArr(int[][] matrix, int layer) {
-        int length = matrix.length - 1 - layer * 2;
-        int total = Math.max(1, length * 4);
-        int[][] iteArr = new int[total][2];
-        for (int i = 0; i < total; i++) {
-            int partition = length == 0 ? 0 : i / length;
-            switch (partition) {
-                case 0 -> {
-                    iteArr[i][0] = 0;
-                    iteArr[i][1] = 1;
-                }
-                case 1 -> {
-                    iteArr[i][0] = 1;
-                    iteArr[i][1] = 0;
-                }
-                case 2 -> {
-                    iteArr[i][0] = 0;
-                    iteArr[i][1] = -1;
-                }
-                case 3 -> {
-                    iteArr[i][0] = -1;
-                    iteArr[i][1] = 0;
-                }
+        for (int i = 0; i < numOfLayers; i++) {
+            int startNum = i == 0 ? 1 : result[i][i - 1] + 1;
+            result[i][i] = startNum;
+            int length = n - i * 2;
+            int boundary = n - i - 1;
+            for (int j = 1; j < length; j++)
+                result[i][i + j] = result[i][i + j - 1] + 1;
+            for (int j = 1; j < length; j++)
+                result[i + j][boundary] = result[i + j - 1][boundary] + 1;
+            for (int j = 1; j < length; j++)
+                result[boundary][boundary - j] = result[boundary][boundary - j + 1] + 1;
+            for (int j = 1; j < length - 1; j++) {
+                result[boundary - j][i] = result[boundary - j + 1][i] + 1;
             }
         }
-
-        return iteArr;
+        return result;
     }
 
 
