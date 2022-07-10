32a6e9de380eeb86bce655a3f28df31fce8bcaa4
Mingda Rui <mingdarui@gmail.com>
2022-07-10 10:54:46 +0100

    LC0059 Spiral Matrix II: calculate numbers based on axis

--- src/main/java/pers/mingda/leetcode/LC0059SpiralMatrixII.java
+++ src/main/java/pers/mingda/leetcode/LC0059SpiralMatrixII.java
@@ -61,4 +61,43 @@ public class LC0059SpiralMatrixII {
 
         return iteArr;
     }
+
+
+    public int[][] generateMatrixCalcNum(int n) {
+        int[][] result = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = calcNum(result ,i, j);
+            }
+        }
+        return result;
+    }
+
+    private int calcNum(int[][] matrix, int x, int y) {
+
+        int n = matrix.length;
+        int numOfLayers = (n + 1) / 2;
+        int mid = matrix.length / 2;
+        if (n % 2 == 0 && x >= mid && y >= mid)
+            mid--;
+        int xOffSet = Math.abs(x - mid);
+        int yOffSet = Math.abs(y - mid);
+        int layer = numOfLayers - Math.max(xOffSet, yOffSet);
+
+        int startNumOfTheLayer = 0;
+        for (int i = 0; i < layer; i++) {
+            int length = n - 1 - i * 2;
+            startNumOfTheLayer += length * 4;
+        }
+        startNumOfTheLayer++;
+        if (x == y && x == layer)
+            return startNumOfTheLayer;
+        if (x <= y)
+            return startNumOfTheLayer + (x - layer) + (y - layer);
+        int length = n - 1 - layer * 2;
+        if (x == n - layer * 2)
+            return startNumOfTheLayer + length * 2 + (length - y);
+        else
+            return startNumOfTheLayer + length * 3 + (length - x);
+    }
 }
