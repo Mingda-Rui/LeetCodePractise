1be2423ed1e52feb6333a342666b728b28d44f99
Mingda Rui <mingdarui@gmail.com>
2022-07-13 00:00:10 +0100

    LC0338 Counting Bits: refactored

--- src/main/java/pers/mingda/leetcode/LC0338CountingBits.java
+++ src/main/java/pers/mingda/leetcode/LC0338CountingBits.java
@@ -3,12 +3,8 @@ package pers.mingda.leetcode;
 public class LC0338CountingBits {
     public int[] countBits(int n) {
         int[] result = new int[n + 1];
-        for (int i = 1; i < result.length; i++) {
-            if (i % 2 != 0)
-                result[i] = result[i - 1] + 1;
-            else
-                result[i] = result[i / 2];
-        }
+        for (int i = 1; i < result.length; i++)
+            result[i] = result[i / 2] + i % 2;
         return result;
     }
 }
