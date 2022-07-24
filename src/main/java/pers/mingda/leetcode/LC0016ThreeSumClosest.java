07dcc06a8624b65ffea947b3240792ec9c24457f
Mingda Rui <mingdarui@gmail.com>
2022-07-24 23:39:40 +0100

    LC0016 Three Sum Closest: get rid of the init boolean

--- src/main/java/pers/mingda/leetcode/LC0016ThreeSumClosest.java
+++ src/main/java/pers/mingda/leetcode/LC0016ThreeSumClosest.java
@@ -5,17 +5,14 @@ import java.util.Arrays;
 public class LC0016ThreeSumClosest {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int closest = 0;
-        boolean initialized = false;
+        int closest = nums[0] + nums[1] + nums[2];
         for (int i = 0; i < nums.length; i++) {
             int start = i + 1;
             int end = nums.length - 1;
             while (start < end) {
                 int val = nums[i] + nums[start] + nums[end];
-                if (!initialized || Math.abs(val - target) < Math.abs(closest - target)) {
-                    initialized = true;
+                if (Math.abs(val - target) < Math.abs(closest - target))
                     closest = val;
-                }
                 if (val < target)
                     start++;
                 else if (val > target)
