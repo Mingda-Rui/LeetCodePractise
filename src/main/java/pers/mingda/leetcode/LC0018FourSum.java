83116bd8cb48b9c582f124ca54d2194d3eab2e4e
Mingda Rui <mingdarui@gmail.com>
2022-07-25 23:19:20 +0100

    LC0018 Four Sum: n sum solution

--- src/main/java/pers/mingda/leetcode/LC0018FourSum.java
+++ src/main/java/pers/mingda/leetcode/LC0018FourSum.java
@@ -8,66 +8,46 @@ import java.util.Set;
 
 public class LC0018FourSum {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        return kSum(nums, 0, target, 4);
+    }
+
+    private List<List<Integer>> kSum(int[] nums, int start, long target, int k) {
+        if (k == 2)
+            return twoSum(nums, start, target);
+
         List<List<Integer>> result = new LinkedList<>();
-        int[] sorted = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(sorted);
-
-        for (int outerL = 0; outerL < sorted.length; outerL++) {
-            if (outerL != 0 && sorted[outerL] == sorted[outerL - 1])
+        for (int i = start; i < nums.length; i++) {
+            int val = nums[i];
+            if (i != start && nums[i] == nums[i - 1])
                 continue;
-            for (int outerR = sorted.length - 1; outerR > outerL; outerR--) {
-                if (outerR != sorted.length - 1 && sorted[outerR] == sorted[outerR + 1])
-                    continue;
-                int innerL = outerL + 1;
-                int innerR = outerR - 1;
-                while (innerL < innerR) {
-                    long val = sorted[outerL];
-                    val += sorted[innerL];
-                    val += sorted[innerR];
-                    val += sorted[outerR];
-                    if (val == target) {
-                        List<Integer> list = Arrays.asList(sorted[outerL], sorted[innerL], sorted[innerR], sorted[outerR]);
-                        result.add(list);
-                    }
-
-                    if (val <= target) {
-                        innerL++;
-                        while (innerL < innerR && sorted[innerL] == sorted[innerL - 1])
-                            innerL++;
-                    } else {
-                        innerR--;
-                        while (innerR > innerL && sorted[innerR] == sorted[innerR + 1])
-                            innerR--;
-                    }
-                }
+            List<List<Integer>> sums = kSum(nums, i + 1, target - val, k - 1);
+            for (List<Integer> sum: sums) {
+                sum.add(val);
+                result.add(sum);
             }
         }
+
         return result;
     }
 
-    public List<List<Integer>> fourSumRefactored(int[] nums, int target) {
-        Set<List<Integer>> result = new HashSet<>();
-        int[] sorted = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(sorted);
-
-        for (int outerL = 0; outerL < sorted.length; outerL++) {
-            for (int outerR = sorted.length - 1; outerR > outerL; outerR--) {
-                int innerL = outerL + 1;
-                int innerR = outerR - 1;
-                while (innerL < innerR) {
-                    long val = (long)sorted[outerL] + (long)sorted[innerL] + (long)sorted[innerR] + (long)sorted[outerR];
-                    if (val == target) {
-                        List<Integer> list = Arrays.asList(sorted[outerL], sorted[innerL], sorted[innerR], sorted[outerR]);
-                        result.add(list);
-                    }
-
-                    if (val <= target)
-                        innerL++;
-                    else
-                        innerR--;
-                }
+    private List<List<Integer>> twoSum(int[] nums, int start, long target) {
+        List<List<Integer>> result = new LinkedList<>();
+        int end = nums.length - 1;
+        Set<Integer> set = new HashSet<>();
+        while (start < end) {
+            long val = (long) nums[start] + (long) nums[end];
+            if (val == target && !set.contains(nums[start])) {
+                List<Integer> list = new LinkedList<>();
+                list.addAll(Arrays.asList(nums[end], nums[start]));
+                result.add(list);
+                set.addAll(list);
             }
+            if (val <= target)
+                start++;
+            else
+                end--;
         }
-        return result.stream().toList();
+        return result;
     }
 }
