29313f3f999ffd9615a161be2d4f761f0f9bbeb9
Mingda Rui <mingdarui@gmail.com>
2022-07-28 00:10:13 +0100

    LC0329 Longest Increasing Path in a Matrix: DFS solution, refactored

--- src/main/java/pers/mingda/leetcode/LC0329LongestIncreasingPathInAMatrix.java
+++ src/main/java/pers/mingda/leetcode/LC0329LongestIncreasingPathInAMatrix.java
@@ -6,30 +6,25 @@ public class LC0329LongestIncreasingPathInAMatrix {
         int[][] record = new int[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++)
             for (int j = 0; j < matrix[0].length; j++)
-                longest = Math.max(longest, longestIncreasingPathDfs(matrix, record, i, j));
+                longest = Math.max(longest, longestIncreasingPathDfs(matrix, record, i, j, -1));
         return longest;
     }
 
-    private int longestIncreasingPathDfs(int[][] matrix, int[][] record, int x, int y) {
+    private int longestIncreasingPathDfs(int[][] matrix, int[][] record, int x, int y, int prevVal) {
         if (x < 0 || x >= matrix.length || y < 0 || y >= matrix[0].length)
+            return 0;
+        if (prevVal >= 0 && prevVal <= matrix[x][y])
             return 0;
         if (record[x][y] != 0)
             return record[x][y];
         int longest = 0;
-        if (indexCheck(matrix, x - 1, y) && matrix[x][y] < matrix[x - 1][y])
-            longest = Math.max(longest, longestIncreasingPathDfs(matrix, record, x - 1, y));
-        if (indexCheck(matrix, x + 1, y) && matrix[x][y] < matrix[x + 1][y])
-            longest = Math.max(longest, longestIncreasingPathDfs(matrix, record, x + 1, y));
-        if (indexCheck(matrix, x, y - 1) && matrix[x][y] < matrix[x][y - 1])
-            longest = Math.max(longest, longestIncreasingPathDfs(matrix, record, x, y - 1));
-        if (indexCheck(matrix, x, y + 1) && matrix[x][y] < matrix[x][y + 1])
-            longest = Math.max(longest, longestIncreasingPathDfs(matrix, record, x, y + 1));
+        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        for (int[] dir: dirs) {
+            int nextLen = longestIncreasingPathDfs(matrix, record, x + dir[0], y + dir[1], matrix[x][y]);
+            longest = Math.max(longest, nextLen);
+        }
         longest++;
         record[x][y] = longest;
         return longest;
-    }
-
-    private boolean indexCheck(int[][] matrix, int x, int y) {
-        return x >= 0 && x < matrix.length && y >= 0 && y < matrix[0].length;
     }
 }
