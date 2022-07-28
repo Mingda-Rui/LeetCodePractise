a0e7a070eeed4ef8989bddb25f57e4b19d243d45
Mingda Rui <mingdarui@gmail.com>
2022-07-28 23:31:19 +0100

    LC0022 Generate Parentheses: refactor, use StringBuilder for better performance

--- src/main/java/pers/mingda/leetcode/LC0022GenerateParentheses.java
+++ src/main/java/pers/mingda/leetcode/LC0022GenerateParentheses.java
@@ -6,19 +6,26 @@ import java.util.List;
 public class LC0022GenerateParentheses {
     public List<String> generateParenthesis(int n) {
         List<String> result = new LinkedList<>();
-        return generateParenthesis(result, n, n, "");
+        return generateParenthesis(result, n, n, new StringBuilder());
     }
 
-    private List<String> generateParenthesis(List<String> result, int numOfStart, int numOfClose, String current) {
+    private List<String> generateParenthesis(List<String> result, int numOfStart, int numOfClose, StringBuilder current) {
         if (numOfStart == 0 && numOfClose == 0) {
-            result.add(current);
+            result.add(current.toString());
             return result;
         }
 
-        if (numOfStart != 0)
-            generateParenthesis(result, numOfStart - 1, numOfClose, current + "(");
-        if (numOfStart != numOfClose)
-            generateParenthesis(result, numOfStart, numOfClose - 1, current + ")");
+        if (numOfStart != 0) {
+            current.append("(");
+            generateParenthesis(result, numOfStart - 1, numOfClose, current);
+            current.deleteCharAt(current.length() - 1);
+        }
+        if (numOfStart != numOfClose) {
+            current.append(")");
+            generateParenthesis(result, numOfStart, numOfClose - 1, current);
+            current.deleteCharAt(current.length() - 1);
+        }
         return result;
     }
 }
+
