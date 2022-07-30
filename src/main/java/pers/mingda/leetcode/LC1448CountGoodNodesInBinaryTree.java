6f1828cdf9fbf80b0f0ae6bde783a978d17f636a
Mingda Rui <mingdarui@gmail.com>
2022-07-30 12:26:21 +0100

    LC14 Count Good Nodes in Binary Tree: get rid of result holder array

--- src/main/java/pers/mingda/leetcode/LC1448CountGoodNodesInBinaryTree.java
+++ src/main/java/pers/mingda/leetcode/LC1448CountGoodNodesInBinaryTree.java
@@ -2,18 +2,17 @@ package pers.mingda.leetcode;
 
 public class LC1448CountGoodNodesInBinaryTree {
     public int goodNodes(TreeNode root) {
-        return goodNodes(root, new int[1], root.val);
+        return goodNodes(root, root.val);
     }
 
-    private int goodNodes(TreeNode root, int[] resultHolder, int max) {
+    private int goodNodes(TreeNode root, int max) {
         if (root == null)
-            return resultHolder[0];
+            return 0;
         int val = root.val;
-        if (val >= max)
-            resultHolder[0]++;
+        int result = val >= max ? 1 : 0;
         max = Math.max(max, val);
-        goodNodes(root.left, resultHolder, max);
-        goodNodes(root.right, resultHolder, max);
-        return resultHolder[0];
+        result += goodNodes(root.left, max);
+        result += goodNodes(root.right, max);
+        return result;
     }
 }
