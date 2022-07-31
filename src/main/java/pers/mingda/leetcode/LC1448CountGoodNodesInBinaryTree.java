490cea83e431d4b3138272c2d774cc9d781cce7a
Mingda Rui <mingdarui@gmail.com>
2022-07-31 21:48:39 +0100

    LC14 Count Good Nodes in Binary Tree: iterative solution

--- src/main/java/pers/mingda/leetcode/LC1448CountGoodNodesInBinaryTree.java
+++ src/main/java/pers/mingda/leetcode/LC1448CountGoodNodesInBinaryTree.java
@@ -1,5 +1,9 @@
 package pers.mingda.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class LC1448CountGoodNodesInBinaryTree {
     public int goodNodes(TreeNode root) {
         return goodNodes(root, root.val);
@@ -13,6 +17,34 @@ public class LC1448CountGoodNodesInBinaryTree {
         max = Math.max(max, val);
         result += goodNodes(root.left, max);
         result += goodNodes(root.right, max);
+        return result;
+    }
+
+    public int goodNodesIterative(TreeNode root) {
+        int result = 0;
+        Map<TreeNode, Integer> maxMap = new HashMap<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        maxMap.put(root, root.val);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            int val = node.val;
+            int parentMax = maxMap.get(node);
+            if (val >= parentMax)
+                result++;
+            if (node.left != null) {
+                TreeNode left = node.left;
+                stack.push(left);
+                int max = Math.max(left.val, parentMax);
+                maxMap.put(left, max);
+            }
+            if (node.right != null) {
+                TreeNode right = node.right;
+                stack.push(right);
+                int max = Math.max(right.val, parentMax);
+                maxMap.put(right, max);
+            }
+        }
         return result;
     }
 }
