d70b5586b0652de24a4437783f294ff357507741
Mingda Rui <mingdarui@gmail.com>
2022-08-07 21:49:22 +0100

    LC0239 Sliding Window Maximum: DP solution

--- src/main/java/pers/mingda/leetcode/LC0239SlidingWindowMaximum.java
+++ src/main/java/pers/mingda/leetcode/LC0239SlidingWindowMaximum.java
@@ -23,4 +23,34 @@ public class LC0239SlidingWindowMaximum {
 
         return result;
     }
+
+    public int[] maxSlidingWindowDp(int[] nums, int t) {
+        int[] result = new int[nums.length - (t - 1)];
+        int[] leftMax = new int[nums.length];
+        int[] rightMax = new int[nums.length];
+
+        int currentMax = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            if (i % t == 0)
+                currentMax = Integer.MIN_VALUE;
+            currentMax = Math.max(val, currentMax);
+            leftMax[i] = currentMax;
+        }
+
+        currentMax = Integer.MIN_VALUE;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int val = nums[i];
+            if (i % t == t - 1)
+                currentMax = Integer.MIN_VALUE;
+            currentMax = Math.max(val, currentMax);
+            rightMax[i] = currentMax;
+        }
+
+        for (int r = 0; r < nums.length - (t - 1); r++) {
+            int l = r + (t - 1);
+            result[r] = Math.max(rightMax[r], leftMax[l]);
+        }
+        return result;
+    }
 }
