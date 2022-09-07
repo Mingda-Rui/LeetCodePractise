c7172aac6dfdb6d67832201a1655b7261296e1e7
Mingda Rui <mingdarui@gmail.com>
2022-09-07 23:22:40 +0100

    LC0408 Valid Word Abbreviation

---
+++ src/main/java/pers/mingda/leetcode/LC0408ValidWordAbbreviation.java
@@ -0,0 +1,29 @@
+package pers.mingda.leetcode;
+
+public class LC0408ValidWordAbbreviation {
+
+}
+
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int pointer = 0;
+        for (int i = 0; i < abbr.length(); i++) {
+            char c = abbr.charAt(i);
+            if (Character.isDigit(c)) {
+                if (c == '0')
+                    return false;
+                int num = c - '0';
+                while (i + 1 < abbr.length() && Character.isDigit(abbr.charAt(i + 1))) {
+                    i++;
+                    num = num * 10 + (abbr.charAt(i) - '0');
+                }
+                pointer += num;
+            } else {
+                if (pointer >= word.length() || word.charAt(pointer) != c)
+                    return false;
+                pointer++;
+            }
+        }
+        return pointer == word.length();
+    }
+}
