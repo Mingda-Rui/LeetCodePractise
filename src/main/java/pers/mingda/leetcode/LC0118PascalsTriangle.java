fb8701e305a3fe8920cf8b2b4e99a0fe3c607ef5
Mingda Rui <mingdarui@gmail.com>
2022-09-07 21:34:45 +0100

    LC0118 Pascal's Triangle

---
+++ src/main/java/pers/mingda/leetcode/LC0118PascalsTriangle.java
@@ -0,0 +1,36 @@
+package pers.mingda.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LC0118PascalsTriangle {
+
+}
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new LinkedList<>();
+        List<Integer> currentRow = Arrays.asList(1);
+        result.add(currentRow);
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> nextRow = generateNextRow(currentRow);
+            currentRow = nextRow;
+            result.add(currentRow);
+        }
+
+        return result;
+    }
+
+    private List<Integer> generateNextRow(List<Integer> currentRow) {
+        List<Integer> nextRow = new ArrayList<>();
+        nextRow.add(1);
+        for (int i = 1; i < currentRow.size(); i++) {
+            int item = currentRow.get(i - 1) + currentRow.get(i);
+            nextRow.add(item);
+        }
+        nextRow.add(1);
+        return nextRow;
+    }
+}
