9223190cd21dfad6c3beba5aeb66a5de18d09a88
Mingda Rui <mingdarui@gmail.com>
2022-09-08 23:46:45 +0100

    LC0122 Best Time to Buy and Sell Stock II: refactored solution

--- src/main/java/pers/mingda/leetcode/LC0122BestTimeToBuyAndSellStockII.java
+++ src/main/java/pers/mingda/leetcode/LC0122BestTimeToBuyAndSellStockII.java
@@ -23,3 +23,14 @@ class Solution {
         return result;
     }
 }
+
+class RefactoredSolution {
+    public int maxProfit(int[] prices) {
+        int result = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i + 1])
+                result += (prices[i + 1] - prices[i]);
+        }
+        return result;
+    }
+}
