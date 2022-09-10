edcc9963091f122be05fb4c0c66688af97fa491a
Mingda Rui <mingdarui@gmail.com>
2022-09-10 22:22:38 +0100

    LC0139 Word Break: refactored dp solution

--- src/main/java/pers/mingda/leetcode/LC0139WordBreak.java
+++ src/main/java/pers/mingda/leetcode/LC0139WordBreak.java
@@ -4,25 +4,16 @@ import java.util.List;
 
 public class LC0139WordBreak {
     public boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] record = new boolean[s.length()];
-        for (int i = 0; i < s.length(); i++) {
+        int length = s.length();
+        boolean[] record = new boolean[length];
+        for (int i = 0; i < length; i++) {
             for (String word: wordDict) {
                 int prevIndex = i - word.length();
-                boolean prevFound = prevIndex == - 1 || (prevIndex >= 0 && record[prevIndex]);
-                if (!record[i] && prevFound) {
-                    if (equals(word, s, prevIndex + 1, i + 1))
-                        record[i] = true;
-                }
+                boolean prevPosFound = prevIndex == -1 || (prevIndex >= 0 && record[prevIndex]);
+                if (prevPosFound && word.equals(s.substring(prevIndex + 1, i + 1)))
+                    record[i] = true;
             }
         }
-        return record[s.length() - 1];
-    }
-
-    private boolean equals(String word, String s, int start, int end) {
-        for (int i = start; i < end; i++) {
-            if (word.charAt(i - start) != s.charAt(i))
-                return false;
-        }
-        return true;
+        return record[length - 1];
     }
 }
