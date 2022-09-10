dd67a5d7d3cff48e00251db34197266927faf5cf
Mingda Rui <mingdarui@gmail.com>
2022-09-10 23:50:06 +0100

    LC1275 Find Winner On a Tic-Tac-Toe Game: refactored

--- src/main/java/pers/mingda/leetcode/LC1275FindWinnerOnATicTacToeGame.java
+++ src/main/java/pers/mingda/leetcode/LC1275FindWinnerOnATicTacToeGame.java
@@ -10,28 +10,26 @@ class Solution {
         // 3 - 5 column
         // 6 \
         // 7 /
-        int[] recordA = new int[8];
-        int[] recordB = new int[8];
+        int[] record = new int[8];
         for (int i = 0; i < moves.length; i++) {
             int[] move = moves[i];
-            boolean isA = i % 2 == 0;
-            int[] record = isA ? recordA : recordB;
-            if (placeMove(record, move))
-                return isA ? "A" : "B";
+            int player = i % 2 == 0 ? 1 : -1;
+            if (placeMove(record, move, player))
+                return player == 1 ? "A" : "B";
         }
         return moves.length == 9 ? "Draw" : "Pending";
     }
 
-    private boolean placeMove(int[] record, int[] move) {
+    private boolean placeMove(int[] record, int[] move, int player) {
         int rowIndex = move[0];
         int columnIndex = move[1] + 3;
-        record[rowIndex]++;
-        record[columnIndex]++;
+        record[rowIndex] += player;
+        record[columnIndex] += player;
         if (move[0] == move[1])
-            record[6]++;
+            record[6] += player;
         if (move[0] + move[1] == 2)
-            record[7]++;
-        if (record[rowIndex] == 3 || record[columnIndex] == 3 || record[6] == 3 || record[7] == 3)
+            record[7] += player;
+        if (Math.abs(record[rowIndex]) == 3 || Math.abs(record[columnIndex]) == 3 || Math.abs(record[6]) == 3 || Math.abs(record[7]) == 3)
             return true;
         return false;
     }
