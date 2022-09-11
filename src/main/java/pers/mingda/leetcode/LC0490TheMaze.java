1f54dc8e7a8f65d0eff013f2039b9ce5925541bb
Mingda Rui <mingdarui@gmail.com>
2022-09-11 12:41:46 +0100

    LC0490 The Maze: BFS solution

--- src/main/java/pers/mingda/leetcode/LC0490TheMaze.java
+++ src/main/java/pers/mingda/leetcode/LC0490TheMaze.java
@@ -2,7 +2,9 @@ package pers.mingda.leetcode;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
 public class LC0490TheMaze {
@@ -63,6 +65,73 @@ class Solution {
         int x = start.get(0);
         int y = start.get(1);
         while (x + dir[0] >= 0 && x + dir[0] < maze.length && y + dir[1] >= 0 && y + dir[1] < maze[0].length && maze[x + dir[0]][y + dir[1]] == 0) {
+            x += dir[0];
+            y += dir[1];
+        }
+
+        return Arrays.asList(x, y);
+    }
+}
+
+class BfsSolution {
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        Set<List<Integer>> seen = new HashSet<>();
+        Queue<List<Integer>> queue = new LinkedList<>();
+        queue.offer(Arrays.asList(start[0], start[1]));
+
+        List<Integer> dest = Arrays.asList(destination[0], destination[1]);
+        while (!queue.isEmpty()) {
+            List<Integer> current = queue.poll();
+            if (isSameSpot(current, dest))
+                return true;
+            seen.add(current);
+            List<Integer> upperWall = findUpperWall(maze, current);
+            if (!seen.contains(upperWall))
+                queue.offer(upperWall);
+            List<Integer> bottomWall = findBottomWall(maze, current);
+            if (!seen.contains(bottomWall))
+                queue.offer(bottomWall);
+            List<Integer> leftWall = findLeftWall(maze, current);
+            if (!seen.contains(leftWall))
+                queue.offer(leftWall);
+            List<Integer> rightWall = findRightWall(maze, current);
+            if (!seen.contains(rightWall))
+                queue.offer(rightWall);
+        }
+        return false;
+    }
+
+    private boolean isSameSpot(List<Integer> start, List<Integer> dest) {
+        return start.get(0) == dest.get(0) && start.get(1) == dest.get(1);
+    }
+
+    private List<Integer> findUpperWall(int[][] maze, List<Integer> start) {
+        int[] dir = {-1, 0};
+        return findBoundryWall(maze, dir, start);
+    }
+
+    private List<Integer> findBottomWall(int[][] maze, List<Integer> start) {
+        int[] dir = {1, 0};
+        return findBoundryWall(maze, dir, start);
+    }
+
+    private List<Integer> findLeftWall(int[][] maze, List<Integer> start) {
+        int[] dir = {0, -1};
+        return findBoundryWall(maze, dir, start);
+
+    }
+
+    private List<Integer> findRightWall(int[][] maze, List<Integer> start) {
+        int[] dir = {0, 1};
+        return findBoundryWall(maze, dir, start);
+    }
+
+    private List<Integer> findBoundryWall(int[][] maze, int[] dir, List<Integer> start) {
+        int x = start.get(0);
+        int y = start.get(1);
+        while (x + dir[0] >= 0 && x + dir[0] < maze.length
+               && y + dir[1] >= 0 && y + dir[1] < maze[0].length
+               && maze[x + dir[0]][y + dir[1]] == 0) {
             x += dir[0];
             y += dir[1];
         }
