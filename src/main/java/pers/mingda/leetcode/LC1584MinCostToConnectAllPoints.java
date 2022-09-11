05fc47c007cc2ef5cea546575761b0a7b6dded57
Mingda Rui <mingdarui@gmail.com>
2022-09-11 19:14:52 +0100

    LC1584 Min Cost to Connect All Points: Union Find Solution

--- src/main/java/pers/mingda/leetcode/LC1584MinCostToConnectAllPoints.java
+++ src/main/java/pers/mingda/leetcode/LC1584MinCostToConnectAllPoints.java
@@ -77,3 +77,75 @@ class Solution {
         return Math.abs(point1[0] - point2[0]) + Math.abs(point1[1] - point2[1]);
     }
 }
+
+class UnionFindSolution {
+    public int minCostConnectPoints(int[][] points) {
+        Comparator<int[][]> edgeComparator = Comparator.comparingInt(this::getLengthOfEdge);
+        Queue<int[][]> queue = new PriorityQueue<>(edgeComparator);
+        Map<int[], Integer> map = new HashMap<>();
+        for (int i = 0; i < points.length; i++) {
+            int[] point = points[i];
+            map.put(point, i);
+
+            for (int j = i + 1; j < points.length; j++) {
+                int[] point2 = points[j];
+                int[][] edge = {point, point2};
+                queue.offer(edge);
+            }
+        }
+
+        int numOfEdge = 0;
+        int sumOfEdge = 0;
+        UnionFind uf = new UnionFind(points.length);
+        while (!queue.isEmpty() && numOfEdge < points.length - 1) {
+            int[][] edge = queue.poll();
+            int point1 = map.get(edge[0]);
+            int point2 = map.get(edge[1]);
+
+            if (uf.union(point1, point2)) {
+                sumOfEdge += getLengthOfEdge(edge);
+                numOfEdge ++;
+            }
+        }
+        return sumOfEdge;
+    }
+
+    private int getLengthOfEdge(int[][] edge) {
+        int[] point1 = edge[0];
+        int[] point2 = edge[1];
+        return Math.abs(point1[0] - point2[0]) + Math.abs(point1[1] - point2[1]);
+    }
+}
+
+class UnionFind {
+    private int[] group;
+    private int[] rank;
+
+    public UnionFind(int size) {
+        this.group = new int[size];
+        this.rank = new int[size];
+        for (int i = 0; i < group.length; i++)
+            group[i] = i;
+    }
+
+    public boolean union(int node1, int node2) {
+        int root1 = find(node1);
+        int root2 = find(node2);
+        if (root1 == root2)
+            return false;
+        if (rank[root1] < rank[root2]) {
+            group[root1] = root2;
+            rank[root2]++;
+        } else {
+            group[root2] = root1;
+            rank[root1]++;
+        }
+        return true;
+    }
+
+    public int find(int node) {
+        while (group[node] != node)
+            node = group[node];
+        return node;
+    }
+}
