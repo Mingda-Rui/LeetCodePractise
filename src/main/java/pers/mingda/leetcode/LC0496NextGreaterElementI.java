d37866edf2b46b1b2904d30601b1d8c7df514bb2
Mingda Rui <mingdarui@gmail.com>
2022-09-14 23:05:18 +0100

    LC0496 Next Greater Element I: Stack solution

--- src/main/java/pers/mingda/leetcode/LC0496NextGreaterElementI.java
+++ src/main/java/pers/mingda/leetcode/LC0496NextGreaterElementI.java
@@ -2,6 +2,7 @@ package pers.mingda.leetcode;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class LC0496NextGreaterElementI {
 
@@ -19,6 +20,28 @@ class Solution {
                     break;
                 }
             }
+        }
+
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            int num = nums1[i];
+            result[i] = map.getOrDefault(num, -1);
+        }
+        return result;
+    }
+}
+
+class StackSolution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int num: nums2) {
+
+            while (!stack.isEmpty() && stack.peek() < num) {
+                int smaller = stack.pop();
+                map.put(smaller, num);
+            }
+            stack.push(num);
         }
 
         int[] result = new int[nums1.length];
