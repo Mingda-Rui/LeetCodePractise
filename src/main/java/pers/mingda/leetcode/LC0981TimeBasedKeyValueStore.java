07104fc86c99c7c8405f8fa792af562644bf2839
Mingda Rui <mingdarui@gmail.com>
2022-09-17 02:07:28 +0100

    LC0981 Time Based Key-Value Store: Binary Search Solution

--- src/main/java/pers/mingda/leetcode/LC0981TimeBasedKeyValueStore.java
+++ src/main/java/pers/mingda/leetcode/LC0981TimeBasedKeyValueStore.java
@@ -1,6 +1,8 @@
 package pers.mingda.leetcode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -27,6 +29,57 @@ class TimeMap {
             return "";
         Map.Entry<Integer, String> tsVal = map.get(key).floorEntry(timestamp);
         return tsVal == null ? "" : tsVal.getValue();
+    }
+}
+
+class TimeMapBinarySearchSolution {
+
+    Map<String, List<TimestampedValue>> map;
+
+    public TimeMapBinarySearchSolution() {
+        this.map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        map.putIfAbsent(key, new ArrayList<>());
+        TimestampedValue tsVal = new TimestampedValue(timestamp, value);
+        map.get(key).add(tsVal);
+    }
+
+    public String get(String key, int timestamp) {
+        if (!map.containsKey(key))
+            return "";
+        List<TimestampedValue> timestamps = map.get(key);
+        TimestampedValue tsVal = findFloor(timestamps, timestamp);
+        return tsVal == null ? "" : tsVal.value;
+    }
+
+    private TimestampedValue findFloor(List<TimestampedValue> timestamps, int timestamp) {
+        return findFloor(timestamps, 0, timestamps.size(), timestamp);
+    }
+
+    private TimestampedValue findFloor(List<TimestampedValue> timestamps, int start, int end, int timestamp) {
+        if (start + 1 == end) {
+            TimestampedValue floorTsVal = timestamps.get(start);
+            return floorTsVal.timestamp <= timestamp ? floorTsVal : null;
+        }
+        int mid = start + (end - start) / 2;
+        TimestampedValue tsVal = timestamps.get(mid);
+        if (tsVal.timestamp < timestamp)
+            return findFloor(timestamps, mid, end, timestamp);
+        else if (tsVal.timestamp > timestamp)
+            return findFloor(timestamps, start, mid, timestamp);
+        return tsVal;
+    }
+}
+
+class TimestampedValue {
+    int timestamp;
+    String value;
+
+    public TimestampedValue(int timestamp, String value) {
+        this.timestamp = timestamp;
+        this.value = value;
     }
 }
 
