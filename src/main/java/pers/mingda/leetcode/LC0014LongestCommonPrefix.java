79c21913597adbf3b5a5020fc4a94ecb597a3c9a
Mingda Rui <mingdarui@gmail.com>
2022-10-08 22:50:35 +0100

    LC0014 Longest Common Prefix: refactored

--- src/main/java/pers/mingda/leetcode/LC0014LongestCommonPrefix.java
+++ src/main/java/pers/mingda/leetcode/LC0014LongestCommonPrefix.java
@@ -2,19 +2,14 @@ package pers.mingda.leetcode;
 
 public class LC0014LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-        int maxLen = 0;
-        while (true) {
-            char c = ' ';
-            for (String str: strs) {
-                if (maxLen >= str.length())
-                    return str.substring(0, maxLen);
-                char current = str.charAt(maxLen);
-                if (c == ' ')
-                    c = current;
-                else if (c != current)
-                    return str.substring(0, maxLen);
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                String comp = strs[j];
+                if (i == comp.length() || c != comp.charAt(i))
+                    return strs[0].substring(0, i);
             }
-            maxLen++;
         }
+        return strs[0];
     }
 }
