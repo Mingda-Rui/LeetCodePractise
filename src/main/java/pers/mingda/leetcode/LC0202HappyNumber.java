6e63a16f506f4226d0387ad78612f6f3cc1b6b13
Mingda Rui <mingdarui@gmail.com>
2022-10-09 17:33:30 +0100

    LC0202 Happy Number

---
+++ src/main/java/pers/mingda/leetcode/LC0202HappyNumber.java
@@ -0,0 +1,33 @@
+package pers.mingda.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LC0202HappyNumber {
+    public boolean isHappy(int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> seen = new HashSet<>();
+        while (!seen.contains(n)) {
+            seen.add(n);
+            n = calcHappyNumber(n, map);
+            if (n == 1)
+                return true;
+        }
+        return false;
+    }
+
+    private int calcHappyNumber(int n, Map<Integer, Integer> map) {
+        int sum = 0;
+        while (n != 0) {
+            int val = n % 10;
+            n /= 10;
+            if (!map.containsKey(val))
+                map.put(val, val * val);
+            int square = map.get(val);
+            sum += square;
+        }
+        return sum;
+    }
+}
