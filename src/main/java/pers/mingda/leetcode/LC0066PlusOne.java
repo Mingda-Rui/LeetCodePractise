22f0be6701f56c4d2669e3c0bd593c57f381e6df
Mingda Rui <mingdarui@gmail.com>
2022-10-10 00:03:35 +0100

    LC0066 Plus One, refactored

--- src/main/java/pers/mingda/leetcode/LC0066PlusOne.java
+++ src/main/java/pers/mingda/leetcode/LC0066PlusOne.java
@@ -2,26 +2,16 @@ package pers.mingda.leetcode;
 
 public class LC0066PlusOne {
     public int[] plusOne(int[] digits) {
-        int carry = 0;
         int len = digits.length;
-        digits[len - 1] += 1;
         for (int i = len - 1; i >= 0; i--) {
-            int val = digits[i] + carry;
-            digits[i] = val % 10;
-            if (val <= 9) {
-                carry = 0;
-                break;
-            }
+            int val = digits[i];
+            digits[i] = (val + 1) % 10;
+            if (digits[i] != 0)
+                return digits;
+        }
 
-            carry = 1;
-        }
-        if (carry == 1) {
-            int[] result = new int[len + 1];
-            result[0] = 1;
-            for (int i = 1; i < result.length; i++)
-                result[i] = digits[i - 1];
-            return result;
-        }
-        return digits;
+        int[] result = new int[len + 1];
+        result[0] = 1;
+        return result;
     }
 }
