57e0731a0b40ee8231dca14961ebe5ef93e9ecfb
Mingda Rui <mingdarui@gmail.com>
2022-10-09 20:10:59 +0100

    LC0026 Remove Duplicates from Sorted Array

---
+++ src/main/java/pers/mingda/leetcode/LC0026RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,14 @@
+package pers.mingda.leetcode;
+
+public class LC0026RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int index = 0;
+        for (int num: nums) {
+            if (num != nums[index]) {
+                index++;
+                nums[index] = num;
+            }
+        }
+        return index + 1;
+    }
+}
