e2a2d6c53b98f16d2fc7174d79b21de5641b5da3
Mingda Rui <mingdarui@gmail.com>
2022-10-09 20:31:44 +0100

    LC0606 Construct String from Binary Tree

---
+++ src/main/java/pers/mingda/leetcode/LC0606ConstructStringFromBinaryTree.java
@@ -0,0 +1,20 @@
+package pers.mingda.leetcode;
+
+public class LC0606ConstructStringFromBinaryTree {
+    public String tree2str(TreeNode root) {
+        if (root == null)
+            return "";
+        String left = tree2str(root.left);
+        String right = tree2str(root.right);
+
+        if (!left.isEmpty() || !right.isEmpty())
+            left = addParenthesis(left);
+        if (!right.isEmpty())
+            right = addParenthesis(right);
+        return root.val + left + right;
+    }
+
+    private String addParenthesis(String tree) {
+        return "(" + tree + ")";
+    }
+}
