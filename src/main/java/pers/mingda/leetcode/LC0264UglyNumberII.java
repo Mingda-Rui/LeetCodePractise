66efcbe9356db580638c449fa9c8e4f8c523851d
Mingda Rui <mingdarui@gmail.com>
2023-01-02 21:21:52 -0800

    LC0264 Ugly Number II: refactored

--- src/main/java/pers/mingda/leetcode/LC0264UglyNumberII.java
+++ src/main/java/pers/mingda/leetcode/LC0264UglyNumberII.java
@@ -3,31 +3,34 @@ package pers.mingda.leetcode;
 public class LC0264UglyNumberII {
     public int nthUglyNumber(int n) {
         int[] uglyNums = new int[n];
-        int current = 0;
-        uglyNums[current] = 1;
-        int p2 = current;
-        int p3 = current;
-        int p5 = current;
-        while (current != n - 1) {
-            int next2 = uglyNums[p2] * 2;
-            int next3 = uglyNums[p3] * 3;
-            int next5 = uglyNums[p5] * 5;
-            int next = 0;
-            if (next2 <= next3 && next2 <= next5) {
-                next = next2;
-                p2++;
-            } else if (next3 <= next2 && next3 <= next5) {
-                next = next3;
-                p3++;
-            } else {
-                next = next5;
-                p5++;
-            }
-            if (uglyNums[current] < next)
-                current++;
-            uglyNums[current] = next;
+        uglyNums[0] = 1;
+        int index2 = 0;
+        int index3 = 0;
+        int index5 = 0;
 
+        int num2 = 2;
+        int num3 = 3;
+        int num5 = 5;
+        for (int i = 1; i < n; i++) {
+            int min = getMin(num2, num3, num5);
+            uglyNums[i] = min;
+            if (num2 == min) {
+                index2++;
+                num2 = uglyNums[index2] * 2;
+            }
+            if (num3 == min) {
+                index3++;
+                num3 = uglyNums[index3] * 3;
+            }
+            if (num5 == min) {
+                index5++;
+                num5 = uglyNums[index5] * 5;
+            }
         }
         return uglyNums[n - 1];
+    }
+
+    private int getMin(int num1, int num2, int num3) {
+        return Math.min(num1, Math.min(num2, num3));
     }
 }
