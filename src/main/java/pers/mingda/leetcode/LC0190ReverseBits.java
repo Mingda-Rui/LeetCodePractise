68042f3c42e440745cf0f9729217270a5607e973
Mingda Rui <mingdarui@gmail.com>
2023-01-04 20:40:12 -0800

    LC0190 Reverse Bits: non built-in method

--- src/main/java/pers/mingda/leetcode/LC0190ReverseBits.java
+++ src/main/java/pers/mingda/leetcode/LC0190ReverseBits.java
@@ -5,4 +5,15 @@ public class LC0190ReverseBits {
     public int reverseBits(int n) {
         return Integer.reverse(n);
     }
+
+    public int reverseBits2(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int bit = n % 2 == 0 ? 0 : 1;
+            result <<= 1;
+            result += bit;
+            n >>= 1;
+        }
+        return result;
+    }
 }
