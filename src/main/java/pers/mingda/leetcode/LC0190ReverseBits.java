95762bc5f2d3aedb691346a5f72b283cf00b0294
Mingda Rui <mingdarui@gmail.com>
2023-01-04 19:43:33 -0800

    LC0190 Reverse Bits

---
+++ src/main/java/pers/mingda/leetcode/LC0190ReverseBits.java
@@ -0,0 +1,8 @@
+package pers.mingda.leetcode;
+
+public class LC0190ReverseBits {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        return Integer.reverse(n);
+    }
+}
