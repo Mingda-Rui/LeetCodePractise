1eb68ee2fce50056a03ab587528139ad725872b7
Mingda Rui <mingdarui@gmail.com>
2023-01-06 20:00:56 -0800

    LC0367 Valid Perfect Square: Binary Search

--- src/main/java/pers/mingda/leetcode/LC0367ValidPerfectSquare.java
+++ src/main/java/pers/mingda/leetcode/LC0367ValidPerfectSquare.java
@@ -5,4 +5,22 @@ public class LC0367ValidPerfectSquare {
         int sqrt = (int) Math.sqrt(num);
         return sqrt * sqrt == num;
     }
+
+    public boolean isPerfectSquareBinarySearch(int num) {
+        int start = 1;
+        int end = num + 1;
+        int limit = (int) Math.sqrt(Integer.MAX_VALUE);
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            int square = mid * mid;
+
+            if (square == num)
+                return true;
+            else if (square > num || mid > limit)
+                end = mid;
+            else
+                start = mid + 1;
+        }
+        return false;
+    }
 }
