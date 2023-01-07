deceb6bfb53283043024b2fc01e86e0b8de75bb9
Mingda Rui <mingdarui@gmail.com>
2023-01-07 13:42:02 -0800

    LC0724 Find Pivot Index: refactored

--- src/main/java/pers/mingda/leetcode/LC0724FindPivotIndex.java
+++ src/main/java/pers/mingda/leetcode/LC0724FindPivotIndex.java
@@ -2,26 +2,15 @@ package pers.mingda.leetcode;
 
 public class LC0724FindPivotIndex {
     public int pivotIndex(int[] nums) {
-        int[] leftSums = new int[nums.length];
-        int[] rightSums = new int[nums.length];
-
+        int sum = 0;
+        for (int num: nums)
+            sum += num;
         int leftSum = 0;
         for (int i = 0; i < nums.length; i++) {
-            leftSum += nums[i];
-            leftSums[i] = leftSum;
-        }
-
-        int rightSum = 0;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            rightSum += nums[i];
-            rightSums[i] = rightSum;
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            int lSum = i == 0 ? 0 : leftSums[i - 1];
-            int rSum = i == nums.length - 1 ? 0 : rightSums[i + 1];
-            if (lSum == rSum)
+            int rightSum = sum - nums[i] - leftSum;
+            if (leftSum == rightSum)
                 return i;
+            leftSum += nums[i];
         }
         return -1;
     }
