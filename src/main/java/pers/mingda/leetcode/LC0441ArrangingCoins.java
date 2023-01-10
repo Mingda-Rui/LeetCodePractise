c4e8b30d1eb79de1e6b15fb6a7a7be0f18d0fa56
Mingda Rui <mingdarui@gmail.com>
2023-01-09 21:14:48 -0800

    LC0441 Arranging Coins: binary search

---
+++ src/main/java/pers/mingda/leetcode/LC0441ArrangingCoins.java
@@ -0,0 +1,27 @@
+package pers.mingda.leetcode;
+
+public class LC0441ArrangingCoins {
+
+}
+
+class LC0441Solution {
+    public int arrangeCoins(int n) {
+        if (n == 1)
+            return 1;
+        long start = 1;
+        long end = n;
+
+        while (start < end) {
+            long mid = start + (end - start) / 2;
+            long sum = (1 + mid) * mid / 2;
+            if (sum > n) {
+                end = mid;
+            } else if (sum < n) {
+                start = mid + 1;
+            } else {
+                return (int) mid;
+            }
+        }
+        return (int) start - 1;
+    }
+}
