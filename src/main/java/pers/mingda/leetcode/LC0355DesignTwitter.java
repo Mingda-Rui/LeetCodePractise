9ee8f1108649b8e5ba5df9ba92f2f2feb5c216bb
Mingda Rui <mingdarui@gmail.com>
2023-01-15 15:42:34 -0800

    LC0355 Design Twitter

---
+++ src/main/java/pers/mingda/leetcode/LC0355DesignTwitter.java
@@ -0,0 +1,104 @@
+package pers.mingda.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class LC0355DesignTwitter {
+
+}
+
+class LC0355Twitter {
+
+    private Map<Integer, Tweet> tweets;
+    private Map<Integer, Set<Integer>> followeeMap;
+    private int timestamp;
+    private static final int FEED_LIMIT = 10;
+
+    public LC0355Twitter() {
+        this.tweets = new HashMap<>();
+        this.followeeMap = new HashMap<>();
+        this.timestamp = 0;
+    }
+
+    public void postTweet(int userId, int tweetId) {
+        Tweet tweetHistory = tweets.get(userId);
+        Tweet tweet = new Tweet(timer(), tweetId, tweetHistory);
+        //tweet.next = tweetHistory;
+        tweets.put(userId, tweet);
+
+        followeeMap.putIfAbsent(userId, new HashSet<>());
+        followeeMap.get(userId).add(userId);
+    }
+
+    public List<Integer> getNewsFeed(int userId) {
+        List<Integer> result = new LinkedList<>();
+        if (!followeeMap.containsKey(userId))
+            return result;
+
+        Set<Tweet> headTweets = new HashSet<>();
+        for (int followee: followeeMap.get(userId)) {
+            Tweet userTweet = tweets.get(followee);
+            if (userTweet != null)
+                headTweets.add(userTweet);
+        }
+
+        while (result.size() < FEED_LIMIT && !headTweets.isEmpty()) {
+            Tweet mostRecent = getMostRecentTweet(headTweets);
+            headTweets.remove(mostRecent);
+            if (mostRecent.next != null)
+                headTweets.add(mostRecent.next);
+            result.add(mostRecent.tId);
+        }
+        return result;
+    }
+
+    private Tweet getMostRecentTweet(Set<Tweet> tweets) {
+        Tweet mostRecent = null;
+        for (Tweet tweet: tweets)
+            if (mostRecent == null || mostRecent.ts < tweet.ts)
+                mostRecent = tweet;
+        return mostRecent;
+    }
+
+    public void follow(int followerId, int followeeId) {
+        if (!followeeMap.containsKey(followerId)) {
+            followeeMap.put(followerId, new HashSet<>());
+            followeeMap.get(followerId).add(followerId);
+        }
+        followeeMap.get(followerId).add(followeeId);
+    }
+
+    public void unfollow(int followerId, int followeeId) {
+        if (followeeMap.containsKey(followerId))
+            followeeMap.get(followerId).remove(followeeId);
+    }
+
+    private int timer() {
+        return timestamp++;
+    }
+
+    class Tweet {
+        Tweet next;
+        private final int ts;
+        final int tId;
+
+        public Tweet(int ts, int tId, Tweet next) {
+            this.ts = ts;
+            this.tId = tId;
+            this.next = next;
+        }
+    }
+}
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
