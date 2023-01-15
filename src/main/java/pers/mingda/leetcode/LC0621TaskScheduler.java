85c8d80abd482338334a624400b0955468da696d
Mingda Rui <mingdarui@gmail.com>
2023-01-14 21:17:04 -0800

    LC0621 Task Scheduler

---
+++ src/main/java/pers/mingda/leetcode/LC0621TaskScheduler.java
@@ -0,0 +1,32 @@
+package pers.mingda.leetcode;
+
+public class LC0621TaskScheduler {
+
+}
+
+class LC0621Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] lettersFreq = new int[26];
+        int maxFreq = 0;
+        char maxFreqLetter = ' ';
+        for (char l: tasks) {
+            int letterIndex = l - 'A';
+            lettersFreq[letterIndex]++;
+            if (lettersFreq[letterIndex] > maxFreq) {
+                maxFreq = lettersFreq[letterIndex];
+                maxFreqLetter = l;
+            }
+        }
+
+        int idleSlots = (maxFreq - 1) * n;
+        for (int i = 0; i < lettersFreq.length; i++) {
+            char l = (char)('A' + i);
+            if (l == maxFreqLetter)
+                continue;
+            int freq = lettersFreq[i];
+            int filledIdleSlot = Math.min(maxFreq - 1, freq);
+            idleSlots -= filledIdleSlot;
+        }
+        return tasks.length + Math.max(0, idleSlots);
+    }
+}
