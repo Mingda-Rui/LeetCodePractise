918978bb3f402b6fd31e0756ef4bad148a69ad89
Mingda Rui <mingdarui@gmail.com>
2023-11-06 21:59:30 -0800

    LC0001 Two Sum: hash map one pass

--- src/main/java/pers/mingda/leetcode/LC0001TwoSum.java
+++ src/main/java/pers/mingda/leetcode/LC0001TwoSum.java
@@ -1,59 +1,17 @@
 package pers.mingda.leetcode;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class LC0001TwoSum {
-
-    public int[] twoSumBruteForce(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-        throw new RuntimeException("No solution found!");
-    }
-
-    public int[] twoSumSorting(int[] nums, int target) {
-        int[] coyied = Arrays.copyOf(nums, nums.length);
-        int[] twoNums = getNums(coyied, target);
-        int first = -1;
-        int second = -1;
-        for (int i = 0; i < nums.length; i++)
-            if (nums[i] == twoNums[0] || nums[i] == twoNums[1]) {
-                first = first == -1 ? i : first;
-                second = first == -1 ? second : i;
-            }
-
-        return new int[]{first, second};
-    }
-
-    private int[] getNums(int[] nums, int target) {
-        Arrays.sort(nums);
-        int first = 0;
-        int second = nums.length - 1;
-        while (first < second) {
-            int sum = nums[first] + nums[second];
-            if (sum == target)
-                return new int[]{nums[first], nums[second]};
-            first = sum < target ? first + 1 : first;
-            second = sum > target ? second - 1 : second;
-        }
-        throw new RuntimeException("No solution found!");
-    }
-
-    public int[] twoSumHash(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> record = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int current = nums[i];
-            int expect = target - current;
-            if (record.containsKey(expect))
-                return new int[]{record.get(expect), i};
-            record.put(current, i);
+            if (record.containsKey(target - nums[i]))
+                return IntStream.of(record.get(target - nums[i]), i).toArray();
+            record.put(nums[i], i);
         }
-        throw new RuntimeException("No solution found!");
+        return null;
     }
 }
