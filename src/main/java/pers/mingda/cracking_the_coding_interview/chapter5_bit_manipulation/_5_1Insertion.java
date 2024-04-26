c332f6148040623c03d935c86fc936496e34d97f
Mingda Rui <mingdarui@gmail.com>
2024-04-26 08:40:40 -0700

    add logs for 5.1 Insersion

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter5_bit_manipulation/_5_1Insertion.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter5_bit_manipulation/_5_1Insertion.java
@@ -1,9 +1,15 @@
 package pers.mingda.cracking_the_coding_interview.chapter5_bit_manipulation;
 
+import java.util.logging.Logger;
+
 public class _5_1Insertion {
+
+    private static final Logger LOGGER = Logger.getLogger(_5_1Insertion.class.getName());
+
     public int insertion(int N, int M, int i, int j) {
         int mBits = countBits(M);
         int shiftedM = M << (j - mBits);
+        LOGGER.info("shiftedM: %s".formatted(Integer.toBinaryString(shiftedM)));
         int clearedBitsN = clearBits(N, j);
         return clearedBitsN | shiftedM;
     }
