586e5aa15defa22e202538be2b5afd3d4b5cb5a5
Mingda Rui <mingdarui@gmail.com>
2024-09-16 23:22:44 -0700

    6.7 The Apocalypse, solution 2

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter6_math_and_logic_puzzles/_6_7TheApocalypse.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter6_math_and_logic_puzzles/_6_7TheApocalypse.java
@@ -1,36 +1,27 @@
 package pers.mingda.cracking_the_coding_interview.chapter6_math_and_logic_puzzles;
 
+import com.google.common.primitives.Ints;
+
 public class _6_7TheApocalypse {
-    // 1/2 girl
-    // 1/4 boy girl
-    // 1/8 boy boy girl
-    // 1/16 boy boy girl
-
-    // girl = 1 * 1/2 + 1/2 * 1/4 + 1/3 * 1/8 + 1/4 * 1/16 + ... + 1/x * 1/2^x
-    // boy  =           1/2 * 1/4 + 2/3 * 1/8 + 3/4 * 1/16 + ... + (x - 1)/x * 1/2^x
-
-    // girl - boy =
-    //        1 * 1/2 - 0 - 1/3 * 1/8 - 2/4 * 1/16 - ... - (x - 2)/x * 1/2^x
-
-    public double simulate(int families, int precision) {
-        double simulatedBoys = simulateBoys(families, precision);
-        double simulatedGirls = simulateGirls(families, precision);
-        return simulatedBoys/simulatedGirls;
+    public double runNFamilies(int families) {
+        int girls = 0;
+        int boys = 0;
+        for (int i = 0; i < families; i++) {
+            girls++;
+            boys += runOneFamily();
+        }
+        return (double) girls / (boys + girls);
     }
 
-    private double simulateBoys(int families, int precision) {
-        double boyPossibility = 0;
-        for (int i = 0; i < precision; i++) {
-            boyPossibility += i * (1 / Math.pow(2, i + 1));
+    private int runOneFamily() {
+        int boys = 0;
+        while (randomIsBoy()) {
+            boys++;
         }
-        return families * boyPossibility;
+        return boys;
     }
 
-    private double simulateGirls(int families, int precision) {
-        double girlPossibility = 0;
-        for (int i = 0; i < precision; i++) {
-            girlPossibility += 1 / Math.pow(2, i + 1);
-        }
-        return families * girlPossibility;
+    private boolean randomIsBoy() {
+        return Ints.saturatedCast(Math.round(Math.random())) == 1;
     }
 }
