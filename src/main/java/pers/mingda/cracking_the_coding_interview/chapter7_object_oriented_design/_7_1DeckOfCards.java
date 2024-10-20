761fa5fb5ca0999188d1de30fbb1fd0a3a9ee2b9
Mingda Rui <mingdarui@gmail.com>
2024-10-20 00:40:46 -0700

    Solution 7.1 Deck of Cards - Add Black Jack Game

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter7_object_oriented_design/_7_1DeckOfCards.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter7_object_oriented_design/_7_1DeckOfCards.java
@@ -119,4 +119,75 @@ class Hand <T extends Card> {
     }
 }
 
+class BlackJackHand extends Hand<BlackJackCard> {
+    /*
+     * There are multiple possible scores for a blackjack hand, since aces have
+     * multiple values. Return the highest possible score that's under 21, or the
+     * lowest score that's over.
+     */
+    public int score() {
+        ArrayList<Integer> scores = possibleScores();
+        int maxUnder = Integer.MIN_VALUE;
+        int minOver = Integer.MAX_VALUE;
+        for (int score: scores) {
+            if (score > 21 && score < minOver) {
+                minOver = score;
+            } else if (score <= 21 && score > maxUnder) {
+                maxUnder = score;
+            }
+        }
+        return maxUnder == Integer.MIN_VALUE ? minOver : maxUnder;
+    }
+
+    /* return a list of all possible scores this hand could have (evaluating each
+     * ace as both 1 and 11 */
+    private ArrayList<Integer> possibleScores() {
+        return null;
+    }
+
+    public boolean busted() {
+        return score() > 21;
+    }
+
+    public boolean is21() {
+        return score() == 21;
+    }
+
+    public boolean isBlackJack() {
+        return is21() && cards.size() == 2;
+    }
+}
+
+class BlackJackCard extends Card {
+
+    public BlackJackCard(int c, Suit s) {
+        super(c, s);
+    }
+
+    @Override
+    public int value() {
+        if (isAce()) return 1;
+        else if (isFaceCard()) return 10;
+        else return faceValue;
+    }
+
+    public int minValue() {
+        if (isAce()) return 1;
+        else return value();
+    }
+
+    public int maxValue() {
+        if (isAce()) return 11;
+        else return value();
+    }
+
+    public boolean isAce() {
+        return faceValue == 1;
+    }
+
+    public boolean isFaceCard() {
+        return faceValue >= 11 && faceValue <= 13;
+    }
+}
+
 
