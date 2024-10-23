f4bbf98536d77e67fe4c393c2ce82c4a5665f068
Mingda Rui <mingdarui@gmail.com>
2024-10-22 22:14:41 -0700

    Solution 7.6 Jigsaw - initial design

---
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter7_object_oriented_design/_7_6Jigsaw.java
@@ -0,0 +1,27 @@
+package pers.mingda.cracking_the_coding_interview.chapter7_object_oriented_design;
+
+public class _7_6Jigsaw {
+
+    // JigsawSolver
+    // JigsawMap
+    //
+    // clear(JigsawMap)
+    // fitWith(JigsawPiece, Edge, JigsawPiece, Edge)
+    // boolean put(int x, int y, Jigsaw, int orientation)
+    // smartPut(int x, int y, Jigsaw) {
+    //   // iterate through all orientation
+    //   // break if return true
+    // }
+
+    // JigsawPiece
+    // Edge[4] // 0: top, 1: bottom, 2: left, 3: right
+
+    // Edge
+    // int edge // 0: top, 1: bottom, 2: left, 3: right
+    // EdgeType
+
+    // Enum EdgeType
+
+    // JigsawMap / JigsawPlate
+    // JigsawPiece[][]
+}
