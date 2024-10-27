23f61681ac22c5315bfcf735ca186c16e20411c3
Mingda Rui <mingdarui@gmail.com>
2024-10-26 23:31:26 -0700

    Solution 7.10 Minesweeper - initial design

---
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter7_object_oriented_design/_7_10Minesweeper.java
@@ -0,0 +1,33 @@
+package pers.mingda.cracking_the_coding_interview.chapter7_object_oriented_design;
+
+public class _7_10Minesweeper {
+
+    // Class: Board
+    // Field: Cell[][]
+
+    // Class: Cell
+    // Field: CellType
+    // Field: Status
+    // Field: Optional<Integer>
+
+    // Enum: CellType
+    // Field: Blank, Number, Bomb
+
+    // Enum: CellStatus
+    // Field: Covered, Uncovered, Flagged
+
+    // Class: Player
+
+    // Class: MinesweeperGame
+    // Field: Player
+    // Field: Board
+    // Field: Display
+
+    // Func: boolean uncover(int x, int y);
+    // Func: boolean unCoverSurroundings(int x, int y)
+    // Func: flag(int x, int y);
+    // Func: unFlag(int x, int y);
+
+    // Class: Display
+    // Func: refreshDisplay(Board);
+}
