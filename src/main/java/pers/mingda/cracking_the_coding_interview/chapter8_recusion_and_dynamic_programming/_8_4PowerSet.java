c3ec21ca34dd5ffd50d8876fa846542c2a005075
Mingda Rui <mingdarui@gmail.com>
2024-11-10 01:09:45 -0800

    Solution 8.4 Power Set - Recursive solution

---
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter8_recusion_and_dynamic_programming/_8_4PowerSet.java
@@ -0,0 +1,25 @@
+package pers.mingda.cracking_the_coding_interview.chapter8_recusion_and_dynamic_programming;
+
+import java.util.ArrayList;
+
+public class _8_4PowerSet {
+    ArrayList<ArrayList<Integer>> getSubsets(ArrayList<Integer> set, int index) {
+        ArrayList<ArrayList<Integer>> allsubsets;
+        if (set.size() == index) { // Base case - add empty set
+            allsubsets = new ArrayList<>();
+            allsubsets.add(new ArrayList<>()); // Empty set
+        } else {
+            allsubsets = getSubsets(set, index + 1);
+            int item = set.get(index);
+            ArrayList<ArrayList<Integer>> moresubsets = new ArrayList<>();
+            for (ArrayList<Integer> subset: allsubsets) {
+                ArrayList<Integer> newsubset = new ArrayList<>();
+                newsubset.addAll(subset);
+                newsubset.add(item);
+                moresubsets.add(newsubset);
+            }
+            allsubsets.addAll(moresubsets);
+        }
+        return allsubsets;
+    }
+}
