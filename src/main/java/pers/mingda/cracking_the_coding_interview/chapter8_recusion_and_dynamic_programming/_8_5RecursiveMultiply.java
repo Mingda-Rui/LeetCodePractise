fcbab318318533597f0c6e4232d89bd2a90df246
Mingda Rui <mingdarui@gmail.com>
2025-01-01 22:30:50 -0800

    Solution 8.5 Recursive Multiply - Dynamic Programming optimization

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter8_recusion_and_dynamic_programming/_8_5RecursiveMultiply.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter8_recusion_and_dynamic_programming/_8_5RecursiveMultiply.java
@@ -1,7 +1,7 @@
 package pers.mingda.cracking_the_coding_interview.chapter8_recusion_and_dynamic_programming;
 
 public class _8_5RecursiveMultiply {
-    public int recursiveMultiply(int a, int b) {
+    public int recursiveMultiply(int a, int b, int[] memo) {
         int smaller = Math.min(a, b);
         int larger = Math.max(a, b);
         if (smaller == 0) {
@@ -10,13 +10,16 @@ public class _8_5RecursiveMultiply {
             return larger;
         }
 
+        if (memo[smaller] != 0) {
+            return memo[smaller];
+        }
+
         // smaller >= 2
         int halfSmallerFloor = smaller >> 1;
-        int halfSum = recursiveMultiply(halfSmallerFloor, larger);
-        if (halfSmallerFloor + halfSmallerFloor == smaller) {
-            return halfSum + halfSum;
-        } else {
-            return halfSum + recursiveMultiply(halfSmallerFloor + 1, larger);
-        }
+        int halfSum = recursiveMultiply(halfSmallerFloor, larger, memo);
+        int secondHalfSum = smaller % 2 == 0 ? halfSum : recursiveMultiply(halfSmallerFloor + 1, larger, memo);
+        int sum = halfSum + secondHalfSum;
+        memo[smaller] = sum;
+        return sum;
     }
 }
