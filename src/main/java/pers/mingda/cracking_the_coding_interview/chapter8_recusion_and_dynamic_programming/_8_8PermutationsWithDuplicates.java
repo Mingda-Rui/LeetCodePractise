de071b6472eb185f6c4115b90df4bbe2af26e137
Mingda Rui <mingdarui@gmail.com>
2025-01-03 22:58:45 -0800

    Solution 8.8 Permutations with Duplicates - optimization with char count map

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter8_recusion_and_dynamic_programming/_8_8PermutationsWithDuplicates.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter8_recusion_and_dynamic_programming/_8_8PermutationsWithDuplicates.java
@@ -1,22 +1,37 @@
 package pers.mingda.cracking_the_coding_interview.chapter8_recusion_and_dynamic_programming;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 public class _8_8PermutationsWithDuplicates {
-    public List<String> getPerms(String str) {
-        if (str.isEmpty() || str.length() == 1) {
-            return List.of(str);
+    public List<String> calculatePerms(String str) {
+        Map<Character, Integer> charCount = builcCountMap(str);
+        return getPerms(charCount, str.length());
+    }
+
+    private Map<Character, Integer> builcCountMap(String str) {
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
         }
-        Set<Character> chars = str.chars().boxed().map(c -> (char)c.intValue()).collect(Collectors.toSet());
+        return charCount;
+    }
+
+    private List<String> getPerms(Map<Character, Integer> charCount, int remains) {
+        if (remains == 0) {
+            return List.of("");
+        }
+
         List<String> allPerms = new ArrayList<>();
-        for (char c : chars) {
-            String remains = str.replaceFirst(String.valueOf(c), "");
-            List<String> subPerms = getPerms(remains);
-            List<String> fullPerms = subPerms.stream().map(subP -> c + subP).toList();
-            allPerms.addAll(fullPerms);
+        for (char c : charCount.keySet()) {
+            int count = charCount.get(c);
+            charCount.put(c, count - 1);
+            List<String> subPerms = getPerms(charCount, remains - 1);
+            charCount.put(c, count);
+            List<String> newPerms = subPerms.stream().map(subP -> c + subP).toList();
+            allPerms.addAll(newPerms);
         }
         return allPerms;
     }
