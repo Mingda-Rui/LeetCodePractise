c77143762b6e1daaa75745784a423307964b75d9
Mingda Rui <mingdarui@gmail.com>
2025-01-04 19:28:00 -0800

    Solution 8.13 Stack of Boxes - optimization, add memo

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter8_recusion_and_dynamic_programming/_8_13StackOfBoxes.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter8_recusion_and_dynamic_programming/_8_13StackOfBoxes.java
@@ -1,8 +1,6 @@
 package pers.mingda.cracking_the_coding_interview.chapter8_recusion_and_dynamic_programming;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class _8_13StackOfBoxes {
     public int stackBoxes(int numOfBoxes) {
@@ -10,28 +8,34 @@ public class _8_13StackOfBoxes {
         boxes.add(new Box());
 
         boxes.sort(Comparator.comparing(Box::getHeight).reversed());
+        Map<Box, Integer> heightMap = new HashMap<>();
         int maxHeight = 0;
         for (int i = 0; i < boxes.size(); i++) {
-            int height = stackBoxes(boxes.subList(i, boxes.size()));
+            int height = stackBoxes(boxes.subList(i, boxes.size()), heightMap);
             maxHeight = Math.max(maxHeight, height);
         }
         return maxHeight;
     }
 
-    private int stackBoxes(List<Box> remainingBoxes) {
+    private int stackBoxes(List<Box> remainingBoxes, Map<Box, Integer> heightMap) {
         if (remainingBoxes.isEmpty()){
             return 0;
         }
 
-        int maxHeight = 0;
+
         Box currentBox = remainingBoxes.getFirst();
+        if (heightMap.containsKey(currentBox)) {
+            return heightMap.get(currentBox);
+        }
+        int maxHeight = 0;
         for (int i = 1; i < remainingBoxes.size(); i++){
             Box nextBox = remainingBoxes.get(i);
             if (!nextBox.canStackOn(currentBox))
                 continue;
-            int height = currentBox.getHeight() + stackBoxes(remainingBoxes.subList(i, remainingBoxes.size()));
+            int height = currentBox.getHeight() + stackBoxes(remainingBoxes.subList(i, remainingBoxes.size()), heightMap);
             maxHeight = Math.max(maxHeight, height);
         }
+        heightMap.put(currentBox, maxHeight);
         return maxHeight;
     }
 }
