56c1a5bf0062ed4e1f6a03e621841fe191d19134
Mingda Rui <mingdarui@gmail.com>
2025-01-19 14:21:50 -0800

    Solution 10.4 Sorted Search, No Size - optimization

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter10_sorting_and_searching/_10_4SortedSearchNoSize.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter10_sorting_and_searching/_10_4SortedSearchNoSize.java
@@ -4,47 +4,25 @@ public class _10_4SortedSearchNoSize {
     private Listy list;
 
     public int findElement(int target) {
-        int len = calcLen(list);
-        return find(list, target, len);
-    }
-
-    private int calcLen(Listy list) {
-        return calcLenHelper(list, 0, 1);
-    }
-
-    private int calcLenHelper(Listy list, int left, int right) {
-        if (left + 1 >= right) {
-            return left;
+        int index = 1;
+        while(list.elementAt(index) != -1 && list.elementAt(index) <= target) {
+            index *= 2;
         }
-        int rightVal = list.elementAt(right);
-        if (rightVal != -1) {
-            return calcLenHelper(list, left, right * 2);
-        }
-
-        int mid = left + right / 2;
-        int midVal = list.elementAt(mid);
-        if (midVal == -1) {
-            return calcLenHelper(list, left, mid);
-        } else {
-            return calcLenHelper(list, mid, right);
-        }
+        return binarySearch(list, target, index / 2, index);
     }
 
-    private int find(Listy list, int element, int len) {
-        return findHelper(list, element, 0, len);
-    }
-
-    private int findHelper(Listy list, int element, int left, int right) {
+    private int binarySearch(Listy list, int target, int left, int right) {
         if (left >= right) {
-            return element == list.elementAt(left) ? left : -1;
+            return -1;
         }
+
         int mid = (left + right) / 2;
-        if (list.elementAt(mid) == element) {
+        if (list.elementAt(left) == target) {
             return mid;
-        } else if (list.elementAt(mid) > element) {
-            return findHelper(list, element, mid + 1, right);
+        } else if (list.elementAt(mid) > target) {
+            return binarySearch(list, target, left, mid);
         } else {
-            return findHelper(list, element, left, mid);
+            return binarySearch(list, target, mid + 1, right);
         }
     }
 }
