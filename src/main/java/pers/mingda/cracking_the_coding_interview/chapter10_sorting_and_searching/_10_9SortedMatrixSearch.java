56ac04771a9da5e0c6f68ca71aa0970f95b31df0
Mingda Rui <mingdarui@gmail.com>
2025-02-12 08:53:25 -0800

    Solution 10.9 Sorted Matrix Search - naive solution

---
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter10_sorting_and_searching/_10_9SortedMatrixSearch.java
@@ -0,0 +1,18 @@
+package pers.mingda.cracking_the_coding_interview.chapter10_sorting_and_searching;
+
+public class _10_9SortedMatrixSearch {
+    boolean findElement(int[][] matrix, int elem) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == elem) {
+                return true;
+            } else if (matrix[row][col] > elem) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+}
