08f8a7e0f76af54e87e420f77f543aa9151eb4c9
Mingda Rui <mingdarui@gmail.com>
2025-02-12 22:47:16 -0800

    Solution 10.10 Rank from Stream

---
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter10_sorting_and_searching/_10_10RankFromStream.java
@@ -0,0 +1,64 @@
+package pers.mingda.cracking_the_coding_interview.chapter10_sorting_and_searching;
+
+public class _10_10RankFromStream {
+
+    RankNode root;
+
+    public void track(int x) {
+        root.add(x);
+    }
+
+    public int getRankOfNumber(int x) {
+
+    }
+}
+
+class RankNode {
+    int count;
+    int value;
+    RankNode left, right;
+
+    public RankNode(int value) {
+        this.value = value;
+        this.count = 0;
+    }
+
+    public void add(int x) {
+        if (x > value) {
+            if (right == null) {
+                right = new RankNode(x);
+            } else {
+                right.add(x);
+            }
+        } else {
+            if (left == null) {
+                left = new RankNode(x);
+            } else {
+                left.add(x);
+            }
+        }
+        count++;
+    }
+
+    public int getRankOfNumber(int x) {
+        if (x < value) {
+            if (left == null) {
+                return 0;
+            } else {
+                return left.getRankOfNumber(x);
+            }
+        } else if (x > value) {
+            if (right == null) {
+                return count;
+            } else {
+                return right.getRankOfNumber(x) + count - right.count;
+            }
+        } else { // x == value
+            if (left == null) {
+                return 0;
+            } else {
+                return left.count;
+            }
+        }
+    }
+}
