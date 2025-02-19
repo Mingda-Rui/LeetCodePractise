394ae3a2e3a8994a6e21a72e8a3a9101493c00cd
Mingda Rui <mingdarui@gmail.com>
2025-02-18 23:28:27 -0800

    Solution 16.3 Intersection

---
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_3Intersection.java
@@ -0,0 +1,54 @@
+package pers.mingda.cracking_the_coding_interview.chapter16_moderate;
+
+public class _16_3Intersection {
+    private IntersectionPoint intersection(IntersectionLine line1, IntersectionLine line2) {
+        if (line1.isSameLine(line2)) {
+            return line1.start;
+        }
+
+        if (line1.isParallel(line2)) {
+            return null;
+        }
+
+        return line1.getIntersection(line2);
+    }
+}
+
+class IntersectionLine {
+    IntersectionPoint start, end;
+
+    public double getSlope() {
+        return (start.y - end.y) / (start.x - end.x);
+    }
+
+    public double getOffset() {
+        return start.y - getSlope() * start.x;
+    }
+
+    public boolean isParallel(IntersectionLine line) {
+        return getSlope() == line.getSlope();
+    }
+
+    public boolean isSameLine(IntersectionLine line) {
+        return isParallel(line) && getOffset() == line.getOffset();
+    }
+
+    public IntersectionPoint getIntersection(IntersectionLine line) {
+        double slope1 = getSlope();
+        double slope2 = line.getSlope();
+        double offset1 = getOffset();
+        double offset2 = line.getOffset();
+        double intersectionX = (offset2 - offset1) / (slope1 - slope2);
+        double intersectionY = slope1 * intersectionX + offset1;
+        return new IntersectionPoint(intersectionX, intersectionY);
+    }
+}
+
+class IntersectionPoint {
+    double x, y;
+
+    public IntersectionPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
