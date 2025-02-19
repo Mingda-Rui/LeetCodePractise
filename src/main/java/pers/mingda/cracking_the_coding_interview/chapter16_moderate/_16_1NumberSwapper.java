2945d3877246432101fc02086e9eef9cf2a8875a
Mingda Rui <mingdarui@gmail.com>
2025-02-18 22:34:13 -0800

    Solution 16.1 Number Swapper

---
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_1NumberSwapper.java
@@ -0,0 +1,9 @@
+package pers.mingda.cracking_the_coding_interview.chapter16_moderate;
+
+public class _16_1NumberSwapper {
+    public void swap(int a, int b) {
+        a = a - b; // a is now diff
+        b += a; // b becomes a0
+        a = b - a; // a becomes b0
+    }
+}
