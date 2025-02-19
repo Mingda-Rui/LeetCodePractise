80960c3198c4babc8c8ec99a4dc603c34e733945
Mingda Rui <mingdarui@gmail.com>
2025-02-18 22:54:05 -0800

    Solution 16.2 Word Frequencies - caching solution

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_2WordFrequencies.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_2WordFrequencies.java
@@ -1,10 +1,18 @@
 package pers.mingda.cracking_the_coding_interview.chapter16_moderate;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class _16_2WordFrequencies {
+
+    private Map<String, Integer> cache = new HashMap<>();
+
     int getFrequency(String[] book, String word) {
-        long count = Arrays.stream(book).filter(s -> s.contains(word)).count();
-        return Math.toIntExact(count);
+        if (!cache.containsKey(word)) {
+            long count = Arrays.stream(book).filter(s -> s.contains(word)).count();
+            cache.put(word, Math.toIntExact(count));
+        }
+        return cache.get(word);
     }
 }
