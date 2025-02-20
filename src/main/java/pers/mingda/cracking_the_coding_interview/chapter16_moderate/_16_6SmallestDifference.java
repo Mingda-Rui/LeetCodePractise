8eb0c57fe291ba7db996824fa6fe3a6f81782996
Mingda Rui <mingdarui@gmail.com>
2025-02-20 00:56:38 -0800

    Solution 16.6 Smallest Difference - bug fixing

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_6SmallestDifference.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_6SmallestDifference.java
@@ -1,15 +1,21 @@
 package pers.mingda.cracking_the_coding_interview.chapter16_moderate;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class _16_6SmallestDifference {
     int findSmallestDifference(int[] array1, int[] array2) {
-        int[] sortedArray = IntStream.concat(Arrays.stream(array1), Arrays.stream(array2)).sorted().toArray();
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+        int i = 0, j = 0;
         int smallest = Integer.MAX_VALUE;
-        for (int i = 1; i < sortedArray.length; i++) {
-            int prev = sortedArray[i - 1];
-            smallest = Math.min(smallest, sortedArray[i] - prev);
+        while (i < array1.length && j < array2.length) {
+            int diff = Math.abs(array1[i] - array2[j]);
+            smallest = Math.min(smallest, diff);
+            if (array1[i] < array2[j]) {
+                i++;
+            } else {
+                j++;
+            }
         }
         return smallest;
     }
