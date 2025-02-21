3a7111828f9338a33ee33a0606d59b1e45815cb8
Mingda Rui <mingdarui@gmail.com>
2025-02-21 00:19:33 -0800

    Solution 16.9 Operations - Subtraction: optimized solution

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_9Operations.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_9Operations.java
@@ -5,9 +5,15 @@ public class _16_9Operations {
     int negate(int a) {
         int neg = 0;
         int newSign = a < 0 ? 1 : -1;
+        int delta = newSign;
         while (a != 0) {
-            neg += newSign;
-            a += newSign;
+            boolean differentSigns = (a + delta > 0) != (a > 0);
+            if (a + delta != 0 && differentSigns) { // If delta is too big, reset it.
+                delta = newSign;
+            }
+            neg += delta;
+            a += delta;
+            delta += delta; // Double the delta
         }
         return neg;
     }
