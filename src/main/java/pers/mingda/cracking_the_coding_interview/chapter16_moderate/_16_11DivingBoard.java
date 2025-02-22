096221c67e3e635b3553a82e47b6b6eeda16054e
Mingda Rui <mingdarui@gmail.com>
2025-02-21 23:00:49 -0800

    Solution 16.11 Diving Board - bug fixing

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_11DivingBoard.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_11DivingBoard.java
@@ -1,27 +1,26 @@
 package pers.mingda.cracking_the_coding_interview.chapter16_moderate;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class _16_11DivingBoard {
     Set<Integer> allLengths(int k, int shorter, int longer) {
-        Map<Integer, Set<Integer>> memory = new HashMap<>();
-        return getLength(k, shorter, longer, 0, memory);
-    }
-
-    Set<Integer> getLength(int k, int shorter, int longer, int currentLen, Map<Integer, Set<Integer>> memory) {
-        if (memory.containsKey(k)) {
-            return memory.get(k);
-        }
         if (k == 0) {
-            return Set.of(currentLen);
+            return new HashSet<>();
         }
+
+        if (k == 1) {
+            Set<Integer> set = new HashSet<>();
+            set.add(shorter);
+            set.add(longer);
+            return set;
+        }
+
         Set<Integer> set = new HashSet<>();
-        set.addAll(getLength(k - 1, shorter, longer, currentLen + shorter, memory));
-        set.addAll(getLength(k - 1, shorter, longer, currentLen + longer, memory));
-        memory.put(k, set);
+        for (int len: allLengths(k - 1, shorter, longer)) {
+            set.add(len + shorter);
+            set.add(len + longer);
+        }
         return set;
     }
 }
