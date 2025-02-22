b10f7c5d990a847bfcdb951b67f9d7dd0f2f8d71
Mingda Rui <mingdarui@gmail.com>
2025-02-21 21:29:15 -0800

    Solution 16.10 Living People - bug fixing

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_10LivingPeople.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_10LivingPeople.java
@@ -1,14 +1,35 @@
 package pers.mingda.cracking_the_coding_interview.chapter16_moderate;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class _16_10LivingPeople {
     int maxAliveYear(LivingPerson[] people, int min, int max) {
-        int count = 0;
-        for (LivingPerson person : people) {
-            if (person.birth >= min && person.birth <= max) {
-                count++;
+        LivingPerson[] sortedByBirth = Arrays.copyOf(people, people.length);
+        Arrays.sort(sortedByBirth, Comparator.comparingInt(a -> a.birth));
+
+        LivingPerson[] sortedByDeath = Arrays.copyOf(people, people.length);
+        Arrays.sort(sortedByDeath, Comparator.comparingInt(a -> a.death));
+
+        int birthIndex = 0;
+        int deathIndex = 0;
+
+        int currentLives = 0;
+        int maxLives = 0;
+
+        for (int year = min; year <= max; year++) {
+            while (birthIndex < sortedByBirth.length && year >= sortedByBirth[birthIndex].birth) {
+                birthIndex++;
+                currentLives++;
+            }
+            maxLives = Math.max(maxLives, currentLives);
+            while(deathIndex < sortedByDeath.length && year > sortedByDeath[deathIndex].death) {
+                deathIndex++;
+                currentLives--;
             }
         }
-        return count;
+
+        return maxLives;
     }
 }
 
