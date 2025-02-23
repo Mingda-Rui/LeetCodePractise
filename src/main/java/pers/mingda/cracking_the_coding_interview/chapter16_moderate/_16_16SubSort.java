2ac1743f0e1706277ae3366c78840c5816a754c2
Mingda Rui <mingdarui@gmail.com>
2025-02-22 20:13:31 -0800

    Solution 16.16 Sub Sort - bug fixing

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_16SubSort.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter16_moderate/_16_16SubSort.java
@@ -1,30 +1,70 @@
 package pers.mingda.cracking_the_coding_interview.chapter16_moderate;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class _16_16SubSort {
     List<Integer> findUnsortedSequence(int[] array) {
-        int[] sorted = Arrays.copyOf(array, array.length);
-        Arrays.sort(sorted);
-        int findLeft = -1;
-        for (int i = 0; i < sorted.length - 1; i++) {
-            if (sorted[i] != array[i]) {
-                findLeft = i;
-                break;
-            }
-        }
-        if (findLeft == -1) {
-            return List.of();
-        }
+        int leftMostSortedIndex = findLeftmostSorted(array);
+        int rightMostSortedIndex = findRightmostSorted(array);
 
-        int findRight = -1;
-        for (int i = sorted.length - 1; i >= 0; i--) {
-            if (sorted[i] != array[i]) {
-                findRight = i;
-                break;
+        int minInRight = findMinFrom(array, leftMostSortedIndex);
+        int maxInLeft = findMaxTill(array, rightMostSortedIndex);
+
+        int adjustedLeftMost = adjustLeftWithMin(array, leftMostSortedIndex, minInRight);
+        int adjustedRightMost = adjustRightWithMax(array, rightMostSortedIndex, maxInLeft);
+
+        return List.of(adjustedLeftMost + 1, adjustedRightMost - 1);
+    }
+
+    int findLeftmostSorted(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i - 1] > array[i]) {
+                return i - 1;
             }
         }
-        return List.of(findLeft, findRight);
+        return array.length - 1;
+    }
+
+    int findRightmostSorted(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            if (array[i - 1] > array[i]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    int findMinFrom(int[] array, int from) {
+        int max = 0;
+        for (int i = from; i < array.length; i++) {
+            max = Math.max(max, array[i]);
+        }
+        return max;
+    }
+
+    int findMaxTill(int[] array, int till) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i <= till; i++) {
+            min = Math.min(min, array[i]);
+        }
+        return min;
+    }
+
+    int adjustLeftWithMin(int[] array, int leftEndIndex, int min) {
+        for (int i = leftEndIndex; i >= 0; i--) {
+            if (array[i] <= min) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    int adjustRightWithMax(int[] array, int rightStartIndex, int max) {
+        for (int i = rightStartIndex; i < array.length; i++) {
+            if (array[i] >= max) {
+                return i;
+            }
+        }
+        return array.length;
     }
 }
