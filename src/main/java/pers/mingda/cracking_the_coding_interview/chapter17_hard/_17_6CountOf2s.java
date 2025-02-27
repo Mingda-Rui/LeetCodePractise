7d1c8bb732026b239a00ab882e847cfcb6d8a2c6
Mingda Rui <mingdarui@gmail.com>
2025-02-26 23:33:14 -0800

    Solution 17.6 Count of 2s - brute force solution: bug fixing

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_6CountOf2s.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_6CountOf2s.java
@@ -1,6 +1,14 @@
 package pers.mingda.cracking_the_coding_interview.chapter17_hard;
 
 public class _17_6CountOf2s {
+    int numberOf2sInRange(int n) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            count += numberOf2s(i);
+        }
+        return count;
+    }
+
     int numberOf2s(int n) {
         int count = 0;
         while (n > 0) {
