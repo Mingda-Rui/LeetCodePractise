c1bda349570237b9780d534fd8976d9cb430f170
Mingda Rui <mingdarui@gmail.com>
2025-02-27 23:35:36 -0800

    Solution 17.9 Kth Multiple - alternative solution

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_9KthMultiple.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_9KthMultiple.java
@@ -1,7 +1,9 @@
 package pers.mingda.cracking_the_coding_interview.chapter17_hard;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class _17_9KthMultiple {
     int getKthMagicNumber(int k) {
@@ -34,5 +36,38 @@ public class _17_9KthMultiple {
             }
         }
         return nums.get(k - 1);
+    }
+
+    int getKthMagicNumberAlternativeSolution(int k) {
+        if (k < 0) {
+            return 0;
+        }
+        int val = 0;
+        Queue<Integer> queue3 = new LinkedList<>();
+        Queue<Integer> queue5 = new LinkedList<>();
+        Queue<Integer> queue7 = new LinkedList<>();
+        queue3.add(1);
+
+        /* Include 0th through kth iteration */
+        for (int i = 0; i <= k; i++) {
+            int v3 = queue3.isEmpty() ? Integer.MAX_VALUE : queue3.peek();
+            int v5 = queue5.isEmpty() ? Integer.MAX_VALUE : queue5.peek();
+            int v7 = queue7.isEmpty() ? Integer.MAX_VALUE : queue7.peek();
+            val = Math.min(v3, Math.min(v5, v7));
+            if (val == 3) { // enqueue into queue 3, 5, and 7
+                queue3.remove();
+                queue3.add(3 * val);
+                queue5.add(5 * val);
+                queue7.add(7 * val);
+            } else if (val == 5) { // enqueue into queue 5 and 7
+                queue5.remove();
+                queue5.add(5 * val);
+                queue7.add(7 * val);
+            } else if (val == 7) { // enqueue into Q7
+                queue7.remove();
+                queue7.add(7 * val);
+            }
+        }
+        return val;
     }
 }
