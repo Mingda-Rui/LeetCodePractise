6d634e40eb2c8e915576083bdf541df970e17c0f
Mingda Rui <mingdarui@gmail.com>
2025-03-01 12:20:52 -0800

    Solution 17.14 Smallest K - max heap solution

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_14SmallestK.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_14SmallestK.java
@@ -1,10 +1,24 @@
 package pers.mingda.cracking_the_coding_interview.chapter17_hard;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class _17_14SmallestK {
     int[] smallestK(int[] array, int k) {
         Arrays.sort(array);
         return Arrays.copyOf(array, k);
+    }
+
+    int[] smallestKMaxHeap(int[] array, int k) {
+        Queue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int num : array) {
+            queue.add(num);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+        return queue.stream().mapToInt(Integer::intValue).toArray();
     }
 }
