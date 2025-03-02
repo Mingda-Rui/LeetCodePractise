308e4da6ac6329e4c0043bd5304441437b41cf27
Mingda Rui <mingdarui@gmail.com>
2025-03-01 19:06:07 -0800

    Solution 17.14 Smallest K - selection rank solution

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_14SmallestK.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_14SmallestK.java
@@ -21,4 +21,31 @@ public class _17_14SmallestK {
         }
         return queue.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
+
+    int[] smallestKSelectionRank(int[] array, int k) {
+        return selectionSort(array, k, 0, array.length);
+    }
+
+    int[] selectionSort(int[] array, int target, int start, int boundary) {
+        int pivotIndex = start;
+        for (int i = start + 1; i < boundary; i++) {
+            if (array[i] < array[pivotIndex]) {
+                swap(array, i, pivotIndex);
+                pivotIndex = i;
+            }
+        }
+        if (pivotIndex < target - 1) {
+            return selectionSort(array, target, pivotIndex + 1, boundary);
+        } else if (pivotIndex > target - 1) {
+            return selectionSort(array, target - pivotIndex, start, pivotIndex);
+        } else {
+            return Arrays.copyOf(array, pivotIndex);
+        }
+    }
+
+    void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
