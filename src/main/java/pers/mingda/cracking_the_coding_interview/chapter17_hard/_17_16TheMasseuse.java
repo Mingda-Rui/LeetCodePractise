168b81548d8b51019e912c10bd3399060cb2ff15
Mingda Rui <mingdarui@gmail.com>
2025-03-01 23:17:00 -0800

    Solution 17.16 The Masseuse - DP

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_16TheMasseuse.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_16TheMasseuse.java
@@ -1,17 +1,26 @@
 package pers.mingda.cracking_the_coding_interview.chapter17_hard;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class _17_16TheMasseuse {
     int maxMinutes(int[] massages) {
-        return maxMinutes(massages, 0);
+        Map<Integer, Integer> record = new HashMap<>();
+        return maxMinutes(massages, 0, record);
     }
 
-    int maxMinutes(int[] massages, int next) {
+    int maxMinutes(int[] massages, int next, Map<Integer, Integer> record) {
+        if (record.containsKey(next)) {
+            return record.get(next);
+        }
         if (next == massages.length) {
             return 0;
         }
 
-        int with = massages[next] + maxMinutes(massages, next + 2);
-        int without = maxMinutes(massages, next + 1);
-        return Math.max(with, without);
+        int with = massages[next] + maxMinutes(massages, next + 2, record);
+        int without = maxMinutes(massages, next + 1, record);
+        int max = Math.max(with, without);
+        record.put(next, max);
+        return max;
     }
 }
