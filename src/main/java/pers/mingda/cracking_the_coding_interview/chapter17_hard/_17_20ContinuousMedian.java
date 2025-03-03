7e7fd23fb3d7e6ab4d5b1f9ce31aece6952a82f9
Mingda Rui <mingdarui@gmail.com>
2025-03-02 23:20:55 -0800

    Solution 17.20 Continuous Median - average median for even number

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_20ContinuousMedian.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_20ContinuousMedian.java
@@ -1,23 +1,24 @@
 package pers.mingda.cracking_the_coding_interview.chapter17_hard;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class _17_20ContinuousMedian {
-    PriorityQueue<Integer> larger;
-    int totalNum;
+    PriorityQueue<Integer> larger = new PriorityQueue<>();
+    PriorityQueue<Integer> smaller = new PriorityQueue<>(Comparator.reverseOrder());
 
     void addNumber(int number) {
         larger.add(number);
-        totalNum++;
-        if (larger.size() * 2 - totalNum > 1) {
-            larger.poll();
+        if (larger.size() - smaller.size() > 1) {
+            int num = larger.poll();
+            smaller.add(num);
         }
     }
 
     int getMedian() {
-        if (!larger.isEmpty()) {
-            return larger.peek();
+        if (larger.size() == smaller.size()) {
+            return (larger.peek() + smaller.peek()) / 2;
         }
-        return -1;
+        return larger.peek();
     }
 }
