1d5a1024692ded6501fe441acb89045b9b493736
Mingda Rui <mingdarui@gmail.com>
2025-03-02 22:16:13 -0800

    17.18 Shortest Super Sequence - min heap solution

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_18ShortestSuperSequence.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_18ShortestSuperSequence.java
@@ -1,5 +1,7 @@
 package pers.mingda.cracking_the_coding_interview.chapter17_hard;
 
+import java.util.*;
+
 public class _17_18ShortestSuperSequence {
     int[] shortestSuperSequence(int[] bigArray, int[] smallArray) {
         int[][] locMap = buildLocMap(bigArray, smallArray);
@@ -46,5 +48,64 @@ public class _17_18ShortestSuperSequence {
             max = Math.max(max, num);
         }
         return max;
+    }
+
+    int[] shortestSuperSequenceMinHeap(int[] bigArray, int[] smallArray) {
+        List<Queue<Integer>> locs = buildLocs(bigArray, smallArray);
+        return computeShortestSeq(locs);
+    }
+
+    List<Queue<Integer>> buildLocs(int[] bigArray, int[] smallArray) {
+        Map<Integer, Queue<Integer>> map = new HashMap<>();
+        for (int small: smallArray) {
+            map.put(small, new LinkedList<>());
+        }
+
+        for (int i = 0; i < bigArray.length; i++) {
+            int val = bigArray[i];
+            if (map.containsKey(val)) {
+                map.get(val).add(i);
+            }
+        }
+        return map.values().stream().toList();
+    }
+
+    int[] computeShortestSeq(List<Queue<Integer>> locs) {
+        PriorityQueue<ShortestSuperSequenceNode> minHeap = new PriorityQueue<>();
+        int max = 0;
+        for (int i = 0; i < locs.size(); i++) {
+            int headIndex = locs.get(i).remove();
+            max = Math.max(max, headIndex);
+            minHeap.add(new ShortestSuperSequenceNode(i, headIndex));
+        }
+
+        int start = 0;
+        int end = Integer.MAX_VALUE;
+
+        while (true) {
+            ShortestSuperSequenceNode minNode = minHeap.poll();
+            if (max - minNode.index <= end - start) {
+                end = max;
+                start = minNode.index;
+            }
+            if (locs.get(minNode.val).isEmpty()) {
+                break;
+            }
+            int newHeadIndex = locs.get(minNode.val).remove();
+            max = Math.max(max, newHeadIndex);
+            minHeap.add(new ShortestSuperSequenceNode(minNode.val, newHeadIndex));
+        }
+
+        return new int[] {start, end};
+    }
+}
+
+class ShortestSuperSequenceNode {
+    int val;
+    int index;
+
+    public ShortestSuperSequenceNode(int val, int index) {
+        this.val = val;
+        this.index = index;
     }
 }
