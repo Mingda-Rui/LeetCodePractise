4f86fbc0a5653cef6d25700555723df5a00b77cf
Mingda Rui <mingdarui@gmail.com>
2025-03-03 22:19:56 -0800

    Solution 17.21 Volume of Histogram - simplified implementation

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_21VolumeOfHistogram.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_21VolumeOfHistogram.java
@@ -22,25 +22,10 @@ public class _17_21VolumeOfHistogram {
 
         int max = 0;
         for (int i = 0; i < leftHighestIndex.length; i++) {
-            int lH = leftHighestIndex[i];
-            int rH = rightHighestIndex[i];
+            int height = Math.min(leftHighestIndex[i], rightHighestIndex[i]);
             int currentHeight = histogram[i];
-            if (histogram[lH] > currentHeight && histogram[rH] > currentHeight) {
-                continue;
-            }
-            max += calculateHistogramVolume(histogram, lH, i);
-
+            max += Math.max(0, height - currentHeight);
         }
         return max;
-    }
-
-    int calculateHistogramVolume(int[] histogram, int a, int b) {
-        int height = Math.min(histogram[a], histogram[b]);
-        int width = Math.abs(a - b - 1);
-        int offset = 0;
-        for (int i = a + 1; i < b; i++) {
-            offset -= histogram[i];
-        }
-        return height * width - offset;
     }
 }
