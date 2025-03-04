5f7c939ecb89549cef94eafe765197165704858d
Mingda Rui <mingdarui@gmail.com>
2025-03-03 23:58:16 -0800

    Solution 17.22 Word Transformer - code refactor

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_22WordTransformer.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_22WordTransformer.java
@@ -1,13 +1,20 @@
 package pers.mingda.cracking_the_coding_interview.chapter17_hard;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class _17_22WordTransformer {
     List<String> transform(String start, String stop, String[] words) {
-        Map<String, WordTransformer> graph = buildGraph(words);
-        WordTransformer startNode = graph.get(start);
-        WordTransformer stopNode = graph.get(stop);
-        Queue<WordTransformer> queue = new LinkedList<>();
+        Map<String, WordTransformerGraphNode> graph = buildGraph(words);
+        WordTransformerGraphNode startNode = graph.get(start);
+        WordTransformerGraphNode stopNode = graph.get(stop);
+        Queue<WordTransformerGraphNode> queue = new LinkedList<>();
         Set<String> seen = new HashSet<>();
         Map<String, List<String>> paths = new HashMap<>();
         queue.add(startNode);
@@ -16,12 +23,12 @@ public class _17_22WordTransformer {
         return findStop(stopNode, seen, queue, paths);
     }
 
-    Map<String, WordTransformer> buildGraph(String[] words) {
-        Map<String, WordTransformer> graph = new HashMap<>();
+    Map<String, WordTransformerGraphNode> buildGraph(String[] words) {
+        Map<String, WordTransformerGraphNode> graph = new HashMap<>();
         for (String word : words) {
-            WordTransformer node = new WordTransformer(word, new HashSet<>());
+            WordTransformerGraphNode node = new WordTransformerGraphNode(word, new HashSet<>());
             for (String value : graph.keySet()) {
-                WordTransformer prev = graph.get(value);
+                WordTransformerGraphNode prev = graph.get(value);
                 if (oneLetterApart(prev, node)) {
                     prev.siblings().add(node);
                     node.siblings().add(prev);
@@ -33,7 +40,7 @@ public class _17_22WordTransformer {
         return graph;
     }
 
-    boolean oneLetterApart(WordTransformer node1, WordTransformer node2) {
+    boolean oneLetterApart(WordTransformerGraphNode node1, WordTransformerGraphNode node2) {
         int count = 0;
         for (int i = 0; i < node1.val().length(); i++) {
             if (node1.val().charAt(i) != node2.val().charAt(i)) {
@@ -43,17 +50,17 @@ public class _17_22WordTransformer {
         return count == 1;
     }
 
-    List<String> findStop(WordTransformer stop, Set<String> seen, Queue<WordTransformer> queue, Map<String, List<String>> paths) {
+    List<String> findStop(WordTransformerGraphNode stop, Set<String> seen, Queue<WordTransformerGraphNode> queue, Map<String, List<String>> paths) {
         if (queue.isEmpty()) {
             return List.of();
         }
-        WordTransformer current = queue.poll();
+        WordTransformerGraphNode current = queue.poll();
         List<String> path = paths.get(current.val());
         if (current.val().equals(stop.val())) {
             return path;
         }
 
-        for (WordTransformer s: current.siblings()) {
+        for (WordTransformerGraphNode s: current.siblings()) {
             if (!seen.contains(s.val())) {
                 seen.add(s.val());
                 queue.offer(s);
@@ -67,4 +74,4 @@ public class _17_22WordTransformer {
     }
 }
 
-record WordTransformer(String val, Set<WordTransformer> siblings) {}
+record WordTransformerGraphNode(String val, Set<WordTransformerGraphNode> siblings) {}
