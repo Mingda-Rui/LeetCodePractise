22fa9fbb01d6fe5224006ce743854f0bc9c5f173
Mingda Rui <mingdarui@gmail.com>
2025-03-04 23:18:52 -0800

    Solution 17.24 Max SubMatrix - DP

--- src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_24MaxSubMatrix.java
+++ src/main/java/pers/mingda/cracking_the_coding_interview/chapter17_hard/_17_24MaxSubMatrix.java
@@ -2,19 +2,40 @@ package pers.mingda.cracking_the_coding_interview.chapter17_hard;
 
 public class _17_24MaxSubMatrix {
     int getMaxMatrix(int[][] matrix) {
+        int[][] preComputedSum = preComputedSum(matrix);
         int max = 0;
         for (int top = 0; top < matrix.length; top++) {
             for (int bottom = top; bottom < matrix.length; bottom++) {
                 for (int left = 0; left < matrix[0].length; left++) {
                     for (int right = left; right < matrix[0].length; right++) {
-                        int sum = calculateSum(matrix, top, bottom, left, right);
+                        int total = preComputedSum[bottom][right];
+                        int topSum = preComputedSum[top][right];
+                        int leftSum = preComputedSum[bottom][left];
+                        int topLeftSum = preComputedSum[top][left];
+                        int sum = total - topSum - leftSum + topLeftSum;
                         max = Math.max(max, sum);
                     }
-
                 }
             }
         }
         return max;
+    }
+
+    int[][] preComputedSum(int[][] matrix) {
+        int[][] preComputedSum = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                preComputedSum[i][j] = calculateSum(matrix, preComputedSum, i, j);
+            }
+        }
+        return preComputedSum;
+    }
+
+    int calculateSum(int[][] matrix, int[][] preComputedSum, int row, int col) {
+        int leftSum = col - 1 < 0 ? 0 : preComputedSum[row][col - 1];
+        int topSum = row - 1 < 0 ? 0 : preComputedSum[row - 1][col];
+        int topLeftSum = col - 1 < 0 || row - 1 < 0 ? 0 : preComputedSum[row - 1][col - 1];
+        return leftSum + topSum - topLeftSum + matrix[row][col];
     }
 
     int calculateSum(int[][] matrix, int top, int bottom, int left, int right) {
