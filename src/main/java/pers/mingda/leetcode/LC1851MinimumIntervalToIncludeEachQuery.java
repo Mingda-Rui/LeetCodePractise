07ea5c70a53f4963afbe9ce4c658d3627730ed3f
Mingda Rui <mingdarui@gmail.com>
2025-03-09 22:10:33 -0700

    LeetCode 1851 Minimum Interval to Include Each Query - Priority Queue

--- src/main/java/pers/mingda/leetcode/LC1851MinimumIntervalToIncludeEachQuery.java
+++ src/main/java/pers/mingda/leetcode/LC1851MinimumIntervalToIncludeEachQuery.java
@@ -1,30 +1,54 @@
 package pers.mingda.leetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class LC1851MinimumIntervalToIncludeEachQuery {
     public int[] minInterval(int[][] intervals, int[] queries) {
+        Queue<int[]> pq = new PriorityQueue<>((i1, i2) -> (i1[1] - i1[0]) - (i2[1] - i2[0]));
+        int[] copied = Arrays.copyOf(queries, queries.length);
+        Arrays.sort(copied);
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+
+        // query : smallestInterval
+        Map<Integer, Integer> smallestQueries = new HashMap<>();
         int[] result = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            result[i] = findSmallest(intervals, queries[i]);
-        }
-        return result;
-    }
 
-    private int findSmallest(int[][] intervals, int query) {
-        int min = Integer.MAX_VALUE;
-        for (int[] interval: intervals) {
-            if (isInclude(interval, query)) {
-                int size = getSize(interval);
-                min = Math.min(size, min);
+        int intervalIndex = 0;
+        for (int query : copied) {
+            if (smallestQueries.containsKey(query)) {
+                continue;
             }
-        }
-        return min == Integer.MAX_VALUE ? -1 : min;
-    }
+            while (intervalIndex < intervals.length && intervals[intervalIndex][0] <= query) {
+                pq.add(intervals[intervalIndex]);
+                intervalIndex++;
+            }
 
-    private boolean isInclude(int[] interval, int query) {
-        return interval[0] <= query && interval[1] >= query;
+            while (!pq.isEmpty() && pq.peek()[1] < query) {
+                pq.remove();
+            }
+
+            int smallestInterval = pq.isEmpty() ? -1 : getSize(pq.peek());
+            smallestQueries.put(query, smallestInterval);
+        }
+
+        return buildResult(queries, smallestQueries);
     }
 
     private int getSize(int[] interval) {
         return interval[1] - interval[0] + 1;
+    }
+
+    private int[] buildResult(int[] queries, Map<Integer, Integer> map) {
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int query = queries[i];
+            result[i] = map.get(query);
+        }
+        return result;
     }
 }
