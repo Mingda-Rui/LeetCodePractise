91b1a451e76755f52baafc0e00a41c49c7a3e7b1
Mingda Rui <mingdarui@gmail.com>
2025-03-20 00:36:30 -0700

    LC0309 Best Time to Buy and Sell with Cooldown

--- src/main/java/pers/mingda/leetcode/LC0309BestTimeToBuyAndSellStockWithCooldown.java
+++ src/main/java/pers/mingda/leetcode/LC0309BestTimeToBuyAndSellStockWithCooldown.java
@@ -1,20 +1,51 @@
 package pers.mingda.leetcode;
 
+import java.util.Arrays;
+
 public class LC0309BestTimeToBuyAndSellStockWithCooldown {
     public int maxProfit(int[] prices) {
-        int[] sellProfit = new int[prices.length];
-        int currentBuyingIndex = 0;
-        for (int i = 1; i < sellProfit.length; i++) {
-            int val = prices[i];
-            if (val <= prices[i - 1]) { // what about <=
-                currentBuyingIndex = i;
-                sellProfit[i] = sellProfit[i - 1];
-            } else {
-                int prevSellProfit = currentBuyingIndex - 2 >= 0 ? sellProfit[currentBuyingIndex - 2] : 0;
-                int profitIfSell = prevSellProfit + val - prices[currentBuyingIndex];
-                sellProfit[i] = Math.max(profitIfSell, sellProfit[i - 1]);
-            }
+        int len = prices.length;
+        int[][] profits = new int[len][len];
+        initProfits(profits);
+        int[] startDateProfits = new int[len];
+        Arrays.fill(startDateProfits, -1);
+        int[] anchoredStartDateP = new int[len];
+        Arrays.fill(anchoredStartDateP, -1);
+        return maxProfit(prices, profits, startDateProfits, anchoredStartDateP, 0);
+
+    }
+
+    private void initProfits(int[][] profits) {
+        for (int[] profit : profits) {
+            Arrays.fill(profit, -1);
         }
-        return sellProfit[sellProfit.length - 1];
+    }
+
+    private int maxProfit(int[] prices, int[][] profits, int[] startDateProfits, int[] anchoredStartDateP, int startDate) {
+        if (startDate >= startDateProfits.length) {
+            return 0;
+        }
+        if (startDateProfits[startDate] != -1) {
+            return startDateProfits[startDate];
+        }
+        int max = 0;
+        for (int buy = startDate; buy < prices.length; buy++) {
+            if (anchoredStartDateP[buy] != -1) {
+                max = Math.max(max, anchoredStartDateP[buy]);
+                continue;
+            }
+            int localMax = 0;
+            for (int sell = buy + 1; sell < prices.length; sell++) {
+                if (profits[buy][sell] == -1) {
+                    int remainingProfit = maxProfit(prices, profits, startDateProfits, anchoredStartDateP, sell + 2);
+                    profits[buy][sell] = prices[sell] - prices[buy] + remainingProfit;
+                }
+                max = Math.max(max, profits[buy][sell]);
+                localMax = Math.max(localMax, profits[buy][sell]);
+            }
+            anchoredStartDateP[buy] = localMax;
+        }
+        startDateProfits[startDate] = max;
+        return max;
     }
 }
