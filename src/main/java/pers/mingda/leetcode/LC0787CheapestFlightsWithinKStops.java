6e0a94f04e7953a42598e6568032a5966585825d
Mingda Rui <mingdarui@gmail.com>
2025-03-19 23:12:51 -0700

    LC0787 Cheapest Flights within K Stops - Dijkstra solution

--- src/main/java/pers/mingda/leetcode/LC0787CheapestFlightsWithinKStops.java
+++ src/main/java/pers/mingda/leetcode/LC0787CheapestFlightsWithinKStops.java
@@ -1,12 +1,16 @@
 package pers.mingda.leetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class LC0787CheapestFlightsWithinKStops {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
@@ -51,9 +55,7 @@ public class LC0787CheapestFlightsWithinKStops {
         Map<Integer, Set<List<Integer>>> flightMap = new HashMap<>();
         for (int[] flight: flights) {
             int from = flight[0];
-            int to = flight[1];
-            int price = flight[2];
-            flightMap.computeIfAbsent(from, k -> new HashSet<>()).add(List.of(from, to, price));
+            flightMap.computeIfAbsent(from, k -> new HashSet<>()).add(Arrays.stream(flight).boxed().collect(Collectors.toList()));
         }
         return flightMap;
     }
@@ -85,5 +87,42 @@ public class LC0787CheapestFlightsWithinKStops {
             k--;
         }
         return cheapestPrice.getOrDefault(dst, -1);
+    }
+
+    public int findCheapestPriceDijkstra(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, Set<List<Integer>>> flightMap = buildFlightMap(flights);
+        Queue<List<Integer>> minHeap = new PriorityQueue<>(Comparator.comparingInt(f -> f.get(1)));
+        // node, dist_from_src, number_of_stops_from_src
+        minHeap.add(List.of(src, 0, -1));
+        Map<Integer, Integer> stopCounter = new HashMap<>();
+
+        while(!minHeap.isEmpty()) {
+            List<Integer> current = minHeap.poll();
+            int node = current.getFirst();
+            int totalDistance = current.get(1);
+            int stop = current.getLast();
+            if (node == dst) {
+                return current.get(1);
+            }
+            if (stop >= k) {
+                continue;
+            }
+            if (stopCounter.containsKey(node) && stopCounter.get(node) <= stop) {
+                continue;
+            }
+
+            stopCounter.put(node, stop);
+
+            if (!flightMap.containsKey(node)) {
+                continue;
+            }
+
+            for (List<Integer> flight: flightMap.get(node)) {
+                int next = flight.get(1);
+                int distance = flight.getLast();
+                minHeap.add(List.of(next, totalDistance + distance, stop + 1));
+            }
+        }
+        return -1;
     }
 }
