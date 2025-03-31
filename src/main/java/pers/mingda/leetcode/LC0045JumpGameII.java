eb49af77061f0dbcf50c331e73a2302f57ade8eb
Mingda Rui <mingdarui@gmail.com>
2025-03-30 22:47:35 -0700

    LC0045 Jump Game II

---
+++ src/main/java/pers/mingda/leetcode/LC0045JumpGameII.java
@@ -0,0 +1,18 @@
+package pers.mingda.leetcode;
+
+public class LC0045JumpGameII {
+    public int jump(int[] nums) {
+        return countJump(nums, 0, 1, 0);
+    }
+
+    private int countJump(int[] nums, int start, int end, int jumpCount) {
+        if (end >= nums.length) {
+            return jumpCount;
+        }
+        int max = start;
+        for (int i = start; i < end; i++) {
+            max = Math.max(max, i + nums[i]);
+        }
+        return countJump(nums, end, max + 1, jumpCount + 1);
+    }
+}
