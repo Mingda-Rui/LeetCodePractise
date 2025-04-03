7d72d25ba3715943647ef8d34729d3d2054b6d31
Mingda Rui <mingdarui@gmail.com>
2025-04-02 23:12:49 -0700

    LC1732 Find the Highest Altitude

---
+++ src/main/java/pers/mingda/leetcode/LC1732FindTheHighestAltitude.java
@@ -0,0 +1,13 @@
+package pers.mingda.leetcode;
+
+public class LC1732FindTheHighestAltitude {
+    public int largestAltitude(int[] gain) {
+        int highest = 0;
+        int current = 0;
+        for (int netGain: gain) {
+            current += netGain;
+            highest = Math.max(current, highest);
+        }
+        return highest;
+    }
+}
