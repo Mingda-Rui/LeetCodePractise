d70d81313be0d25bac4d94500596484d698d59ed
Mingda Rui <mingdarui@gmail.com>
2025-04-04 00:09:17 -0700

    LC0846 Hand of Straights - optimized solution with map

--- src/main/java/pers/mingda/leetcode/LC0846HandOfStraights.java
+++ src/main/java/pers/mingda/leetcode/LC0846HandOfStraights.java
@@ -2,40 +2,34 @@ package pers.mingda.leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class LC0846HandOfStraights {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if (hand.length % groupSize != 0) {
-            return false;
-        }
-        Queue<Integer> queue = new PriorityQueue<>();
+        Map<Integer, Integer> map = new TreeMap<>();
         for (int num: hand) {
-            queue.add(num);
+            int val = map.getOrDefault(num, 0) + 1;
+            map.put(num, val);
         }
-        int current = 0;
-        List<Integer> cache = new ArrayList<>();
-        for (int i = 0; i < hand.length; i++) {
-            if (i % groupSize == 0) {
-                queue.addAll(cache);
-                current = queue.poll();
-                cache.clear();
-            } else {
-                while (!queue.isEmpty()) {
-                    int next = queue.poll();
-                    if (current + 1 == next) {
-                        current = next;
-                        break;
-                    } else {
-                        cache.add(next);
-                    }
-                }
-                if (queue.isEmpty() && !cache.isEmpty()) {
+        List<Integer> list = new ArrayList<>(map.keySet());
+        for (int key: list) {
+            int count = map.getOrDefault(key, 0);
+            if (count == 0) {
+                continue;
+            }
+            for (int i = 0; i < groupSize; i++) {
+                int currentKey = key + i;
+                int val = map.getOrDefault(currentKey, 0) - count;
+                if (val < 0) {
                     return false;
+                } else if (val == 0) {
+                    map.remove(currentKey);
+                } else {
+                    map.put(currentKey, val);
                 }
             }
         }
-        return true;
+        return map.isEmpty();
     }
 }
