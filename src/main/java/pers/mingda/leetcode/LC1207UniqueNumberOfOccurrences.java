97173848df7913c57ad7210ce09679f3ed08e817
Mingda Rui <mingdarui@gmail.com>
2025-04-06 13:38:28 -0700

    LC1207 Unique Number of Occurrences

---
+++ src/main/java/pers/mingda/leetcode/LC1207UniqueNumberOfOccurrences.java
@@ -0,0 +1,26 @@
+package pers.mingda.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LC1207UniqueNumberOfOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num: arr) {
+            int val = map.getOrDefault(num, 0);
+            map.put(num, val + 1);
+        }
+
+        Set<Integer> count = new HashSet<>();
+        for (int num: map.keySet()) {
+            int val = map.get(num);
+            if (count.contains(val)) {
+                return false;
+            }
+            count.add(val);
+        }
+        return true;
+    }
+}
