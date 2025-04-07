0f125b85ff2861d906bd01d019fa2baa06567d47
Mingda Rui <mingdarui@gmail.com>
2025-04-06 21:56:24 -0700

    LC0050 Pow(x, n) - iterative solution

--- src/main/java/pers/mingda/leetcode/LC0050PowXN.java
+++ src/main/java/pers/mingda/leetcode/LC0050PowXN.java
@@ -2,6 +2,25 @@ package pers.mingda.leetcode;
 
 public class LC0050PowXN {
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        return binaryExp(x, (long) n);
+    }
+
+    private double binaryExp(double x, double n) {
+        if (n < 0) {
+            n = -n;
+            x = 1/x;
+        }
+
+        double result = 1;
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+                x *= x;
+            } else {
+                n -=1;
+                result *= x;
+            }
+        }
+        return result;
     }
 }
