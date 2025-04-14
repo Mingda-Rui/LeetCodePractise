e6a956e1995bc663d095083c67e1b5b5805bc072
Mingda Rui <mingdarui@gmail.com>
2025-04-14 00:04:52 -0700

    LC1372 Longest Zig Zag Path in a Binary Tree

---
+++ src/main/java/pers/mingda/leetcode/LC1372LongestZigZagPathInABinaryTree.java
@@ -0,0 +1,44 @@
+package pers.mingda.leetcode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class LC1372LongestZigZagPathInABinaryTree {
+    int max = 0;
+
+    public int longestZigZag(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftZigZag = longestZigZag(root.left, true);
+        int rightZigZag = longestZigZag(root.right, false);
+        int currentMax = Math.max(leftZigZag, rightZigZag);
+        max = Math.max(currentMax, max);
+        return max;
+    }
+
+    private int longestZigZag(TreeNode root, boolean isLeft) {
+        if (root == null) {
+            return 0;
+        }
+
+        TreeNode continueNode = isLeft ? root.right : root.left;
+        int continueZigZag = longestZigZag(continueNode, !isLeft) + 1;
+        TreeNode newHead = isLeft ? root.left : root.right;
+        int newZigZag = longestZigZag(newHead, isLeft);
+        max = Math.max(max, newZigZag);
+        return continueZigZag;
+    }
+}
