2fe0d8fbe2c274562a31bc79fc134c0b8ee9ffb9
Mingda Rui <mingdarui@gmail.com>
2025-04-22 22:23:26 -0700

    LC0219 Contains Duplicate II

---
+++ src/main/java/pers/mingda/leetcode/LC0219ContainsDuplicateII.java
@@ -0,0 +1,26 @@
+package pers.mingda.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC0219ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i - j > k) {
+                int count = map.getOrDefault(nums[j], 0);
+                map.put(nums[j], count - 1);
+                j++;
+            }
+            int num = nums[i];
+            if (map.getOrDefault(num, 0) != 0) {
+                return true;
+            }
+            map.put(num, 1);
+        }
+        return false;
+
+    }
+}
