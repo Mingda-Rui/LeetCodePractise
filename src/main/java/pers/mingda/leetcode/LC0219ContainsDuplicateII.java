713950ad4c67bc08faeaed86924f0822790e8e15
Mingda Rui <mingdarui@gmail.com>
2025-04-22 22:42:09 -0700

    LC0219 Contains Duplicate II - HashSet solution

--- src/main/java/pers/mingda/leetcode/LC0219ContainsDuplicateII.java
+++ src/main/java/pers/mingda/leetcode/LC0219ContainsDuplicateII.java
@@ -1,25 +1,21 @@
 package pers.mingda.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LC0219ContainsDuplicateII {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int j = 0;
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
-            if (i - j > k) {
-                int count = map.getOrDefault(nums[j], 0);
-                map.put(nums[j], count - 1);
-                j++;
-            }
             int num = nums[i];
-            if (map.getOrDefault(num, 0) != 0) {
+            if (set.contains(num)) {
                 return true;
             }
-            map.put(num, 1);
+            set.add(num);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
         }
         return false;
-
     }
 }
