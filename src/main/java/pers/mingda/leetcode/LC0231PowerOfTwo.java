0c6a6393c8eff63d41d315b7d3ad4634fcdb4b9a
Mingda Rui <mingdarui@gmail.com>
2025-05-03 00:50:30 -0700

    LC0231 Power of Two - bit manipulation

--- src/main/java/pers/mingda/leetcode/LC0231PowerOfTwo.java
+++ src/main/java/pers/mingda/leetcode/LC0231PowerOfTwo.java
@@ -2,12 +2,9 @@ package pers.mingda.leetcode;
 
 public class LC0231PowerOfTwo {
     public boolean isPowerOfTwo(int n) {
-        while (n != 0) {
-            if (n % 2 != 0) {
-                return n == 1;
-            }
-            n /= 2;
+        if (n <= 0) {
+            return false;
         }
-        return false;
+        return n == (n & -n);
     }
 }
