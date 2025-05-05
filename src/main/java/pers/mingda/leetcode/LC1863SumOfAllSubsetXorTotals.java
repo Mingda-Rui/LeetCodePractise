20e26a21d50137fb19d1662eddbe08fae35c3671
Mingda Rui <mingdarui@gmail.com>
2025-05-04 20:44:39 -0700

    LC1863 Sum of All Subset XOR Totals - backtracking code refactor

--- src/main/java/pers/mingda/leetcode/LC1863SumOfAllSubsetXorTotals.java
+++ src/main/java/pers/mingda/leetcode/LC1863SumOfAllSubsetXorTotals.java
@@ -5,28 +5,34 @@ import java.util.List;
 
 public class LC1863SumOfAllSubsetXorTotals {
     public int subsetXORSum(int[] nums) {
+        List<List<Integer>> allSubsets = generateSubsets(nums, 0, new ArrayList<>());
         int sum = 0;
-        for (int len = 1; len <= nums.length; len++) {
-            for (int head = 0; head + len <= nums.length; head++) {
-                sum += subsetXorSum(nums, head, len).stream().mapToInt(Integer::intValue).sum();
-            }
+        for (List<Integer> subset: allSubsets) {
+            sum += xor(subset);
         }
         return sum;
     }
 
-    private List<Integer> subsetXorSum(int[] nums, int index, int len) {
-        if (index == nums.length || len == 0) {
-            return List.of();
+    private List<List<Integer>> generateSubsets(int[] nums, int index, List<Integer> currSet) {
+        if (index == nums.length) {
+            return List.of(new ArrayList<>(currSet));
         }
-        if (len == 1) {
-            return List.of(nums[index]);
-        }
-        int current = nums[index];
-        List<Integer> result = new ArrayList<>();
-        for (int i = index + 1; i + len - 1 <= nums.length; i++) {
-            subsetXorSum(nums, i, len - 1).stream().map(num -> num ^ current).forEach(result::add);
-        }
+        int currNum = nums[index];
+        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> withoutCurr = generateSubsets(nums, index + 1, currSet);
+        currSet.add(currNum);
+        List<List<Integer>> withCurr = generateSubsets(nums, index + 1, currSet);
+        currSet.removeLast();
+        result.addAll(withoutCurr);
+        result.addAll(withCurr);
+        return result;
+    }
 
+    private int xor(List<Integer> subset) {
+        int result = 0;
+        for (int num: subset) {
+            result ^= num;
+        }
         return result;
     }
 }
