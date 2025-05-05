eb5ed2afd9ba770d5f598e0596c47bdd476874f8
Mingda Rui <mingdarui@gmail.com>
2025-05-04 21:00:35 -0700

    LC1863 Sum of All Subset XOR Totals - optimized backtracking

--- src/main/java/pers/mingda/leetcode/LC1863SumOfAllSubsetXorTotals.java
+++ src/main/java/pers/mingda/leetcode/LC1863SumOfAllSubsetXorTotals.java
@@ -1,38 +1,18 @@
 package pers.mingda.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LC1863SumOfAllSubsetXorTotals {
     public int subsetXORSum(int[] nums) {
-        List<List<Integer>> allSubsets = generateSubsets(nums, 0, new ArrayList<>());
-        int sum = 0;
-        for (List<Integer> subset: allSubsets) {
-            sum += xor(subset);
-        }
-        return sum;
+        return sumXor(nums, 0, 0);
     }
 
-    private List<List<Integer>> generateSubsets(int[] nums, int index, List<Integer> currSet) {
+    private int sumXor(int[] nums, int index, int xor) {
         if (index == nums.length) {
-            return List.of(new ArrayList<>(currSet));
+            return xor;
         }
-        int currNum = nums[index];
-        List<List<Integer>> result = new ArrayList<>();
-        List<List<Integer>> withoutCurr = generateSubsets(nums, index + 1, currSet);
-        currSet.add(currNum);
-        List<List<Integer>> withCurr = generateSubsets(nums, index + 1, currSet);
-        currSet.removeLast();
-        result.addAll(withoutCurr);
-        result.addAll(withCurr);
-        return result;
-    }
 
-    private int xor(List<Integer> subset) {
-        int result = 0;
-        for (int num: subset) {
-            result ^= num;
-        }
-        return result;
+        int currNum = nums[index];
+        int withCurr = sumXor(nums, index + 1, xor ^ currNum);
+        int withoutCurr = sumXor(nums, index + 1, xor);
+        return withCurr + withoutCurr;
     }
 }
