ed0f32d0255a0f414db385cd00268cef4d1c4834
Mingda Rui <mingdarui@gmail.com>
2025-05-06 00:22:56 -0700

    LC1466 Reorder Routes to Make All Paths Lead to the City Zero - DFS

--- src/main/java/pers/mingda/leetcode/LC1466ReorderRoutesToMakeAllPathsLeadToTheCityZero.java
+++ src/main/java/pers/mingda/leetcode/LC1466ReorderRoutesToMakeAllPathsLeadToTheCityZero.java
@@ -1,59 +1,46 @@
 package pers.mingda.leetcode;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class LC1466ReorderRoutesToMakeAllPathsLeadToTheCityZero {
     public int minReorder(int n, int[][] connections) {
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-        for (int[] connection: connections) {
+        Map<Integer, Set<List<Integer>>> map = new HashMap<>();
+        for (int[] connection : connections) {
             mapConnection(connection, map);
         }
 
-        int result = 0;
-        Set<List<Integer>> connectionSet =
-                Arrays.stream(connections)
-                        .map(arr -> Arrays.stream(arr).boxed().toList())
-                        .collect(Collectors.toSet());
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(0);
-        Set<Integer> seenCities = new HashSet<>();
-        seenCities.add(0);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int city = queue.remove();
-                for (int dest: map.get(city)) {
-                    if (seenCities.contains(dest)) {
-                        continue;
-                    }
-                    seenCities.add(dest);
-                    queue.offer(dest);
-                    if (!connectionSet.contains(List.of(dest, city))) {
-                        result++;
-                    }
-                }
-            }
-        }
-        return result;
+        return countFlip(0, -1, map);
     }
 
-    private void mapConnection(int[] connection, Map<Integer, Set<Integer>> map) {
+    private int countFlip(int city, int parentCity, Map<Integer, Set<List<Integer>>> map) {
+        int count = 0;
+        for (List<Integer> dest : map.get(city)) {
+            int destCity = dest.getFirst();
+            boolean isOriginalEdge = dest.getLast() == 1;
+            if (destCity == parentCity) {
+                continue;
+            }
+            if (isOriginalEdge) {
+                count++;
+            }
+            count += countFlip(destCity, city, map);
+        }
+        return count;
+    }
+
+    private void mapConnection(int[] connection, Map<Integer, Set<List<Integer>>> map) {
         int origin = connection[0];
         int dest = connection[1];
-        map.computeIfAbsent(origin, computeEmptySet()).add(dest);
-        map.computeIfAbsent(dest, computeEmptySet()).add(origin);
+        map.computeIfAbsent(origin, computeEmptySet()).add(List.of(dest, 1));
+        map.computeIfAbsent(dest, computeEmptySet()).add(List.of(origin, 0));
     }
 
-    private Function<Integer, Set<Integer>> computeEmptySet() {
+    private Function<Integer, Set<List<Integer>>> computeEmptySet() {
         return (key) -> new HashSet<>();
     }
 }
