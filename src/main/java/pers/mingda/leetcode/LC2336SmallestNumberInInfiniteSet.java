93941f8580f12db97bbc84c19e63f4876765fb1f
Mingda Rui <mingdarui@gmail.com>
2025-05-11 22:09:35 -0700

    LC2336 Smallest Number in Infinite Set

---
+++ src/main/java/pers/mingda/leetcode/LC2336SmallestNumberInInfiniteSet.java
@@ -0,0 +1,44 @@
+package pers.mingda.leetcode;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class LC2336SmallestNumberInInfiniteSet {
+}
+
+class SmallestInfiniteSet {
+
+    private int infinitHead;
+    private Queue<Integer> addedBackQueue;
+
+    public SmallestInfiniteSet() {
+        this.infinitHead = 1;
+        this.addedBackQueue = new PriorityQueue<>();
+    }
+
+    public int popSmallest() {
+        if (addedBackQueue.isEmpty()) {
+            int currentHead = infinitHead;
+            infinitHead++;
+            return currentHead;
+        }
+        int smallest = addedBackQueue.poll();
+        while (!addedBackQueue.isEmpty() && addedBackQueue.peek() == smallest) {
+            addedBackQueue.poll();
+        }
+        return smallest;
+    }
+
+    public void addBack(int num) {
+        if (num < infinitHead) {
+            addedBackQueue.offer(num);
+        }
+    }
+}
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+ * int param_1 = obj.popSmallest();
+ * obj.addBack(num);
+ */
