a32e472aa60117cc6b8eddfb6849531af7fa1e76
Mingda Rui <mingdarui@gmail.com>
2025-05-17 15:13:29 -0700

    LC0004 Median of Two Sorted Arrays - resolve conflict

--- src/main/java/pers/mingda/leetcode/LC0004MedianOfTwoSortedArrays.java
+++ src/main/java/pers/mingda/leetcode/LC0004MedianOfTwoSortedArrays.java
@@ -6,27 +6,27 @@ import java.util.Queue;
 
 public class LC0004MedianOfTwoSortedArrays {}
 
-class Solution {
+class LC0004Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         Queue<Integer> lowerQueue = new PriorityQueue<>(Comparator.reverseOrder());
         Queue<Integer> higherQueue = new PriorityQueue<>();
         enqueue(nums1, lowerQueue, higherQueue);
         enqueue(nums2, lowerQueue, higherQueue);
         if (lowerQueue.size() == higherQueue.size()) {
-            return ((double) lowerQueue.poll() + (double) higherQueue.poll()) / 2;
+            return ((double) lowerQueue.remove() + (double) higherQueue.remove()) / 2;
         }
-        return (double) lowerQueue.poll();
+        return (double) lowerQueue.remove();
     }
 
     private void enqueue(int[] nums, Queue<Integer> lowerQueue, Queue<Integer> higherQueue) {
         for (int num : nums) {
             if (lowerQueue.size() == higherQueue.size()) {
                 higherQueue.offer(num);
-                int tmp = higherQueue.poll();
+                int tmp = higherQueue.remove();
                 lowerQueue.offer(tmp);
             } else {
                 lowerQueue.offer(num);
-                int tmp = lowerQueue.poll();
+                int tmp = lowerQueue.remove();
                 higherQueue.offer(tmp);
             }
         }
