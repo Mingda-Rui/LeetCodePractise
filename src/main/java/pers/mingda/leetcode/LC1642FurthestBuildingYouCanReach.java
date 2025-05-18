fd3fb63d2dd08d683af06c6a5ac92d5f5fc403d4
Mingda Rui <mingdarui@gmail.com>
2025-05-18 00:33:39 -0700

    LC1642 Furthest Building You Can Reach - code refactor

---
+++ src/main/java/pers/mingda/leetcode/LC1642FurthestBuildingYouCanReach.java
@@ -0,0 +1,38 @@
+package pers.mingda.leetcode;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class LC1642FurthestBuildingYouCanReach {
+}
+
+class LC1642Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        Queue<Integer> ladderUsed = new PriorityQueue<>();
+        for (int i = 0; i < heights.length - 1; i++) {
+            int current = heights[i];
+            int next = heights[i + 1];
+            int heightDiff = next - current;
+            if (heightDiff <= 0) {
+                continue;
+            }
+
+            if (ladderUsed.size() < ladders) {
+                ladderUsed.add(heightDiff);
+                continue;
+            }
+
+            boolean shouldUseLadder = ladders != 0 && !ladderUsed.isEmpty() && ladderUsed.peek() < heightDiff;
+            if (shouldUseLadder) {
+                ladderUsed.add(heightDiff);
+            }
+            int bricksNeeded = shouldUseLadder ? ladderUsed.remove() : heightDiff;
+
+            bricks -= bricksNeeded;
+            if (bricks < 0) {
+                return i;
+            }
+        }
+        return heights.length - 1;
+    }
+}
