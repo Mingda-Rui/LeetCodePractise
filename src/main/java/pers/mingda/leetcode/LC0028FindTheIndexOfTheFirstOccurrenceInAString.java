6e76c400b65d0e0af51391d0de4488e54bdade8d
Mingda Rui <mingdarui@gmail.com>
2025-05-25 12:35:10 -0700

    LC0028 Find the Index of the First Occurrence in a String

---
+++ src/main/java/pers/mingda/leetcode/LC0028FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,26 @@
+package pers.mingda.leetcode;
+
+public class LC0028FindTheIndexOfTheFirstOccurrenceInAString {
+}
+
+class LC0028Solution {
+    public int strStr(String haystack, String needle) {
+        // 0 1 2 3 4
+        //       0 1
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            if (findNeedle(haystack, i, needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean findNeedle(String haystack, int index, String needle) {
+        for (int i = 0; i < needle.length(); i++) {
+            if (haystack.charAt(index + i) != needle.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
