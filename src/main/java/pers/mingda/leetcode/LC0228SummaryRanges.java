7ed1cff5cf2f4606ffa7f0e4d444c14d564e2c49
Mingda Rui <mingdarui@gmail.com>
2025-05-25 16:47:05 -0700

    LC0228 Summary Ranges - code refactor

--- src/main/java/pers/mingda/leetcode/LC0228SummaryRanges.java
+++ src/main/java/pers/mingda/leetcode/LC0228SummaryRanges.java
@@ -10,13 +10,13 @@ class LC0228Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
         final String arrow = "->";
+        int head = 0;
         for (int i = 0; i < nums.length; i++) {
-            int head = i;
-            while(i + 1 < nums.length && nums[i] + 1 == nums[i + 1]) {
-                i++;
+            if (i + 1 == nums.length || nums[i] + 1 != nums[i + 1]) {
+                String range = head == i ? String.valueOf(nums[head]) : nums[head] + arrow + nums[i];
+                result.add(range);
+                head = i + 1;
             }
-            String range = head == i ? String.valueOf(nums[head]) : nums[head] + arrow + nums[i];
-            result.add(range);
         }
         return result;
     }
