43d85c23fe8842d0dbf1a0723373adb45c28d3be
Mingda Rui <mingdarui@gmail.com>
2025-07-04 23:39:59 -0700

    LC1318 Minimum Flips to Make A OR B Equal to C

---
+++ src/main/java/pers/mingda/leetcode/LC1318MinimumFlipsToMakeAOrBEqualToC.java
@@ -0,0 +1,30 @@
+package pers.mingda.leetcode;
+
+public class LC1318MinimumFlipsToMakeAOrBEqualToC {}
+
+class LC1318Solution {
+
+  public int minFlips(int a, int b, int c) {
+    int result = 0;
+    while (a != 0 || b != 0 || c != 0) {
+      int aBit = a & 1;
+      int bBit = b & 1;
+      int cBit = c & 1;
+      result += flip(aBit, bBit, cBit);
+      a >>= 1;
+      b >>= 1;
+      c >>= 1;
+    }
+    return result;
+  }
+
+  private int flip(int aBit, int bBit, int cBit) {
+    if (cBit == 0) {
+      return aBit + bBit;
+    } else if (aBit == 1 || bBit == 1) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+}
