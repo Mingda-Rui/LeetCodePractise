c3e7fb318fb96165d9395999270047e22f2be11a
Mingda Rui <mingdarui@gmail.com>
2025-08-03 13:37:41 -0700

    LC0031 Next Permutation - Binary search solution

--- src/main/java/pers/mingda/leetcode/LC0031NextPermutation.java
+++ src/main/java/pers/mingda/leetcode/LC0031NextPermutation.java
@@ -38,3 +38,50 @@ class LC0031Solution {
     nums[index2] = temp;
   }
 }
+
+class LC0031BinarySearchSolution {
+
+  public void nextPermutation(int[] nums) {
+    for (int i = nums.length - 2; i >= 0; i--) {
+      if (nums[i] >= nums[i + 1]) {
+        continue;
+      }
+
+      // Returns the index of the element immediately greater than num
+      int swapIndex = binarySearch(nums, i + 1, nums.length, nums[i]);
+      swap(nums, i, swapIndex);
+      reverse(nums, i + 1);
+      return;
+    }
+
+    Arrays.sort(nums);
+  }
+
+  private int binarySearch(int[] nums, int start, int end, int target) {
+    if (start == end - 1) {
+      return target >= nums[start] ? start - 1 : start;
+    }
+    int mid = (start + end) / 2;
+    if (target >= nums[mid]) {
+      end = mid;
+    } else {
+      start = mid;
+    }
+    return binarySearch(nums, start, end, target);
+  }
+
+  private void swap(int[] nums, int index1, int index2) {
+    int temp = nums[index1];
+    nums[index1] = nums[index2];
+    nums[index2] = temp;
+  }
+
+  private void reverse(int[] nums, int start) {
+    int end = nums.length - 1;
+    while (start < end) {
+      swap(nums, start, end);
+      start++;
+      end--;
+    }
+  }
+}
