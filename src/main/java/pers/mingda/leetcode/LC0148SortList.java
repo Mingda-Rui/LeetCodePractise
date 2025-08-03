aa8d69c32cc4b9396c915009311d47533925375a
Mingda Rui <mingdarui@gmail.com>
2025-08-02 18:13:54 -0700

    LC0148 Sort List - In place merge sort solution

--- src/main/java/pers/mingda/leetcode/LC0148SortList.java
+++ src/main/java/pers/mingda/leetcode/LC0148SortList.java
@@ -54,3 +54,109 @@ class Solution {
     return merged;
   }
 }
+
+class InPlaceSortingSolution {
+
+  public ListNode sortList(ListNode head) {
+    int totalLen = countNodes(head);
+    ListNode newHead = head;
+    for (int currentLen = 1; currentLen < totalLen; currentLen *= 2) {
+      ListNode prev = null;
+      ListNode pointer = newHead;
+      while (pointer != null) {
+        ListNode result = sortTwoSubList(prev, pointer, currentLen);
+        if (prev == null) {
+          newHead = result;
+          prev = jumpTo(newHead, currentLen * 2 - 1);
+        } else {
+          prev = jumpTo(prev, currentLen * 2);
+        }
+        pointer = prev == null ? null : prev.next;
+      }
+    }
+    return newHead;
+  }
+
+  private ListNode sortTwoSubList(ListNode prev, ListNode head, int len) {
+    ListNode rightHead = head;
+
+    int leftLen = 0;
+    while (leftLen != len && rightHead != null) {
+      leftLen++;
+      rightHead = rightHead.next;
+    }
+
+    int rightLen = 0;
+    ListNode pointer = rightHead;
+    while (rightLen != len && pointer != null) {
+      rightLen++;
+      pointer = pointer.next;
+    }
+
+    return merge(prev, head, rightHead, leftLen, rightLen);
+  }
+
+  private ListNode merge(
+    ListNode prevTail,
+    ListNode leftHead,
+    ListNode rightHead,
+    int leftLen,
+    int rightLen
+  ) {
+    if (rightLen == 0) {
+      return leftHead;
+    }
+    ListNode sorted = null;
+    ListNode remainHead = jumpTo(rightHead, rightLen);
+    int totalLen = leftLen + rightLen;
+    while (leftLen != 0 && rightLen != 0) {
+      ListNode next = leftHead.val < rightHead.val ? leftHead : rightHead;
+      if (totalLen == leftLen + rightLen) {
+        sorted = next;
+      }
+      if (prevTail != null) {
+        prevTail.next = next;
+      }
+      prevTail = next;
+      if (leftHead.val < rightHead.val) {
+        leftLen--;
+        leftHead = leftHead.next;
+      } else {
+        rightLen--;
+        rightHead = rightHead.next;
+      }
+    }
+
+    ListNode next = leftLen == 0 ? rightHead : leftHead;
+    int nextLen = leftLen == 0 ? rightLen : leftLen;
+    while (nextLen != 0) {
+      prevTail.next = next;
+      prevTail = next;
+      nextLen--;
+      if (nextLen == 0) {
+        next.next = remainHead;
+      } else {
+        next = next.next;
+      }
+    }
+
+    return sorted;
+  }
+
+  private ListNode jumpTo(ListNode start, int distance) {
+    while (distance != 0 && start != null) {
+      start = start.next;
+      distance--;
+    }
+    return start;
+  }
+
+  private int countNodes(ListNode head) {
+    int count = 0;
+    while (head != null) {
+      count++;
+      head = head.next;
+    }
+    return count;
+  }
+}
