bf45aa3cf43a4179adb3b7800df2eb1ac0af75ee
Mingda Rui <mingdarui@gmail.com>
2025-08-04 22:23:37 -0700

    LC0274 H-Index - counting solution

--- src/main/java/pers/mingda/leetcode/L0274HIndex.java
+++ src/main/java/pers/mingda/leetcode/L0274HIndex.java
@@ -21,3 +21,30 @@ class LC0274Solution {
     return hIndex;
   }
 }
+
+class LC0274CountingSolution {
+
+  public int hIndex(int[] citations) {
+    int len = citations.length;
+
+    int[] record = new int[len + 1];
+    for (int cite : citations) {
+      int normalizedCite = Math.min(len, cite);
+      record[normalizedCite]++;
+    }
+
+    int paperLeft = len;
+    for (int numOfCite = 0; numOfCite < record.length; numOfCite++) {
+      int numOfPaperWithCites = record[numOfCite];
+      if (numOfPaperWithCites == 0) {
+        continue;
+      }
+
+      if (numOfCite >= paperLeft) {
+        return paperLeft;
+      }
+      paperLeft -= numOfPaperWithCites;
+    }
+    return 0;
+  }
+}
