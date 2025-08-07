40f54911495995817f41d263c0f5e8882a843838
Mingda Rui <mingdarui@gmail.com>
2025-08-06 23:55:14 -0700

    LC0135 Candy - one pass solution

--- src/main/java/pers/mingda/leetcode/LC0135Candy.java
+++ src/main/java/pers/mingda/leetcode/LC0135Candy.java
@@ -25,3 +25,54 @@ class LC0135Solution {
     return Arrays.stream(candies).sum();
   }
 }
+
+class LC0135OnePassSolution {
+
+  public int candy(int[] ratings) {
+    int len = ratings.length;
+
+    int candies = 0;
+    int i = 0;
+    int up = 0;
+    int down = 0;
+    while (i < len) {
+      while (i + 1 < len && ratings[i] == ratings[i + 1]) {
+        if ((i > 0 && ratings[i - 1] > ratings[i]) || isFlat(ratings, i)) {
+          candies++;
+        }
+        i++;
+      }
+      while (i + 1 < len && ratings[i] < ratings[i + 1]) {
+        up++;
+        i++;
+      }
+      while (i + 1 < len && ratings[i] > ratings[i + 1]) {
+        down++;
+        i++;
+      }
+      if (up != 0 || down != 0) {
+        candies += count(up) + Math.max(0, count(down) - 1) + Math.max(up, down) + 1;
+      }
+      up = 0;
+      down = 0;
+      if (i == len - 1) {
+        if (len == 1 || ratings[len - 2] >= ratings[len - 1]) {
+          candies++;
+        }
+        return candies;
+      }
+    }
+
+    return candies;
+  }
+
+  private int count(int n) {
+    return (n * (n + 1)) / 2;
+  }
+
+  private boolean isFlat(int[] ratings, int index) {
+    boolean equalsToLeft = index == 0 || ratings[index - 1] == ratings[index];
+    boolean equalsToRight = index == ratings.length - 1 || ratings[index] == ratings[index + 1];
+    return equalsToLeft && equalsToRight;
+  }
+}
