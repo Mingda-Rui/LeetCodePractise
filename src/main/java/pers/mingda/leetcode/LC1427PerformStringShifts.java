b984bc1eba21e189d66794811179d2616d987756
Mingda Rui <mingdarui@gmail.com>
2025-08-13 23:41:51 -0700

    LC1427 Perform String Shifts - use Math.floorMod

--- src/main/java/pers/mingda/leetcode/LC1427PerformStringShifts.java
+++ src/main/java/pers/mingda/leetcode/LC1427PerformStringShifts.java
@@ -6,13 +6,13 @@ class LC1427Solution {
 
   public String stringShift(String s, int[][] shift) {
     int combinedShift = calculateShifts(shift);
-    return shift(s, combinedShift % s.length());
+    return shift(s, Math.floorMod(combinedShift, s.length()));
   }
 
   private int calculateShifts(int[][] shift) {
     int combinedShift = 0;
     for (int[] shiftCommand : shift) {
-      int direction = shiftCommand[0] == 0 ? -1 : 1;
+      int direction = shiftCommand[0] == 0 ? 1 : -1;
       int amount = shiftCommand[1];
       combinedShift += (direction * amount);
     }
@@ -24,7 +24,6 @@ class LC1427Solution {
     if (s == null || s.isEmpty() || shiftAmount == 0) {
       return s;
     }
-    int newHead = (s.length() - shiftAmount) % s.length();
-    return s.substring(newHead) + s.substring(0, newHead);
+    return s.substring(shiftAmount) + s.substring(0, shiftAmount);
   }
 }
