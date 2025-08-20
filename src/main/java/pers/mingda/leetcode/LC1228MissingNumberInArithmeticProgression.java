149f5ebab9038c5da593ea673f77690afeca3146
Mingda Rui <mingdarui@gmail.com>
2025-08-19 23:43:26 -0700

    LC1228 Missing Number in Arithmetic Progression - binary search solution

--- src/main/java/pers/mingda/leetcode/LC1228MissingNumberInArithmeticProgression.java
+++ src/main/java/pers/mingda/leetcode/LC1228MissingNumberInArithmeticProgression.java
@@ -15,3 +15,26 @@ class LC1228Solution {
     return arr[0];
   }
 }
+
+class LC1228BinarySearchSolution {
+
+  public int missingNumber(int[] arr) {
+    int len = arr.length;
+    int diff = (arr[len - 1] - arr[0]) / len;
+    return binarySearch(arr, 0, len, diff);
+  }
+
+  private int binarySearch(int[] arr, int start, int end, int diff) {
+    if (start == end - 1) {
+      return arr[0] + diff * end;
+    }
+    int mid = (start + end) / 2;
+    int midVal = arr[mid];
+    int expectedVal = arr[0] + mid * diff;
+    if (midVal == expectedVal) {
+      return binarySearch(arr, mid, end, diff);
+    } else {
+      return binarySearch(arr, start, mid, diff);
+    }
+  }
+}
