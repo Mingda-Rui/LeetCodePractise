cc76baf12426991767894077724c3999a6282160
Mingda Rui <mingdarui@gmail.com>
2025-08-24 16:37:12 -0700

    LC1190 Count Substring with Only One Distinct Letter - code refactor

--- src/main/java/pers/mingda/leetcode/LC1180CountSubstringsWithOnlyOneDistinctLetter.java
+++ src/main/java/pers/mingda/leetcode/LC1180CountSubstringsWithOnlyOneDistinctLetter.java
@@ -5,30 +5,25 @@ public class LC1180CountSubstringsWithOnlyOneDistinctLetter {}
 class LC1180Solution {
 
   public int countLetters(String s) {
+    int currentHead = -1;
+    char currentChar = ' ';
     int result = 0;
-    for (int windowSize = 1; windowSize <= s.length(); windowSize++) {
-      int[] letterCount = new int[26];
-      for (int i = 0; i < s.length(); i++) {
-        letterCount[s.charAt(i) - 'a']++;
-        int prevHead = i - windowSize;
-        if (prevHead >= 0) {
-          letterCount[s.charAt(prevHead) - 'a']--;
-        }
-        if (i >= windowSize - 1 && countDistinctLetters(letterCount) == 1) {
-          result++;
-        }
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) != currentChar) {
+        result += calcCombination(s, currentHead, i - 1);
+        currentHead = i;
+        currentChar = s.charAt(i);
       }
     }
+    result += calcCombination(s, currentHead, s.length() - 1);
     return result;
   }
 
-  private int countDistinctLetters(int[] letterCount) {
-    int distinctCount = 0;
-    for (int count : letterCount) {
-      if (count > 0) {
-        distinctCount++;
-      }
+  private int calcCombination(String s, int start, int endInclusive) {
+    if (start < 0 || endInclusive < start) {
+      return 0;
     }
-    return distinctCount;
+    int len = endInclusive - start + 1;
+    return ((1 + len) * len) / 2;
   }
 }
