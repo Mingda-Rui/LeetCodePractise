c871144e19fcdcf4b1a83d37bbc82ea8624351f0
Mingda Rui <mingdarui@gmail.com>
2025-08-24 16:48:27 -0700

    LC1180 Count Substring with Only One Distinct Letter - code refactor

--- src/main/java/pers/mingda/leetcode/LC1180CountSubstringsWithOnlyOneDistinctLetter.java
+++ src/main/java/pers/mingda/leetcode/LC1180CountSubstringsWithOnlyOneDistinctLetter.java
@@ -5,25 +5,19 @@ public class LC1180CountSubstringsWithOnlyOneDistinctLetter {}
 class LC1180Solution {
 
   public int countLetters(String s) {
-    int currentHead = -1;
-    char currentChar = ' ';
+    int currentHead = 0;
     int result = 0;
-    for (int i = 0; i < s.length(); i++) {
-      if (s.charAt(i) != currentChar) {
-        result += calcCombination(s, currentHead, i - 1);
+    for (int i = 0; i <= s.length(); i++) {
+      if (i == s.length() || s.charAt(i) != s.charAt(currentHead)) {
+        result += calcCombination(s, currentHead, i);
         currentHead = i;
-        currentChar = s.charAt(i);
       }
     }
-    result += calcCombination(s, currentHead, s.length() - 1);
     return result;
   }
 
-  private int calcCombination(String s, int start, int endInclusive) {
-    if (start < 0 || endInclusive < start) {
-      return 0;
-    }
-    int len = endInclusive - start + 1;
+  private int calcCombination(String s, int start, int endExclusive) {
+    int len = endExclusive - start;
     return ((1 + len) * len) / 2;
   }
 }
