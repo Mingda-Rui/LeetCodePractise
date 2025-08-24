28ccaaa6618f14b62473e869540e9f38211c972e
Mingda Rui <mingdarui@gmail.com>
2025-08-24 16:57:20 -0700

    LC1180 Count Substring with Only One Distinct Letter - DP solution

--- src/main/java/pers/mingda/leetcode/LC1180CountSubstringsWithOnlyOneDistinctLetter.java
+++ src/main/java/pers/mingda/leetcode/LC1180CountSubstringsWithOnlyOneDistinctLetter.java
@@ -21,3 +21,24 @@ class LC1180Solution {
     return ((1 + len) * len) / 2;
   }
 }
+
+class LC1180DpSolution {
+
+  public int countLetters(String s) {
+    int sameLetterCount = 1;
+    int result = 0;
+    for (int i = 1; i <= s.length(); i++) {
+      if (i == s.length() || s.charAt(i) != s.charAt(i - 1)) {
+        result += calcArithmeticSum(sameLetterCount);
+        sameLetterCount = 1;
+      } else if (s.charAt(i) == s.charAt(i - 1)) {
+        sameLetterCount++;
+      }
+    }
+    return result;
+  }
+
+  private int calcArithmeticSum(int num) {
+    return ((1 + num) * num) / 2;
+  }
+}
