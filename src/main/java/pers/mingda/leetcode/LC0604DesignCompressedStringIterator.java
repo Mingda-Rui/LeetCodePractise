c2951c9b5ca868d6d32ac9632a7902c27c322445
Mingda Rui <mingdarui@gmail.com>
2025-09-03 23:26:08 -0700

    LC0604 Design Compressed String Iterator - code refactor

--- src/main/java/pers/mingda/leetcode/LC0604DesignCompressedStringIterator.java
+++ src/main/java/pers/mingda/leetcode/LC0604DesignCompressedStringIterator.java
@@ -2,20 +2,26 @@ package pers.mingda.leetcode;
 
 public class LC0604DesignCompressedStringIterator {}
 
-class StringIterator {
+/**
+ * Your StringIterator object will be instantiated and called as such:
+ * StringIterator obj = new StringIterator(compressedString);
+ * char param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+class LC0603StringIterator {
 
   private static final char WHITE_SPACE = ' ';
 
   private int currentCount;
+  private char currentChar;
   private int index;
-  private int compressedLetterCount;
   private final String compressedString;
 
-  public StringIterator(String compressedString) {
+  public LC0603StringIterator(String compressedString) {
     this.currentCount = 0;
+    this.currentChar = WHITE_SPACE;
     this.index = 0;
     this.compressedString = compressedString;
-    this.compressedLetterCount = getLetterCount();
   }
 
   public char next() {
@@ -23,19 +29,16 @@ class StringIterator {
       return WHITE_SPACE;
     }
 
-    currentCount++;
-    char result = compressedString.charAt(index);
-
-    if (compressedLetterCount == currentCount) {
-      index = getNextIndex();
-      currentCount = 0;
-      compressedLetterCount = getLetterCount();
+    if (currentCount == 0) {
+      currentChar = compressedString.charAt(index);
+      currentCount = getLetterCount();
     }
-    return result;
+    currentCount--;
+    return currentChar;
   }
 
   public boolean hasNext() {
-    return index < compressedString.length();
+    return index < compressedString.length() || currentCount > 0;
   }
 
   private int getLetterCount() {
@@ -44,36 +47,13 @@ class StringIterator {
       return -1;
     }
 
-    int tempIndex = index + 1;
+    index++;
     int result = 0;
-    while (tempIndex < len && isDigit(compressedString.charAt(tempIndex))) {
+    while (index < len && Character.isDigit(compressedString.charAt(index))) {
       result *= 10;
-      result += (compressedString.charAt(tempIndex) - '0');
-      tempIndex++;
+      result += (compressedString.charAt(index) - '0');
+      index++;
     }
     return result;
   }
-
-  private int getNextIndex() {
-    int len = compressedString.length();
-    if (index == len) {
-      return index;
-    }
-
-    index++;
-    while (index < len && isDigit(compressedString.charAt(index))) {
-      index++;
-    }
-    return index == len - 1 ? len : index;
-  }
-
-  private boolean isDigit(char c) {
-    return c >= '0' && c <= '9';
-  }
 }
-/**
- * Your StringIterator object will be instantiated and called as such:
- * StringIterator obj = new StringIterator(compressedString);
- * char param_1 = obj.next();
- * boolean param_2 = obj.hasNext();
- */
