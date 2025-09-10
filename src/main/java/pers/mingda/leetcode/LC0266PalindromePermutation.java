4b8cc7683e977499802295268fb19d90d7edb5a5
Mingda Rui <mingdarui@gmail.com>
2025-09-09 22:04:38 -0700

    LC0266 Palindrome Permutation - Set one pass solution

--- src/main/java/pers/mingda/leetcode/LC0266PalindromePermutation.java
+++ src/main/java/pers/mingda/leetcode/LC0266PalindromePermutation.java
@@ -1,5 +1,8 @@
 package pers.mingda.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LC0266PalindromePermutation {}
 
 class LC0266Solution {
@@ -19,5 +22,20 @@ class LC0266Solution {
       }
     }
     return true;
+  }
+}
+
+class LC0266SetSolution {
+
+  public boolean canPermutePalindrome(String s) {
+    Set<Character> set = new HashSet<>();
+    for (char c : s.toCharArray()) {
+      if (set.contains(c)) {
+        set.remove(c);
+      } else {
+        set.add(c);
+      }
+    }
+    return set.size() <= 1;
   }
 }
