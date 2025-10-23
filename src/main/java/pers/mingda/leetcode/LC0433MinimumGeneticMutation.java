e116ca6576def9df99fe9fce678617294cfffce0
Mingda Rui <mingdarui@gmail.com>
2025-10-22 23:10:40 -0700

    LC0433 Minimum Genetic Mutation - BFS solution

--- src/main/java/pers/mingda/leetcode/LC0433MinimumGeneticMutation.java
+++ src/main/java/pers/mingda/leetcode/LC0433MinimumGeneticMutation.java
@@ -1,9 +1,8 @@
 package pers.mingda.leetcode;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
 public class LC0433MinimumGeneticMutation {}
@@ -11,62 +10,40 @@ public class LC0433MinimumGeneticMutation {}
 class LC0433Solution {
 
   public int minMutation(String startGene, String endGene, String[] bank) {
-    Map<String, Set<String>> mutationMap = buildMutationMap(startGene, bank);
     Set<String> seenGenes = new HashSet<>();
-    return findMutation(startGene, endGene, mutationMap, seenGenes);
-  }
+    Queue<String> queue = new LinkedList<>();
+    queue.add(startGene);
 
-  private int findMutation(
-    String startGene,
-    String endGene,
-    Map<String, Set<String>> mutationMap,
-    Set<String> seenGenes
-  ) {
-    if (startGene.equals(endGene)) {
-      return 0;
-    }
-    int min = Integer.MAX_VALUE;
-    for (String mutation : mutationMap.get(startGene)) {
-      if (seenGenes.contains(mutation)) {
-        continue;
-      }
-      seenGenes.add(mutation);
-      int localMin = findMutation(mutation, endGene, mutationMap, seenGenes);
-
-      seenGenes.remove(mutation);
-      if (localMin != -1) {
-        min = Math.min(min, localMin);
-      }
-    }
-    return min == Integer.MAX_VALUE ? -1 : min + 1;
-  }
-
-  private Map<String, Set<String>> buildMutationMap(String startGene, String[] bank) {
-    Map<String, Set<String>> map = new HashMap<>();
-    Set<String> geneBank = new HashSet<>(Arrays.asList(bank));
-    geneBank.add(startGene);
-    for (String gene : geneBank) {
-      if (map.containsKey(gene)) {
-        continue;
-      }
-      Set<String> mutations = new HashSet<>();
-      for (String mutation : bank) {
-        if (isMutation(gene, mutation)) {
-          mutations.add(mutation);
+    int step = 0;
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        String next = queue.remove();
+        if (seenGenes.contains(next)) {
+          continue;
+        }
+        if (next.equals(endGene)) {
+          return step;
+        }
+        seenGenes.add(next);
+        for (String geneInBank : bank) {
+          if (!seenGenes.contains(geneInBank) && isMutation(next, geneInBank)) {
+            queue.add(geneInBank);
+          }
         }
       }
-      map.put(gene, mutations);
+      step++;
     }
-    return map;
+    return -1;
   }
 
-  private boolean isMutation(String startGene, String mutatedGene) {
-    int diffCount = 0;
-    if (startGene.length() != mutatedGene.length()) {
+  private boolean isMutation(String gene1, String gene2) {
+    if (gene1.length() != gene2.length()) {
       return false;
     }
-    for (int i = 0; i < startGene.length(); i++) {
-      if (startGene.charAt(i) != mutatedGene.charAt(i)) {
+    int diffCount = 0;
+    for (int i = 0; i < gene1.length(); i++) {
+      if (gene1.charAt(i) != gene2.charAt(i)) {
         diffCount++;
       }
       if (diffCount > 1) {
