6ea9d6e0a5b5ccfa3d1c74b312caf6427d2e11a7
Mingda Rui <mingdarui@gmail.com>
2025-11-18 08:49:52 -0800

    LC0063 Unique Paths II - in place solution

--- src/main/java/pers/mingda/leetcode/LC0063UniquePathsII.java
+++ src/main/java/pers/mingda/leetcode/LC0063UniquePathsII.java
@@ -5,41 +5,25 @@ public class LC0063UniquePathsII {}
 class LC0063Solution {
 
   public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    if (obstacleGrid[0][0] == 1) {
-      return 0;
-    }
     int row = obstacleGrid.length;
     int col = obstacleGrid[0].length;
-    int[][] count = new int[row][col];
-    count[row - 1][col - 1] = 1;
-    for (int i = row - 1; i >= 0; i--) {
-      for (int j = col - 1; j >= 0; j--) {
-        recordRoutes(obstacleGrid, count, i, j);
+
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        if (obstacleGrid[i][j] == 1) {
+          obstacleGrid[i][j] = 0;
+          continue;
+        }
+        if (i == 0 && j == 0) {
+          obstacleGrid[i][j] = 1;
+          continue;
+        }
+
+        int left = j == 0 ? 0 : obstacleGrid[i][j - 1];
+        int up = i == 0 ? 0 : obstacleGrid[i - 1][j];
+        obstacleGrid[i][j] = left + up;
       }
     }
-    return count[0][0];
-  }
-
-  private void recordRoutes(int[][] obstacleGrid, int[][] count, int row, int col) {
-    if (isBottomRightCorner(obstacleGrid, row, col)) {
-      return;
-    }
-
-    if (obstacleGrid[row][col] == 1) {
-      return;
-    }
-    int down = col + 1;
-    int downNeighbor = (down >= obstacleGrid[0].length || obstacleGrid[row][down] == 1)
-      ? 0
-      : count[row][down];
-    int right = row + 1;
-    int rightNeighbor = (right >= obstacleGrid.length || obstacleGrid[right][col] == 1)
-      ? 0
-      : count[right][col];
-    count[row][col] = downNeighbor + rightNeighbor;
-  }
-
-  private boolean isBottomRightCorner(int[][] obstacleGrid, int row, int col) {
-    return row == obstacleGrid.length - 1 && col == obstacleGrid[0].length - 1;
+    return obstacleGrid[row - 1][col - 1];
   }
 }
