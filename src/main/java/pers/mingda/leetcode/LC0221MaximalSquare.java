97fe7df76f2aa64c20817d50e6a6411f91cdf0c6
Mingda Rui <mingdarui@gmail.com>
2026-01-07 23:30:54 -0800

    LC0221 Maximal Square - Tabulation code refactor

--- src/main/java/pers/mingda/leetcode/LC0221MaximalSquare.java
+++ src/main/java/pers/mingda/leetcode/LC0221MaximalSquare.java
@@ -57,32 +57,25 @@ class LC0221TabulationSolution {
     int row = matrix.length;
     int col = matrix[0].length;
 
-    int[] tab = new int[col];
+    int[] tab = new int[col + 1];
     int topLeft = 0;
     int maxLen = Integer.MIN_VALUE;
-    for (int i = 0; i < row; i++) {
-      for (int j = 0; j < col; j++) {
-        if (matrix[i][j] == '1') {
-          int top = inBoundary(matrix, i - 1, j) ? tab[j] : 0;
-          int left = inBoundary(matrix, i, j - 1) ? tab[j - 1] : 0;
+    for (int i = 1; i <= row; i++) {
+      for (int j = 1; j <= col; j++) {
+        int temp = tab[j];
+        if (matrix[i - 1][j - 1] == '1') {
+          int top = tab[j];
+          int left = tab[j - 1];
 
           int min = Math.min(top, Math.min(left, topLeft));
-          topLeft = tab[j];
           tab[j] = min + 1;
           maxLen = Math.max(tab[j], maxLen);
         } else {
-          topLeft = tab[j];
           tab[j] = 0;
         }
+        topLeft = temp;
       }
-      topLeft = 0;
     }
     return maxLen * maxLen;
-  }
-
-  private boolean inBoundary(char[][] matrix, int row, int col) {
-    int rowMax = matrix.length;
-    int colMax = matrix[0].length;
-    return 0 <= row && row < rowMax && 0 <= col && col < colMax;
   }
 }
