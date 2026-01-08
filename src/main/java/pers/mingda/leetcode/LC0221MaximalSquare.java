a0c3961bc28a296bbe06bf70ffad78370e4705da
Mingda Rui <mingdarui@gmail.com>
2026-01-07 23:14:34 -0800

    LC0221 Maximal Square - Tabulation solution

--- src/main/java/pers/mingda/leetcode/LC0221MaximalSquare.java
+++ src/main/java/pers/mingda/leetcode/LC0221MaximalSquare.java
@@ -50,3 +50,39 @@ class LC0221Solution {
     return 0 <= row && row < maxRow && 0 <= col && col < maxCol;
   }
 }
+
+class LC0221TabulationSolution {
+
+  public int maximalSquare(char[][] matrix) {
+    int row = matrix.length;
+    int col = matrix[0].length;
+
+    int[] tab = new int[col];
+    int topLeft = 0;
+    int maxLen = Integer.MIN_VALUE;
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        if (matrix[i][j] == '1') {
+          int top = inBoundary(matrix, i - 1, j) ? tab[j] : 0;
+          int left = inBoundary(matrix, i, j - 1) ? tab[j - 1] : 0;
+
+          int min = Math.min(top, Math.min(left, topLeft));
+          topLeft = tab[j];
+          tab[j] = min + 1;
+          maxLen = Math.max(tab[j], maxLen);
+        } else {
+          topLeft = tab[j];
+          tab[j] = 0;
+        }
+      }
+      topLeft = 0;
+    }
+    return maxLen * maxLen;
+  }
+
+  private boolean inBoundary(char[][] matrix, int row, int col) {
+    int rowMax = matrix.length;
+    int colMax = matrix[0].length;
+    return 0 <= row && row < rowMax && 0 <= col && col < colMax;
+  }
+}
