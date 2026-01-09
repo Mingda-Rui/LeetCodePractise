66f6fb45a081ce859e23abf20e1ed40b795463c2
Mingda Rui <mingdarui@gmail.com>
2026-01-08 22:10:00 -0800

    LC0229 Majority Element II - Boyer-Moore solution

--- src/main/java/pers/mingda/leetcode/LC0229MajorityElementII.java
+++ src/main/java/pers/mingda/leetcode/LC0229MajorityElementII.java
@@ -27,3 +27,48 @@ class LC0229Solution {
     return result;
   }
 }
+
+class LC0229BoyerMooreSolution {
+
+  public List<Integer> majorityElement(int[] nums) {
+    List<Integer> result = new ArrayList<>();
+    int candidate1 = 0;
+    int candidate2 = 0;
+    int count1 = 0;
+    int count2 = 0;
+
+    for (int num : nums) {
+      if (count1 != 0 && candidate1 == num) {
+        count1++;
+      } else if (count2 != 0 && candidate2 == num) {
+        count2++;
+      } else if (count1 == 0) {
+        candidate1 = num;
+        count1++;
+      } else if (count2 == 0) {
+        candidate2 = num;
+        count2++;
+      } else {
+        count1--;
+        count2--;
+      }
+    }
+
+    boolean hasCandidate1 = count1 != 0;
+    boolean hasCandidate2 = count2 != 0;
+    count1 = 0;
+    count2 = 0;
+    for (int num : nums) {
+      if (hasCandidate1 && num == candidate1) count1++;
+      else if (hasCandidate2 && num == candidate2) count2++;
+    }
+    if (count1 > nums.length / 3) {
+      result.add(candidate1);
+    }
+    if (count2 > nums.length / 3) {
+      result.add(candidate2);
+    }
+
+    return result;
+  }
+}
