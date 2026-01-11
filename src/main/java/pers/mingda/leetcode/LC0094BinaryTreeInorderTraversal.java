5e6f02d3ef2807e835a80d4e7b1ee32359d409b4
Mingda Rui <mingdarui@gmail.com>
2026-01-10 19:26:29 -0800

    LC0094 Binary Tree Inorder Traversal - Iterative solution

---
+++ src/main/java/pers/mingda/leetcode/LC0094BinaryTreeInorderTraversal.java
@@ -0,0 +1,51 @@
+package pers.mingda.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class LC0094BinaryTreeInorderTraversal {}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class LC0094IterativeSolution {
+
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+
+    TreeNode node = root;
+    Stack<TreeNode> stack = new Stack<>();
+
+    while (node != null || !stack.empty()) {
+      while (node.left != null) {
+        stack.push(node);
+        node = node.left;
+      }
+      result.add(node.val);
+      node = node.right;
+      while (node == null && !stack.empty()) {
+        node = stack.pop();
+        result.add(node.val);
+        node = node.right;
+      }
+    }
+
+    return result;
+  }
+}
