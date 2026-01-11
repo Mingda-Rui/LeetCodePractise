fbde57dea84969f8f28d9463a817bf2d33bf15d7
Mingda Rui <mingdarui@gmail.com>
2026-01-10 23:02:41 -0800

    LC0145 Binary Tree Postorder Traversal - Iterative natural order solution

--- src/main/java/pers/mingda/leetcode/LC0145BinaryTreePostorderTraversal.java
+++ src/main/java/pers/mingda/leetcode/LC0145BinaryTreePostorderTraversal.java
@@ -27,3 +27,33 @@ class LC0145IterativeSolution {
     return result;
   }
 }
+
+class LC0145IterativeNaturalOrderSolution {
+
+  public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> result = new LinkedList<>();
+    Stack<TreeNode> stack = new Stack<>();
+
+    TreeNode prev = null;
+    TreeNode node = root;
+
+    while (node != null || !stack.empty()) {
+      if (node != null) {
+        stack.push(node);
+        node = node.left;
+      } else {
+        node = stack.peek();
+        if (node.right == null || node.right == prev) {
+          result.add(node.val);
+          prev = node;
+          stack.pop();
+          node = null;
+        } else {
+          node = node.right;
+        }
+      }
+    }
+
+    return result;
+  }
+}
