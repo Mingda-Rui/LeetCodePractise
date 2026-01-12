2ed4d75293834b5bb1cdebec96760cadefbebf1f
Mingda Rui <mingdarui@gmail.com>
2026-01-11 22:55:48 -0800

    LC0201 Bitwise AND of Numbers Range - Brian Kernighan's Algorithm

--- src/main/java/pers/mingda/leetcode/LC0201BitwiseAndOfNumbersRange.java
+++ src/main/java/pers/mingda/leetcode/LC0201BitwiseAndOfNumbersRange.java
@@ -14,3 +14,13 @@ class LC0201Solution {
     return left << count;
   }
 }
+
+class LC0201BrianKernighanAlgorithmSolution {
+
+  public int rangeBitwiseAnd(int left, int right) {
+    while (left < right) {
+      right = right & (right - 1);
+    }
+    return right;
+  }
+}
