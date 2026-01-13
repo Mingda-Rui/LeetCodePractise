d47703b84f0aac827fc100d088f85132a174b0d6
Mingda Rui <mingdarui@gmail.com>
2026-01-13 00:02:14 -0800

    LC0877 Stone Game - Code refactor

--- src/main/java/pers/mingda/leetcode/LC0877StoneGame.java
+++ src/main/java/pers/mingda/leetcode/LC0877StoneGame.java
@@ -1,64 +1,49 @@
 package pers.mingda.leetcode;
 
-import java.util.Arrays;
-
 public class LC0877StoneGame {}
 
 class LC0877Solution {
 
   public boolean stoneGame(int[] piles) {
-    int totalStones = Arrays.stream(piles).sum();
-    int[][] aliceRecord = new int[piles.length][piles.length];
-    int maxStone = stoneGame(piles, 0, piles.length - 1, totalStones, 0, true, aliceRecord);
-    return maxStone > totalStones / 2;
+    int[][] record = new int[piles.length][piles.length];
+    int maxStone = stoneGame(piles, 0, piles.length - 1, 0, record);
+    return maxStone > 0;
   }
 
   private int stoneGame(
     int[] piles,
     int leftPile,
     int rightPile,
-    int totalStones,
     int currentStones,
-    boolean aliceTurn,
-    int[][] aliceRecord
+    int[][] record
   ) {
     if (leftPile > rightPile) {
       return currentStones;
     }
-    if (aliceTurn && aliceRecord[leftPile][rightPile] != 0) {
-      return aliceRecord[leftPile][rightPile];
-    }
-    if (currentStones >= totalStones / 2) {
-      aliceRecord[leftPile][rightPile] = currentStones;
+    if (record[leftPile][rightPile] != 0) {
+      return record[leftPile][rightPile];
     }
 
+    boolean isAlice = (rightPile - leftPile) % 2 == 0;
     int leftPiles = piles[leftPile];
     int leftStones = stoneGame(
       piles,
       leftPile + 1,
       rightPile,
-      totalStones,
-      aliceTurn ? currentStones + leftPiles : currentStones,
-      !aliceTurn,
-      aliceRecord
+      isAlice ? currentStones + leftPiles : currentStones - leftPiles,
+      record
     );
-    if (leftStones > totalStones / 2) {
-      return leftStones;
-    }
+
     int rightPiles = piles[rightPile];
     int rightStones = stoneGame(
       piles,
       leftPile,
       rightPile - 1,
-      totalStones,
-      aliceTurn ? currentStones + rightPiles : currentStones,
-      !aliceTurn,
-      aliceRecord
+      isAlice ? currentStones + rightPiles : currentStones - rightPiles,
+      record
     );
     int maxStone = Math.max(leftStones, rightStones);
-    if (aliceTurn) {
-      aliceRecord[leftPile][rightPile] = maxStone;
-    }
+    record[leftPile][rightPile] = maxStone;
     return maxStone;
   }
 }
