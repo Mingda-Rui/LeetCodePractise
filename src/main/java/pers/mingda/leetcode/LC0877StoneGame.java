4ef13b126831dd6d0b262d5e00be9d193da83d83
Mingda Rui <mingdarui@gmail.com>
2026-01-13 23:07:03 -0800

    LC0877 Stone Game - Iterative solution

--- src/main/java/pers/mingda/leetcode/LC0877StoneGame.java
+++ src/main/java/pers/mingda/leetcode/LC0877StoneGame.java
@@ -51,3 +51,32 @@ class LC0877Solution {
     return record[leftPile][rightPile];
   }
 }
+
+class Solution {
+
+  public boolean stoneGame(int[] piles) {
+    int[][] record = new int[piles.length][piles.length];
+    for (int remainedPiles = 1; remainedPiles <= piles.length; remainedPiles++) {
+      for (int leftPile = 0; leftPile <= piles.length - remainedPiles; leftPile++) {
+        int rightPile = leftPile + remainedPiles - 1;
+        boolean isAlice = (piles.length - remainedPiles) % 2 == 0;
+
+        if (isAlice) {
+          int leftPick = piles[leftPile] + record[leftPile + 1][rightPile];
+          int rightPick = piles[rightPile] + record[leftPile][rightPile - 1];
+          int max = Math.max(leftPick, rightPick);
+          record[leftPile][rightPile] = max;
+        } else {
+          int leftPick =
+            -piles[leftPile] + (leftPile == rightPile ? 0 : record[leftPile + 1][rightPile]);
+          int rightPick =
+            -piles[rightPile] + (leftPile == rightPile ? 0 : record[leftPile][rightPile - 1]);
+          int max = Math.min(leftPick, rightPick);
+          record[leftPile][rightPile] = max;
+        }
+      }
+    }
+
+    return record[0][piles.length - 1] > 0;
+  }
+}
