17070bb6a4e2138d395f6340edc5b743ff3a1ac1
Mingda Rui <mingdarui@gmail.com>
2026-01-21 00:26:59 -0800

    LC1405 Greedy Solution

--- src/main/java/pers/mingda/leetcode/LC1405LongestHappyString.java
+++ src/main/java/pers/mingda/leetcode/LC1405LongestHappyString.java
@@ -50,3 +50,48 @@ class LC1405Solution {
     return ch == sb.charAt(len - 1) && ch == sb.charAt(len - 2);
   }
 }
+
+class LC1405GreedySolution {
+
+  public String longestDiverseString(int a, int b, int c) {
+    int consecutiveA = 0;
+    int consecutiveB = 0;
+    int consecutiveC = 0;
+    int maxLen = a + b + c;
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < maxLen; i++) {
+      if (
+        (a >= b && a >= c && consecutiveA < 2) ||
+        (a > 0 && (consecutiveB == 2 || consecutiveC == 2))
+      ) {
+        sb.append('a');
+        a--;
+        consecutiveA++;
+        consecutiveB = 0;
+        consecutiveC = 0;
+      } else if (
+        (b >= a && b >= c && consecutiveB < 2) ||
+        (b > 0 && (consecutiveA == 2 || consecutiveC == 2))
+      ) {
+        sb.append('b');
+        b--;
+        consecutiveB++;
+        consecutiveA = 0;
+        consecutiveC = 0;
+      } else if (
+        (c >= a && c >= b && consecutiveC < 2) ||
+        (c > 0 && (consecutiveA == 2 || consecutiveB == 2))
+      ) {
+        sb.append('c');
+        c--;
+        consecutiveC++;
+        consecutiveA = 0;
+        consecutiveB = 0;
+      } else {
+        return sb.toString();
+      }
+    }
+    return sb.toString();
+  }
+}
