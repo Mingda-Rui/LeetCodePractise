0b3682623a2df58d502d88de3bfb9ed37a3b711f
Mingda Rui <mingdarui@gmail.com>
2026-01-21 01:16:54 -0800

    LC1094 Car Pooling - Bucket Sort Solution

--- src/main/java/pers/mingda/leetcode/LC1094CarPooling.java
+++ src/main/java/pers/mingda/leetcode/LC1094CarPooling.java
@@ -27,3 +27,27 @@ class LC1094TreeMapQueueSolution {
     return true;
   }
 }
+
+class LC1094BucketSortSolution {
+
+  public boolean carPooling(int[][] trips, int capacity) {
+    int[] bucket = new int[1001];
+    for (int[] trip : trips) {
+      int passengers = trip[0];
+      int from = trip[1];
+      bucket[from] += passengers;
+
+      int to = trip[2];
+      bucket[to] -= passengers;
+    }
+
+    int passengerCount = 0;
+    for (int p : bucket) {
+      passengerCount += p;
+      if (passengerCount > capacity) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
