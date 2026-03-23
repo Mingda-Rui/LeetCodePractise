3e9d1b44a4490d68aa86aaddb317cd8e9f4e65ee
Mingda Rui <mingdarui@gmail.com>
2026-03-22 17:48:18 -0700

    LC0137 Single Number II - Bit minapulation solution

--- src/main/java/pers/mingda/leetcode/LC0137SingleNumberII.java
+++ src/main/java/pers/mingda/leetcode/LC0137SingleNumberII.java
@@ -21,3 +21,19 @@ class LC0137Solution {
     return 0;
   }
 }
+
+class LC0137BitManipulationSolution {
+
+  public int singleNumber(int[] nums) {
+    int result = 0;
+    for (int i = 0; i < 32; i++) {
+      int bitSum = 0;
+      for (int num : nums) {
+        bitSum += (num >> i) & 1;
+      }
+      result += (bitSum % 3) << i;
+    }
+
+    return result;
+  }
+}
