0d0dafb63f727b9ee48abecc4b07eb8ed57480e7
Mingda Rui <mingdarui@gmail.com>
2026-05-02 18:42:01 -0700

    LC1631 Path with Minimum Effort - Union Find Solution

--- src/main/java/pers/mingda/leetcode/LC1631PathWithMinimumEffort.java
+++ src/main/java/pers/mingda/leetcode/LC1631PathWithMinimumEffort.java
@@ -46,3 +46,88 @@ class LC1632Solution {
 }
 
 record LC1631Cell(int row, int col, int effort){}
+
+class LC1631UnionFindSolution {
+  public int minimumEffortPath(int[][] heights) {
+    int rows = heights.length;
+    int cols = heights[0].length;
+    Comparator<LC1631Edge> edgeComparator = Comparator.comparingInt(LC1631Edge::diff);
+    Queue<LC1631Edge> nonDecreaseEdges = new PriorityQueue<>(edgeComparator);
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        int flatten = flatten(i, j, cols);
+        if (i + 1 < rows) {
+          LC1631Edge toBelow = new LC1631Edge(flatten, flatten(i + 1, j, cols), getDiff(i, j, 1, 0, heights));
+          nonDecreaseEdges.add(toBelow);
+        }
+        if (j + 1 < cols) {
+          LC1631Edge toRight = new LC1631Edge(flatten, flatten(i, j + 1, cols), getDiff(i, j, 0, 1, heights));
+          nonDecreaseEdges.add(toRight);
+        }
+      }
+    }
+
+    LC1631UnionFind uf = new LC1631UnionFind(rows * cols);
+    int start = flatten(0, 0, cols);
+    int end = flatten(rows - 1, cols - 1, cols);
+    while (!nonDecreaseEdges.isEmpty()) {
+      LC1631Edge edge = nonDecreaseEdges.remove();
+      uf.union(edge);
+      if (uf.find(start) == uf.find(end)) {
+        return edge.diff();
+      }
+    }
+    return 0;
+  }
+
+  private int flatten(int row, int col, int cols) {
+    return row * cols + col;
+  }
+
+  private int getDiff(int row, int col, int rowOffset, int colOffset, int[][] heights) {
+    int height1 = heights[row][col];
+    int height2 = heights[row + rowOffset][col + colOffset];
+    return Math.abs(height1 - height2);
+  }
+}
+
+class LC1631UnionFind {
+  private final int[] parent;
+  private final int[] size;
+
+  public LC1631UnionFind(int size) {
+    this.parent = new int[size];
+    this.size = new int[size];
+    for (int i = 0; i < parent.length; i++) {
+      parent[i] = i;
+    }
+  }
+
+  public void union(LC1631Edge edge) {
+    int parent1 = find(edge.start());
+    int parent2 = find(edge.end());
+
+    if (parent1 == parent2) {
+      return;
+    }
+
+    if (size[parent1] > size[parent2]) {
+      parent[parent2] = parent1;
+      size[parent1] += size[parent2];
+    } else {
+      parent[parent1] = parent2;
+      size[parent2] += size[parent1];
+    }
+  }
+
+  public int find(int node) {
+    if (parent[node] == node) {
+      return node;
+    }
+
+    parent[node] = find(parent[node]);
+    return parent[node];
+  }
+}
+
+record LC1631Edge(int start, int end, int diff){};
