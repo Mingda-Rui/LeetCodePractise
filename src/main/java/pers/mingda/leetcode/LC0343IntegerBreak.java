4cef09d4c48065cc285fe50175eec46baed4fd9d
Mingda Rui <mingdarui@gmail.com>
2026-05-10 15:23:15 -0700

    LC0343 Integer Break - Code Refactor

--- src/main/java/pers/mingda/leetcode/LC0343IntegerBreak.java
+++ src/main/java/pers/mingda/leetcode/LC0343IntegerBreak.java
@@ -8,24 +8,21 @@ public class LC0343IntegerBreak {
 
 class LC0343Solution {
   public int integerBreak(int n) {
-    Map<Integer, Integer> maxProductMap = new HashMap<>();
-    int maxProduct = 0;
-    for (int n1 = 1; n1 <= n / 2; n1++) {
-      int n2 = n - n1;
-      int product = getMaxProduct(n1, maxProductMap) * getMaxProduct(n2, maxProductMap);
-      maxProduct = Math.max(maxProduct, product);
+    if (n <= 3) {
+      return n - 1;
     }
-    return maxProduct;
+    Map<Integer, Integer> maxProductMap = new HashMap<>();
+    return getMaxProduct(n, maxProductMap);
   }
 
   private int getMaxProduct(int n, Map<Integer, Integer> maxProductMap) {
     if (maxProductMap.containsKey(n)) {
       return maxProductMap.get(n);
     }
-    if (n == 1) {
+    if (n <= 3) {
       return n;
     }
-    int maxProduct = n;
+    int maxProduct = Integer.MIN_VALUE;
     for (int p = 1; p < n; p++) {
       int product = p * getMaxProduct(n - p, maxProductMap);
       maxProduct = Math.max(maxProduct, product);
