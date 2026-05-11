d10f622e022088ee228f5f75031c737ca4cbf982
Mingda Rui <mingdarui@gmail.com>
2026-05-10 19:00:59 -0700

    LC0343 Integer Break - Equation Solution

--- src/main/java/pers/mingda/leetcode/LC0343IntegerBreak.java
+++ src/main/java/pers/mingda/leetcode/LC0343IntegerBreak.java
@@ -67,3 +67,19 @@ class LC0343MathematicalSolution {
     return result;
   }
 }
+
+class LC0343EquationSolution {
+  public int integerBreak(int n) {
+    if (n <= 3) {
+      return n - 1;
+    }
+
+    if (n % 3 == 0) {
+      return (int) Math.pow(3, (double) n / 3);
+    } else if (n % 3 == 1) {
+      return (int) Math.pow(3, (double) n / 3 - 1) * 4;
+    } else {
+      return (int) Math.pow(3, (double) n / 3) * 2;
+    }
+  }
+}
