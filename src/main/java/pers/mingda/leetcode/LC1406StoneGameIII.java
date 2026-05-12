75a221acd243afee851c71e19928297f3e777c82
Mingda Rui <mingdarui@gmail.com>
2026-05-11 21:48:27 -0700

    LC1406 Stone Game III - O(1) Space Complexity Optimization

--- src/main/java/pers/mingda/leetcode/LC1406StoneGameIII.java
+++ src/main/java/pers/mingda/leetcode/LC1406StoneGameIII.java
@@ -5,23 +5,23 @@ public class LC1406StoneGameIII {
 
 
 class LC1406Solution {
-  public String stoneGameIII(int[] stoneValue) {
-    int[] memo = new int[stoneValue.length + 1];
-    for (int i = stoneValue.length - 1; i >= 0; i--) {
-      int max = Integer.MIN_VALUE;
-      int score = stoneValue[i];
-      max = Math.max(max, score - memo[i + 1]);
-      if (i + 1 < stoneValue.length) {
-        score += stoneValue[i + 1];
-        max = Math.max(max, score - memo[i + 2]);
+    public String stoneGameIII(int[] stoneValue) {
+      int[] memo = new int[3];
+      for (int i = stoneValue.length - 1; i >= 0; i--) {
+        int max = Integer.MIN_VALUE;
+        int score = stoneValue[i];
+        max = Math.max(max, score - memo[(i + 1) % 3]);
+        if (i + 1 < stoneValue.length) {
+          score += stoneValue[i + 1];
+          max = Math.max(max, score - memo[(i + 2) % 3]);
+        }
+        if (i + 2 < stoneValue.length) {
+          score += stoneValue[i + 2];
+          max = Math.max(max,score - memo[(i + 3) % 3]);
+        }
+        memo[i % 3] = max;
       }
-      if (i + 2 < stoneValue.length) {
-        score += stoneValue[i + 2];
-        max = Math.max(max,score - memo[i + 3]);
-      }
-      memo[i] = max;
-    }
 
-    return memo[0] > 0 ? "Alice" : (memo[0] < 0 ? "Bob" : "Tie");
+      return memo[0] > 0 ? "Alice" : (memo[0] < 0 ? "Bob" : "Tie");
+    }
   }
-}
