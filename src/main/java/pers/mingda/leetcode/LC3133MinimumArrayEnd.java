6c4ea09ad70d77138d77d0fd7de0cfde6fe3b504
Mingda Rui <mingdarui@gmail.com>
2026-05-15 00:16:44 -0700

    LC3133 Minimum Array End - Bit Manipulation Solution

--- src/main/java/pers/mingda/leetcode/LC3133MinimumArrayEnd.java
+++ src/main/java/pers/mingda/leetcode/LC3133MinimumArrayEnd.java
@@ -15,3 +15,30 @@ class LC3133Solution {
     return result;
   }
 }
+
+class LC3133BitManipulationSolution {
+  public long minEnd(int n, int x) {
+    long result = x;
+    int pos = 0;
+    n--;
+
+    while (n != 0) {
+      int xBit = x & 1;
+      x >>= 1;
+      pos++;
+
+      if (xBit == 1) {
+        continue;
+      }
+
+      int nBit = n & 1;
+      n >>= 1;
+      if (nBit == 0) {
+        continue;
+      }
+
+      result |= (1L << (pos - 1));
+    }
+    return result;
+  }
+}
