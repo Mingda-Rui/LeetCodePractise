f88d686a3273d5443a42bfe5c83894330410499f
Mingda Rui <mingdarui@gmail.com>
2026-05-22 22:03:12 -0700

    LC1498 Number of Subsequences That Satisfy the Given Sum Condition

---
+++ src/main/java/pers/mingda/leetcode/LC1498NumberOfSubsequencesThatSatisfyTheGivenSumCondition.java
@@ -0,0 +1,45 @@
+package pers.mingda.leetcode;
+
+import java.util.Arrays;
+
+public class LC1498NumberOfSubsequencesThatSatisfyTheGivenSumCondition {
+}
+
+class LC1498Solution {
+  public int numSubSeq(int[] nums, int target) {
+    Arrays.sort(nums);
+    int modulo = 1_000_000_007;
+    int[] powers = calculatePowers(nums.length, modulo);
+
+    int result = 0;
+    for (int i = 0; i < nums.length; i++) {
+      int right = binarySearchRightMost(nums, i, target - nums[i]);
+      if (i <= right) {
+        result = (result + powers[right - i]) % modulo;
+      }
+    }
+    return result;
+  }
+
+  private int[] calculatePowers(int digits, int modulo) {
+    int[] powers = new int[digits];
+    powers[0] = 1;
+    for (int i = 1; i < digits; i++) {
+      powers[i] = powers[i - 1] * 2 % modulo;
+    }
+    return powers;
+  }
+
+  private int binarySearchRightMost(int[] nums, int start, int target) {
+    int end = nums.length;
+    while (start < end) {
+      int mid = (start + end) / 2;
+      if (nums[mid] > target) {
+        end = mid;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return start - 1;
+  }
+}
