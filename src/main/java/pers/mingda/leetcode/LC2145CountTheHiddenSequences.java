d8e4b35a0343d28b3055fc5cdc137bb4f3dd0768
Mingda Rui <mingdarui@gmail.com>
2026-06-04 20:49:34 -0700

    LC2145 Count the Hidden Sequences

---
+++ src/main/java/pers/mingda/leetcode/LC2145CountTheHiddenSequences.java
@@ -0,0 +1,20 @@
+package pers.mingda.leetcode;
+
+public class LC2145CountTheHiddenSequences {
+}
+
+class LC2145Solution {
+  public int numberOfArrays(int[] differences, int lower, int upper) {
+    long offset = 0;
+    long maxOffset = offset;
+    long minOffset = offset;
+
+    for (int diff : differences) {
+      offset += diff;
+      maxOffset = Math.max(maxOffset, offset);
+      minOffset = Math.min(minOffset, offset);
+    }
+    long range = maxOffset - minOffset;
+    return Math.max(0, (int)((upper - lower) - range + 1));
+  }
+}
