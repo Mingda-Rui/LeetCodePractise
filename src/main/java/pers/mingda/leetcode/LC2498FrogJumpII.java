bb5de6091bb17e6dc8d5ac4152af873b7a56be83
Mingda Rui <mingdarui@gmail.com>
2026-06-07 19:53:10 -0700

    LC2498 Frog Jump II

---
+++ src/main/java/pers/mingda/leetcode/LC2498FrogJumpII.java
@@ -0,0 +1,15 @@
+package pers.mingda.leetcode;
+
+public class LC2498FrogJumpII {
+}
+
+class LC2498Solution {
+  public int maxJump(int[] stones) {
+    int result = 0;
+    for (int i = 1; i < stones.length; i++) {
+      int prevStep = Math.max(0, i - 2);
+      result = Math.max(result, stones[i] - stones[prevStep]);
+    }
+    return result;
+  }
+}
