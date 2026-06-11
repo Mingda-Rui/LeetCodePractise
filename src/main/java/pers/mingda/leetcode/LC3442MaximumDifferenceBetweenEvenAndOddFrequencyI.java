30a9f27f49ee1482c89d07ddbbfa5588b8fea63d
Mingda Rui <mingdarui@gmail.com>
2026-06-10 21:39:15 -0700

    LC3442 Maximum Difference Between Event and Odd Frequency I

---
+++ src/main/java/pers/mingda/leetcode/LC3442MaximumDifferenceBetweenEvenAndOddFrequencyI.java
@@ -0,0 +1,27 @@
+package pers.mingda.leetcode;
+
+public class LC3442MaximumDifferenceBetweenEvenAndOddFrequencyI {
+}
+
+class LC3442Solution {
+  public int maxDifference(String s) {
+    int[] letterCounter = new int[26];
+    for (char c : s.toCharArray()) {
+      int index = c - 'a';
+      letterCounter[index]++;
+    }
+    int maxOdd = Integer.MIN_VALUE;
+    int minEven = Integer.MAX_VALUE;
+    for (int count : letterCounter) {
+      if (count == 0) {
+        continue;
+      }
+      if (count % 2 == 0) {
+        minEven = Math.min(minEven, count);
+      } else {
+        maxOdd = Math.max(maxOdd, count);
+      }
+    }
+    return maxOdd - minEven;
+  }
+}
