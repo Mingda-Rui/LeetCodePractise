ae1d0ff46eb2e2e36717e4f1b82a627bfa9328b7
Mingda Rui <mingdarui@gmail.com>
2026-06-16 22:33:26 -0700

    LC0951 Flip Equivalent Binary Trees - Stack DFS Solution

--- src/main/java/pers/mingda/leetcode/LC0951FlipEquivalentBinaryTrees.java
+++ src/main/java/pers/mingda/leetcode/LC0951FlipEquivalentBinaryTrees.java
@@ -1,5 +1,7 @@
 package pers.mingda.leetcode;
 
+import java.util.Stack;
+
 public class LC0951FlipEquivalentBinaryTrees {}
 
 /**
@@ -21,5 +23,52 @@ class LC0951Solution {
     }
     return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right))
         || (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+  }
+}
+
+class LC0951StackSolution {
+  public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+    Stack<TreeNode> stack1 = new Stack<>();
+    Stack<TreeNode> stack2 = new Stack<>();
+
+    stack1.push(root1);
+    stack2.push(root2);
+    while (!stack1.isEmpty()) {
+      TreeNode node1 = stack1.pop();
+      TreeNode node2 = stack2.pop();
+      if (!isEqual(node1, node2)) {
+        return false;
+      }
+      if (node1 == null) {
+        continue;
+      }
+
+      if (isEqual(node1.left, node2.left) && isEqual(node1.right, node2.right)) {
+        stack1.push(node1.left);
+        stack2.push(node2.left);
+        stack1.push(node1.right);
+        stack2.push(node2.right);
+      } else if (isEqual(node1.left, node2.right) && isEqual(node1.right, node2.left)) {
+        stack1.push(node1.left);
+        stack2.push(node2.right);
+        stack1.push(node1.right);
+        stack2.push(node2.left);
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean isEqual(TreeNode node1, TreeNode node2) {
+    if (node1 == null && node2 == null) {
+      return true;
+    }
+
+    if (node1 == null || node2 == null) {
+      return false;
+    }
+
+    return node1.val == node2.val;
   }
 }
