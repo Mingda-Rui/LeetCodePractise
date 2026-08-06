ea698a042becee82fcd340da9cc12b94b00dd383
Mingda Rui <mingdarui@gmail.com>
2026-08-06 00:09:32 -0700

    LC4012 Count of Unfinished Tasks After Each Shift - Code Refactor

--- src/main/java/pers/mingda/leetcode/LC4012CountOfUnfinishedTasksAfterEachShift.java
+++ src/main/java/pers/mingda/leetcode/LC4012CountOfUnfinishedTasksAfterEachShift.java
@@ -3,50 +3,44 @@ package pers.mingda.leetcode;
 public class LC4012CountOfUnfinishedTasksAfterEachShift {}
 
 class LC4012Solution {
-  public int[] countTasks(int[] tasks, int[] shifts) {
-    long[] taskSum = new long[tasks.length];
-    taskSum[0] = tasks[0];
-    for (int i = 1; i < tasks.length; i++) {
-      taskSum[i] = taskSum[i - 1] + (long) tasks[i];
-    }
-    int[] count = new int[shifts.length];
-    int taskIndex = 0;
-    int currTaskWorked = 0;
-    for (int i = 0; i < shifts.length; i++) {
-      int shift = shifts[i];
-
-      long target = (taskIndex - 1 >= 0 ? taskSum[taskIndex - 1] : 0) + currTaskWorked + shift;
-      taskIndex = doShift(taskSum, taskIndex, target);
-
-      if (taskIndex == tasks.length || target == taskSum[taskIndex]) {
-        currTaskWorked = 0;
-      } else {
-        int leftOver = (int) (taskSum[taskIndex] - target);
-        currTaskWorked = tasks[taskIndex] - leftOver;
+    public int[] countTasks(int[] tasks, int[] shifts) {
+      long[] taskSum = new long[tasks.length];
+      taskSum[0] = tasks[0];
+      for (int i = 1; i < tasks.length; i++) {
+        taskSum[i] = taskSum[i - 1] + (long)tasks[i];
       }
-      count[i] = tasks.length - taskIndex;
+      long totalTasks = taskSum[taskSum.length - 1];
 
-      if (taskIndex == tasks.length) {
-        taskIndex = 0;
+      int[] count = new int[shifts.length];
+      long taskFinished = 0;
+      for (int i = 0; i < shifts.length; i++) {
+        int shift = shifts[i];
+        taskFinished += shift;
+        if (taskFinished >= totalTasks) {
+          taskFinished = 0;
+          count[i] = 0;
+        } else {
+          int nextIndex = doShift(taskSum, taskFinished);
+          count[i] = tasks.length - nextIndex;
+        }
       }
+      return count;
     }
-    return count;
+
+    private int doShift(long[] taskSum, long target) {
+      int start = 0;
+      int end = taskSum.length;
+      while (start < end) {
+        int mid = start + (end - start) / 2;
+        if (taskSum[mid] == target) {
+          end = mid + 1;
+          return end;
+        } else if (taskSum[mid] < target) {
+          start = mid + 1;
+        } else {
+          end = mid;
+        }
+      }
+      return end;
+    }
   }
-
-  private int doShift(long[] taskSum, int taskIndex, long target) {
-    int start = taskIndex;
-    int end = taskSum.length;
-    while (start < end) {
-      int mid = start + (end - start) / 2;
-      if (taskSum[mid] == target) {
-        end = mid + 1;
-        return end;
-      } else if (taskSum[mid] < target) {
-        start = mid + 1;
-      } else {
-        end = mid;
-      }
-    }
-    return end;
-  }
-}
