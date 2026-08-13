a636c53bf53d84754fd56df161b9c014c2096bee
Mingda Rui <mingdarui@gmail.com>
2026-08-12 22:23:13 -0700

    LC4013 Count Subarrays With Even Odd Ratio II - Segment Tree Solution

--- src/main/java/pers/mingda/leetcode/LC4013CountSubarraysWithEvenOddRatioII.java
+++ src/main/java/pers/mingda/leetcode/LC4013CountSubarraysWithEvenOddRatioII.java
@@ -146,3 +146,115 @@ class FenwickTree {
     return sum;
   }
 }
+
+class LC4013SegmentTreeSolution {
+  public long countRatioSubarrays(int[] nums, int a, int b) {
+    long[] prefixSum = getPrefixSum(nums, a, b);
+    Map<Long, Integer> coordCompMap = coordinateComp(prefixSum);
+
+    SegmentTree st = new SegmentTree(coordCompMap.size());
+    long result = 0;
+    for (long p : prefixSum) {
+      int coordinateCompIndex = coordCompMap.get(p);
+
+      if (p >= 0) {
+        result++;
+      }
+
+      result += st.query(0, coordinateCompIndex + 1);
+      st.increase(coordinateCompIndex);
+    }
+    return result;
+  }
+
+  private long[] getPrefixSum(int[] nums, int a, int b) {
+    long[] prefixSum = new long[nums.length];
+    // x / y <= a / b
+    // x * b <= y * a
+    // y * a - x * b >= 0
+    // x -> even number
+    // y -> odd number
+
+    for (int i = 0; i < nums.length; i++) {
+      int num = nums[i];
+      prefixSum[i] = (i == 0 ? 0 : prefixSum[i - 1]) + (num % 2 == 0 ? -b : a);
+    }
+
+    return prefixSum;
+  }
+
+  private Map<Long, Integer> coordinateComp(long[] prefixSum) {
+    Map<Long, Integer> coordinateComp = new HashMap<>();
+    long[] copied = Arrays.copyOf(prefixSum, prefixSum.length);
+    Arrays.sort(copied);
+    int index = 0;
+    for (long p : copied) {
+      if (!coordinateComp.containsKey(p)) {
+        coordinateComp.put(p, index);
+        index++;
+      }
+    }
+    return coordinateComp;
+  }
+}
+
+class SegmentTree {
+  int size;
+  SegTreeNode root;
+
+  public SegmentTree(int size) {
+    this.size = size;
+    this.root = init(0, size);
+  }
+
+  private SegTreeNode init(int start, int end) {
+    SegTreeNode node = new SegTreeNode(0, start, end);
+    if (start + 1 < end) {
+      int mid = (start + end) / 2;
+      node.left = init(start, mid);
+      node.right = init(mid, end);
+    }
+    return node;
+  }
+
+  public void increase(int index) {
+    increase(root, index);
+  }
+
+  public void increase(SegTreeNode root, int index) {
+    if (root.start == index && root.end == index + 1) {
+      root.val++;
+    } else if (root.start <= index && root.end > index) {
+      increase(root.left, index);
+      increase(root.right, index);
+      root.val = root.left.val + root.right.val;
+    }
+  }
+
+  public long query(int start, int end) {
+    return query(root, start, end);
+  }
+
+  private long query(SegTreeNode root, int start, int end) {
+    if (root.end <= start || root.start >= end) {
+      return 0;
+    } else if (root.start >= start && root.end <= end) {
+      return root.val;
+    }
+    return query(root.left, start, end) + query(root.right, start, end);
+  }
+}
+
+class SegTreeNode {
+  int start;
+  int end;
+  long val;
+  SegTreeNode left;
+  SegTreeNode right;
+
+  public SegTreeNode(long val, int start, int end) {
+    this.val = val;
+    this.start = start;
+    this.end = end;
+  }
+}
