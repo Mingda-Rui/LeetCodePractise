df111df6f524ba3a93931b3fc86011361f8e011e
Mingda Rui <mingdarui@gmail.com>
2026-08-13 22:48:20 -0700

    LC4016 Maximum Area of Two Non-overlapping Square Submatrices

---
+++ src/main/java/pers/mingda/leetcode/LC4016MaximumAreaOfTwoNonOverlappingSquareSubmatrices.java
@@ -0,0 +1,51 @@
+package pers.mingda.leetcode;
+
+public class LC4016MaximumAreaOfTwoNonOverlappingSquareSubmatrices {}
+
+class LC4016Solution {
+  public int maxArea(int[][] mat) {
+    int r = mat.length;
+    int c = mat[0].length;
+    int[][] pf = new int[r][c];
+
+    for (int i = 0; i < r; i++) {
+      for (int j = 0; j < c; j++) {
+        int leftSum = j == 0 ? 0 : pf[i][j - 1];
+        int upSum = i == 0 ? 0 : pf[i - 1][j];
+        int diagonalSum = (i == 0 || j == 0) ? 0 : pf[i - 1][j - 1];
+        pf[i][j] = leftSum + upSum - diagonalSum + mat[i][j];
+      }
+    }
+
+    for (int k = Math.min(r, c); k > 0; k--) {
+      int minLeft = c;
+      int maxLeft = 0;
+      int minUp = r;
+      int maxUp = 0;
+
+      for (int i = k - 1; i < r; i++) {
+        for (int j = k - 1; j < c; j++) {
+          if (isSquare(pf, i, j, k)) {
+            minLeft = Math.min(minLeft, j - k + 1);
+            maxLeft = Math.max(maxLeft, j - k + 1);
+            minUp = Math.min(minUp, i - k + 1);
+            maxUp = Math.max(maxUp, i - k + 1);
+          }
+        }
+      }
+
+      if (maxLeft - minLeft >= k || maxUp - minUp >= k) {
+        return k * k;
+      }
+    }
+    return 0;
+  }
+
+  private boolean isSquare(int[][] pf, int i, int j, int k) {
+    int leftCount = j - k < 0 ? 0 : pf[i][j - k];
+    int upCount = i - k < 0 ? 0 : pf[i - k][j];
+    int diagonalCount = (i - k < 0 || j - k < 0) ? 0 : pf[i - k][j - k];
+    int count = pf[i][j] - leftCount - upCount + diagonalCount;
+    return count == k * k;
+  }
+}
