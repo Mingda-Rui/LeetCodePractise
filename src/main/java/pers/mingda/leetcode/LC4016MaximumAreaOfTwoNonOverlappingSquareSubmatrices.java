51d86f0cc8fe2c5a40a6d88378aaf59f2779788f
Mingda Rui <mingdarui@gmail.com>
2026-08-15 00:11:55 -0700

    LC4016 Maximum Area of Two Non-overlapping Square Submatrices - Partition plus dp O(m*n) solution

--- src/main/java/pers/mingda/leetcode/LC4016MaximumAreaOfTwoNonOverlappingSquareSubmatrices.java
+++ src/main/java/pers/mingda/leetcode/LC4016MaximumAreaOfTwoNonOverlappingSquareSubmatrices.java
@@ -101,3 +101,54 @@ class LC4016BinarySearchSolution {
     return start;
   }
 }
+
+class LC4016PartitionSolution {
+  public int maxArea(int[][] mat) {
+    int r = mat.length;
+    int c = mat[0].length;
+    int[][] pf = new int[r][c];
+
+    int[] top = new int[r];
+    int[] left = new int[c];
+    for (int i = 0; i < r; i++) {
+      for (int j = 0; j < c; j++) {
+        int leftSum = j == 0 ? 0 : pf[i][j - 1];
+        int upSum = i == 0 ? 0 : pf[i - 1][j];
+        int diagonalSum = (i == 0 || j == 0) ? 0 : pf[i - 1][j - 1];
+        pf[i][j] = mat[i][j] == 0 ? 0 : Math.min(Math.min(leftSum, upSum), diagonalSum) + 1;
+
+        top[i] = Math.max(i == 0 ? 0 : top[i - 1], Math.max(top[i], pf[i][j]));
+        left[j] = Math.max(j == 0 ? 0 : left[j - 1], Math.max(left[j], pf[i][j]));
+      }
+    }
+
+    int[] bottom = new int[r];
+    int[] right = new int[c];
+    int[][] reversedPf = new int[r][c];
+    for (int i = r - 1; i >= 0; i--) {
+      for (int j = c - 1; j >= 0; j--) {
+        int rightSum = j == c - 1 ? 0 : reversedPf[i][j + 1];
+        int bottomSum = i == r - 1 ? 0 : reversedPf[i + 1][j];
+        int diagonalSum = (i == r - 1 || j == c - 1) ? 0 : reversedPf[i + 1][j + 1];
+        reversedPf[i][j] =
+            mat[i][j] == 0 ? 0 : Math.min(Math.min(rightSum, bottomSum), diagonalSum) + 1;
+
+        bottom[i] = Math.max(i == r - 1 ? 0 : bottom[i + 1], Math.max(bottom[i], reversedPf[i][j]));
+        right[j] = Math.max(j == c - 1 ? 0 : right[j + 1], Math.max(right[j], reversedPf[i][j]));
+      }
+    }
+
+    int k = 0;
+    for (int i = 0; i < r - 1; i++) {
+      int localMax = Math.min(top[i], bottom[i + 1]);
+      k = Math.max(k, localMax);
+    }
+
+    for (int j = 0; j < c - 1; j++) {
+      int localMax = Math.min(left[j], right[j + 1]);
+      k = Math.max(k, localMax);
+    }
+
+    return k * k;
+  }
+}
