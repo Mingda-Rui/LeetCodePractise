3b140fb44468dc6947f57107212424433bfe631d
Mingda Rui <mingdarui@gmail.com>
2026-08-27 22:35:38 -0700

    LC4030 Check ASCII Palindromic - Bit Manipulation

--- src/main/java/pers/mingda/leetcode/LC4030CheckASCIIPalindromic.java
+++ src/main/java/pers/mingda/leetcode/LC4030CheckASCIIPalindromic.java
@@ -22,3 +22,26 @@ class LC4030Solution {
     return binaryString.toString();
   }
 }
+
+class LC4030BitManipulationSolution {
+  public boolean isPalindromic(String s) {
+    int len = s.length();
+    for (int i = 0; i <= len / 2; i++) {
+      int j = len - i - 1;
+      if (!isPartialPalindromic(s.charAt(i), s.charAt(j))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean isPartialPalindromic(char a, char b) {
+    for (int i = 0; i < 8; i++) {
+      int j = 8 - i - 1;
+      if ((((int) a >> i) & 1) != (((int) b >> j) & 1)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
